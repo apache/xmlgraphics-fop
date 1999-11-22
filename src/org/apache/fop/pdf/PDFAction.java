62870da6d398a363c4493cd293df83723ad2b888
jtauber <jtauber@unknown>
1999-11-22 17:37:17 +0000

    incorporated the PDF side of simple-link support from Arved Sandstrom, ie org.apache.fop.pdf has additional classes for producing PDF documents with links
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193234 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/pdf/PDFAction.java
+++ src/org/apache/fop/pdf/PDFAction.java
@@ -48,101 +48,53 @@
  Software Foundation, please see <http://www.apache.org/>.
  
  */
+
 package org.apache.fop.pdf;
 
 /**
- * class representing a /Page object.
- *
- * There is one of these for every page in a PDF document. The object
- * specifies the dimensions of the page and references a /Resources
- * object, a contents stream and the page's parent in the page
- * hierarchy.
+ * class representing a /Action object.
  */
-public class PDFPage extends PDFObject {
+public class PDFAction extends PDFObject {
 
-    /** the page's parent, a /Pages object */
-    protected PDFPages parent;
-
-    /** the page's /Resource object */
-    protected PDFResources resources;
-
-    /** the contents stream */
-    protected PDFStream contents;
-
-    /** the width of the page in points */
-    protected int pagewidth;
-
-    /** the height of the page in points */
-    protected int pageheight;
-
-    /** the list of annotation objects for this page */
-    protected PDFAnnotList annotList;
+    /** the file specification */
+    protected PDFFileSpec fileSpec;
 
     /**
-     * create a /Page object
+     * create an /Action object.
      *
      * @param number the object's number
-     * @param resources the /Resources object
-     * @param contents the content stream
-     * @param pagewidth the page's width in points
-     * @param pageheight the page's height in points
+     * @param fileSpec the fileSpec associated with the action
      */
-    public PDFPage(int number, PDFResources resources,
-		   PDFStream contents, int pagewidth,
-		   int pageheight) {
+    public PDFAction(int number, PDFFileSpec fileSpec) {
 
 	/* generic creation of object */
 	super(number);
-
-	/* set fields using parameters */
-	this.resources = resources;
-	this.contents = contents;
-	this.pagewidth = pagewidth;
-	this.pageheight = pageheight;
-
-	this.annotList = null;
+	
+	this.fileSpec = fileSpec;
     }
 
     /**
-     * set this page's parent
-     *
-     * @param parent the /Pages object that is this page's parent
-     */
-    public void setParent(PDFPages parent) {
-	this.parent = parent;
-    }
-
-    /**
-     * set this page's annotation list
-     *
-     * @param annotList a PDFAnnotList list of annotations
-     */
-    public void setAnnotList(PDFAnnotList annotList) {
-	this.annotList = annotList;
-    }
-
-    /**
-     * represent this object as PDF
+     * represent the object in PDF
      *
      * @return the PDF string
      */
     public String toPDF() {
-	StringBuffer sb = new StringBuffer();
-
-	sb = sb.append(this.number + " " + this.generation + " obj\n" +
-		"<< / Type /Page\n" +
-		"/Parent " + this.parent.referencePDF() + "\n" +
-		"/MediaBox [ 0 0 " + this.pagewidth + " " +
-		this.pageheight + " ]\n" + 
-		"/Resources " + this.resources.referencePDF() + "\n" + 
-		"/Contents " + this.contents.referencePDF() + "\n");
-	if (this.annotList != null) {
-	    sb = sb.append("/Annots " +
-			   this.annotList.referencePDF() + "\n");
-	}
-
-	sb = sb.append(">>\nendobj\n");
-
-	return sb.toString();
+	String p = new String(this.number + " " + this.generation +
+			      " obj\n" +
+			      "<<\n/S /GoToR\n" +
+			      "/F " + fileSpec.referencePDF() + "\n" +
+			      "/D [ 0 /XYZ null null null ]" +
+			      " \n>>\nendobj\n"); 
+	return p;
     }
+
+    /* example
+       28 0 obj
+       << 
+       /S /GoToR 
+       /F 29 0 R 
+       /D [ 0 /XYZ -6 797 null ] 
+       >> 
+       endobj
+    */
 }
--- src/org/apache/fop/pdf/PDFAnnotList.java
+++ src/org/apache/fop/pdf/PDFAnnotList.java
@@ -48,101 +48,78 @@
  Software Foundation, please see <http://www.apache.org/>.
  
  */
+
 package org.apache.fop.pdf;
 
+// Java
+import java.util.Vector;
+
 /**
- * class representing a /Page object.
+ * class representing an object which is a list of annotations.
  *
- * There is one of these for every page in a PDF document. The object
- * specifies the dimensions of the page and references a /Resources
- * object, a contents stream and the page's parent in the page
- * hierarchy.
+ * This PDF object is a list of references to /Annot objects. So far we
+ * are dealing only with links.
  */
-public class PDFPage extends PDFObject {
+public class PDFAnnotList extends PDFObject {
 
-    /** the page's parent, a /Pages object */
-    protected PDFPages parent;
+    /** the /Annot objects */
+    protected Vector links = new Vector();
 
-    /** the page's /Resource object */
-    protected PDFResources resources;
-
-    /** the contents stream */
-    protected PDFStream contents;
-
-    /** the width of the page in points */
-    protected int pagewidth;
-
-    /** the height of the page in points */
-    protected int pageheight;
-
-    /** the list of annotation objects for this page */
-    protected PDFAnnotList annotList;
+    /** the number of /Annot objects */
+    protected int count = 0;
 
     /**
-     * create a /Page object
+     * create a /Annots object.
      *
      * @param number the object's number
-     * @param resources the /Resources object
-     * @param contents the content stream
-     * @param pagewidth the page's width in points
-     * @param pageheight the page's height in points
      */
-    public PDFPage(int number, PDFResources resources,
-		   PDFStream contents, int pagewidth,
-		   int pageheight) {
+    public PDFAnnotList(int number) {
 
 	/* generic creation of object */
 	super(number);
-
-	/* set fields using parameters */
-	this.resources = resources;
-	this.contents = contents;
-	this.pagewidth = pagewidth;
-	this.pageheight = pageheight;
-
-	this.annotList = null;
     }
 
     /**
-     * set this page's parent
+     * add an /Annot object of /Subtype /Link.
      *
-     * @param parent the /Pages object that is this page's parent
+     * @param link the PDFLink to add.
      */
-    public void setParent(PDFPages parent) {
-	this.parent = parent;
+    public void addLink(PDFLink link) {
+	this.links.addElement(link);
+	this.count++;
     }
 
     /**
-     * set this page's annotation list
+     * get the count of /Annot objects
      *
-     * @param annotList a PDFAnnotList list of annotations
+     * @return the number of links
      */
-    public void setAnnotList(PDFAnnotList annotList) {
-	this.annotList = annotList;
+    public int getCount() {
+	return this.count;
     }
 
     /**
-     * represent this object as PDF
+     * represent the object in PDF
      *
      * @return the PDF string
      */
     public String toPDF() {
-	StringBuffer sb = new StringBuffer();
-
-	sb = sb.append(this.number + " " + this.generation + " obj\n" +
-		"<< / Type /Page\n" +
-		"/Parent " + this.parent.referencePDF() + "\n" +
-		"/MediaBox [ 0 0 " + this.pagewidth + " " +
-		this.pageheight + " ]\n" + 
-		"/Resources " + this.resources.referencePDF() + "\n" + 
-		"/Contents " + this.contents.referencePDF() + "\n");
-	if (this.annotList != null) {
-	    sb = sb.append("/Annots " +
-			   this.annotList.referencePDF() + "\n");
+	StringBuffer p = new StringBuffer(this.number + " "
+					  + this.generation
+					  + " obj\n[\n"); 
+	for (int i = 0; i < this.count; i++) {
+	    p = p.append(((PDFObject)
+			  links.elementAt(i)).referencePDF() + "\n");
 	}
-
-	sb = sb.append(">>\nendobj\n");
-
-	return sb.toString();
+	p = p.append("]\nendobj\n");
+	return p.toString();
     }
+
+    /* example
+       20 0 obj
+       [ 
+       19 0 R 
+       ]
+       endobj
+    */
 }
