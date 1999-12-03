7897f01b9e3aeb3cbf3027357dad8850f2cfae4a
jtauber <jtauber@apache.org>
1999-12-03 08:36:49 +0000

    fixed bug that stopped XTCommandLine from working. contributed by Bill Raudabaugh
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193260 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/apps/XTCommandLine.java
+++ src/org/apache/fop/apps/XTCommandLine.java
@@ -22,7 +22,7 @@
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
  
- 4. The names "Fop" and  "Apache Software Foundation"  must not be used to
+ 4. The names "FOP" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
@@ -48,6 +48,7 @@
  Software Foundation, please see <http://www.apache.org/>.
  
  */
+
 package org.apache.fop.apps;
 
 import org.apache.fop.render.pdf.PDFRenderer;
@@ -117,7 +118,7 @@ public class XTCommandLine extends CommandLine {
 	    driver.addElementMapping("org.apache.fop.fo.StandardElementMapping");
 	    driver.addElementMapping("org.apache.fop.svg.SVGElementMapping");
 	    driver.setWriter(new PrintWriter(new FileWriter(args[2])));
-	    driver.buildFOTree(parser, fileInputSource(args[0]));
+	    driver.buildFOTree(xslProcessor, fileInputSource(args[0]));
 	    driver.format();
 	    driver.render();
 	} catch (Exception e) {
