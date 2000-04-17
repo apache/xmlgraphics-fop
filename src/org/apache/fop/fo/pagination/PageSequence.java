a5b36ba10217f6a39d713c89ded84107cd49ddfc
arved <arved@apache.org>
2000-04-17 23:16:52 +0000

    Added initial-page-number support
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193333 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/pagination/PageSequence.java
+++ src/org/apache/fop/fo/pagination/PageSequence.java
@@ -107,7 +107,7 @@ public class PageSequence extends FObj {
 	layoutMasterSet = root.getLayoutMasterSet();
 
 	InitialPageNumber ipn = (InitialPageNumber) this.properties.get("initial-page-number");
-	this.currentPageNumber = ipn.getInitialPageNumber() - 1;
+	this.currentPageNumber = ipn.getInteger().intValue() - 1;
     }
 
     protected Page makePage(AreaTree areaTree) throws FOPException {
