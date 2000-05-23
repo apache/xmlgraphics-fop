17d4afc21407ac00effa79ac46b3c76df4b5869c
eschaeffer <eschaeffer@unknown>
2000-05-23 09:05:07 +0000

    new FopImage interface
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193360 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/image/FopImage.java
+++ src/org/apache/fop/image/FopImage.java
@@ -48,27 +48,58 @@
  Software Foundation, please see <http://www.apache.org/>.
  
  */
-/* modified by JKT to integrate into 0.12.0 */
-
-//Title:        BoBoGi FOP
-//Version:      
-//Copyright:    Copyright (c) 1999
-//Author:       Sergio Botti
-//Company:      Dibe Elsag
-//Description:  Part in xml to pdf converter
-
+//Author:       Eric SCHAEFFER
+//Description:  represent an image object
 
 package org.apache.fop.image;
 
+import org.apache.fop.datatypes.ColorSpace;
+import org.apache.fop.pdf.PDFColor;
+import org.apache.fop.pdf.PDFFilter;
+
 public interface FopImage {
-  public int getpixelwidth();
-  public int getpixelheight();
-  public int getWidth();
-  public int getHeight();
-  public int getX();
-  public int getY();
-  public String gethref();
-  public int[] getimagemap();
-  public boolean getcolor();
-  public int getbitperpixel();
+	// Init the object.
+	// If href protocol isn't file://, can load the entire image
+	// and keep it in memory.
+	// Should cache the input stream, and load data when needed.
+//	public FopImage(URL href) throws FopImageException;
+
+	// Get image general properties.
+	// Methods throw exception because they can retrieve data
+	// when needed.
+
+	// Ressource location
+	public String getURL();
+
+	// image size
+	public int getWidth() throws FopImageException;
+	public int getHeight() throws FopImageException;
+
+	// DeviceGray, DeviceRGB, or DeviceCMYK
+	public ColorSpace getColorSpace() throws FopImageException;
+
+	// bits per pixel
+	public int getBitsPerPixel() throws FopImageException;
+
+	// For transparent images
+	public boolean isTransparent() throws FopImageException;
+	public PDFColor getTransparentColor() throws FopImageException;
+
+	// get the image bytes, and bytes properties
+
+	// get uncompressed image bytes
+	public byte[] getBitmaps() throws FopImageException;
+// width * (bitsPerPixel / 8) * height, no ?
+	public int getBitmapsSize() throws FopImageException;
+
+	// get compressed image bytes
+	// I don't know if we really need it, nor if it
+	// should be changed...
+	public byte[] getRessourceBytes() throws FopImageException;
+	public int getRessourceBytesSize() throws FopImageException;
+	// return null if no corresponding PDFFilter
+	public PDFFilter getPDFFilter() throws FopImageException;
+
+	// release memory
+	public void close();
 }
