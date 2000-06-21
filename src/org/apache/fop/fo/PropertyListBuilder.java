ef237bc7a3f13a201b3e694db79a04ae29f8279c
Jordan Naftolin <jordan@apache.org>
2000-06-21 01:22:09 +0000

    Added internal-destination entry
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193412 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/PropertyListBuilder.java
+++ src/org/apache/fop/fo/PropertyListBuilder.java
@@ -110,6 +110,7 @@ public class PropertyListBuilder {
 	propertyTable.put("padding-left",PaddingLeft.maker());
 	propertyTable.put("padding-right",PaddingRight.maker());
 	propertyTable.put("external-destination",ExternalDestination.maker());
+        propertyTable.put("internal-destination",InternalDestination.maker());
 	propertyTable.put("height",SVGLength.maker());
 	propertyTable.put("width",SVGLength.maker());
 	propertyTable.put("x",SVGLength.maker());
