c7d3398ac095e15f138505bab7395d7e342cf53e
fotis <fotis@apache.org>
2000-06-23 13:16:43 +0000

    add: xml files with the fop documentation (mirrors files from xml-site) and an ant script to write all into one pdf file
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193436 13f79535-47bb-0310-9956-ffa450edef68

---
+++ docs/xml-docs/makedoc.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo Building Fop documentation (pdf)
+echo ----------------
+
+if "%JAVA_HOME%" == "" goto error
+
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;..\..\lib\ant.jar;..\..\lib\xml.jar;..\..\lib;..\..\build\fop.jar
+set ANT_HOME=.\lib
+
+echo Building Fop documentation (pdf) ...
+
+%JAVA_HOME%\bin\java.exe -Dant.home=%ANT_HOME% -classpath %LOCALCLASSPATH%;%CLASSPATH% org.apache.tools.ant.Main %1 %2 %3 %4 %5
+
+goto end
+
+:error
+
+echo ERROR: JAVA_HOME not found in your environment.
+echo Please, set the JAVA_HOME variable in your environment to match the
+echo location of the Java Virtual Machine you want to use.
+
+:end
+
+rem set LOCALCLASSPATH=
+
