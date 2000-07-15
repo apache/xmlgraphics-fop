3ff3ad617bd9a888e0ef7f14c093c9aadeb3a4a3
fotis <fotis@apache.org>
2000-07-15 20:39:49 +0000

    adds method hasAttribute and hasAttributeNS to make class DOM 2 conform
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193510 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/dom/svg/SVGElementImpl.java
+++ src/org/apache/fop/dom/svg/SVGElementImpl.java
@@ -1,36 +1,36 @@
-/*-- $Id$ -- 
+/*-- $Id$ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
     Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "FOP" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,12 +41,12 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- James Tauber <jtauber@jtauber.com>. For more  information on the Apache 
+ James Tauber <jtauber@jtauber.com>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
 package org.apache.fop.dom.svg;
 
@@ -120,7 +120,7 @@ class ElementImpl implements Element {
 		return null;
 	}
 
-    public Node insertBefore(Node newChild, 
+    public Node insertBefore(Node newChild,
                                      Node refChild)
                                      throws DOMException
 	{
@@ -154,7 +154,7 @@ class ElementImpl implements Element {
 	{
 	}
 
-    public boolean supports(String feature, 
+    public boolean supports(String feature,
                                  String version)
 	{
 		return false;
@@ -237,26 +237,26 @@ class ElementImpl implements Element {
 		return null;
 	}
 
-    public String getAttributeNS(String namespaceURI, 
+    public String getAttributeNS(String namespaceURI,
                                        String localName)
 	{
 		return null;
 	}
 
-    public void setAttributeNS(String namespaceURI, 
-                                       String qualifiedName, 
+    public void setAttributeNS(String namespaceURI,
+                                       String qualifiedName,
                                        String value)
                                        throws DOMException
 	{
 	}
 
-    public void removeAttributeNS(String namespaceURI, 
+    public void removeAttributeNS(String namespaceURI,
                                           String localName)
                                           throws DOMException
 	{
 	}
 
-    public Attr getAttributeNodeNS(String namespaceURI, 
+    public Attr getAttributeNodeNS(String namespaceURI,
                                            String localName)
 	{
 		return null;
@@ -268,11 +268,22 @@ class ElementImpl implements Element {
 		return null;
 	}
 
-    public NodeList getElementsByTagNameNS(String namespaceURI, 
+    public NodeList getElementsByTagNameNS(String namespaceURI,
                                                String localName)
 	{
 		return null;
 	}
+
+    public boolean hasAttributeNS (String namespaceURI,
+                                   String localName)
+  {
+    return false;
+  }
+
+    public boolean hasAttribute (String name)
+  {
+    return false;
+  }
 
 }
 
