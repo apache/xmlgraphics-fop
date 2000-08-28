a29465175a4ed8502ec4b41e9534f88b3d553955
Keiron Liddle <keiron@apache.org>
2000-08-28 23:56:44 +0000

    returns the proper type of data
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193675 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/datatypes/StyleData.java
+++ src/org/apache/fop/datatypes/StyleData.java
@@ -54,6 +54,9 @@ import org.apache.fop.fo.Property;
 import org.apache.fop.messaging.MessageHandler;
 
 import org.apache.fop.dom.svg.*;
+import org.apache.fop.dom.css.*;
+
+import org.w3c.dom.css.*;
 
 import java.util.*;
 /**
@@ -62,141 +65,17 @@ import java.util.*;
  * @author Keiron Liddle <keiron@aftexsw.com>
  */
 public class StyleData {
-	Hashtable table = new Hashtable();
+	CSSStyleDeclaration styleDec = new CSSStyleDeclarationImpl();
 
 	/**
 	 * set the StyleData given a particular String specifying StyleData and units
 	 */
-	public StyleData (String len) {
-		try {
-		convert(len);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public StyleData (String str) {
+	    styleDec.setCssText(str);
 	}
 
-	protected void convert(String len) {
-		StringTokenizer st = new StringTokenizer(len, ";");
-		while(st.hasMoreTokens()) {
-			String str = st.nextToken();
-			int pos;
-			pos = str.indexOf(":");
-			if(pos != -1) {
-				String type = str.substring(0, pos).trim();
-				String value = str.substring(pos + 1, str.length()).trim();
-				if(type.equals("stroke-width")) {
-					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("stroke")) {
-					if(value.startsWith("url(")) {
-						table.put(type, new String(value));
-					} else if(!value.equals("none")) {
-						table.put(type, new ColorType(value));
-					} else if(value.equals("none")) {
-						table.put(type, value);
-					}
-				} else if(type.equals("color")) {
-//					if(!value.equals("none"))
-//						table.put("stroke", new ColorType(value));//??
-					table.put(type, new ColorType(value));//??
-				} else if(type.equals("stroke-linecap")) {
-					table.put(type, value);
-				} else if(type.equals("stroke-linejoin")) {
-					table.put(type, value);
-				} else if(type.equals("stroke-miterlimit")) {
-					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("stroke-dasharray")) {
-					// array of space or comma separated numbers
-					if(!value.equals("none")) {
-						Vector list = new Vector();
-						StringTokenizer array = new StringTokenizer(value, " ,");
-						while(array.hasMoreTokens()) {
-							String intstr = array.nextToken();
-							list.addElement(new Integer(Integer.parseInt(intstr)));
-						}
-						table.put(type, list);
-					}
-					// else leave ??
-				} else if(type.equals("stroke-dashoffset")) {
-					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("stroke-opacity")) {
-				} else if(type.equals("fill")) {
-					if(value.startsWith("url(")) {
-						table.put(type, new String(value));
-					} else if(!value.equals("none")) {
-						table.put(type, new ColorType(value));
-					} else {
-						table.put(type, value);
-					}
-				} else if(type.equals("fill-rule")) {
-					// nonzero
-					table.put(type, value);
-				} else if(type.equals("font")) {
-					table.put(type, value);
-				} else if(type.equals("font-size")) {
-					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("font-family")) {
-					table.put(type, value);
-				} else if(type.equals("font-weight")) {
-					table.put(type, value);
-				} else if(type.equals("font-style")) {
-					table.put(type, value);
-				} else if(type.equals("font-variant")) {
-					table.put(type, value);
-				} else if(type.equals("font-stretch")) {
-					table.put(type, value);
-				} else if(type.equals("font-size-adjust")) {
-					table.put(type, value);
-				} else if(type.equals("letter-spacing")) {
-					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("word-spacing")) {
-					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("text-decoration")) {
-					table.put(type, value);
-				} else if(type.equals("mask")) {
-					if(value.startsWith("url(")) {
-						value = value.substring(4, value.length() - 1);
-					}
-					table.put(type, value);
-				} else if(type.equals("fill-opacity")) {
-//					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("opacity")) {
-//					table.put(type, new SVGLengthImpl(value));
-				} else if(type.equals("filter")) {
-//					table.put(type, new Filter(value));
-				} else if(type.equals("stop-color")) {
-					table.put(type, new ColorType(value));
-				} else if(type.equals("marker-start")) {
-//					table.put(type, new URLType(value));
-				} else if(type.equals("marker-mid")) {
-//					table.put(type, new URLType(value));
-				} else if(type.equals("marker-end")) {
-//					table.put(type, new URLType(value));
-				} else if(type.equals("text-antialiasing")) {
-//					boolean
-				} else if(type.equals("stroke-antialiasing")) {
-//					boolean
-				} else if(type.equals("writing-mode")) {
-				} else if(type.equals("glyph-orientation-vertical")) {
-				} else if(type.equals("glyph-orientation-horizontal")) {
-				} else if(type.equals("direction")) {
-				} else if(type.equals("unicode-bidi")) {
-				} else if(type.equals("text-anchor")) {
-					table.put(type, value);
-				} else if(type.equals("dominant-baseline")) {
-				} else if(type.equals("baseline-identifier")) {
-				} else if(type.equals("baseline-shift")) {
-				} else if(type.equals("font-size-adjust")) {
-				} else {
-					MessageHandler.logln("WARNING: Unknown style element : " + type);
-				}
-			} else {
-				MessageHandler.errorln("ERROR: Invalid style element " + str);
-			}
-		}
-	}
-
-	public Hashtable getStyle()
+	public CSSStyleDeclaration getStyle()
 	{
-		return table;
+		return styleDec;
 	}
 }
