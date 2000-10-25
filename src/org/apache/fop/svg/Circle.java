ac37c285a285d089136bab5519b87ca42ee221c0
Keiron Liddle <keiron@apache.org>
2000-10-25 01:48:44 +0000

    changed the SVGLengthProperty so that it is possible to tell if the value is not present, needed for gradients
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193736 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/svg/Circle.java
+++ src/org/apache/fop/svg/Circle.java
@@ -1,36 +1,36 @@
-/*-- $Id$ -- 
+/*-- $Id$ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
     Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "FOP" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,12 +41,12 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- James Tauber <jtauber@jtauber.com>. For more  information on the Apache 
+ James Tauber <jtauber@jtauber.com>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
 
 package org.apache.fop.svg;
@@ -71,58 +71,69 @@ import org.w3c.dom.svg.SVGElement;
  */
 public class Circle extends SVGObj {
 
-	/**
-	 * inner class for making Circle objects.
-	 */
-	public static class Maker extends FObj.Maker {
+    /**
+     * inner class for making Circle objects.
+     */
+    public static class Maker extends FObj.Maker {
 
-		/**
-		 * make a Circle object.
-		 *
-		 * @param parent the parent formatting object
-		 * @param propertyList the explicit properties of this object
-		 *
-		 * @return the Circle object
-		 */
-		public FObj make(FObj parent, PropertyList propertyList) throws FOPException
-		{
-			return new Circle(parent, propertyList);
-		}
-	}
+        /**
+         * make a Circle object.
+         *
+         * @param parent the parent formatting object
+         * @param propertyList the explicit properties of this object
+         *
+         * @return the Circle object
+         */
+        public FObj make(FObj parent,
+                         PropertyList propertyList) throws FOPException {
+            return new Circle(parent, propertyList);
+        }
+    }
 
-	/**
-	 * returns the maker for this object.
-	 *
-	 * @return the maker for Circle objects
-	 */
-	public static FObj.Maker maker() {
-		return new Circle.Maker();
-	}
+    /**
+     * returns the maker for this object.
+     *
+     * @return the maker for Circle objects
+     */
+    public static FObj.Maker maker() {
+        return new Circle.Maker();
+    }
 
-	/**
-	 * constructs a Circle object (called by Maker).
-	 *
-	 * @param parent the parent formatting object
-	 * @param propertyList the explicit properties of this object
-	 */
-	protected Circle(FObj parent, PropertyList propertyList) {
-		super(parent, propertyList);
-		this.name = "svg:circle";
-	}
+    /**
+     * constructs a Circle object (called by Maker).
+     *
+     * @param parent the parent formatting object
+     * @param propertyList the explicit properties of this object
+     */
+    protected Circle(FObj parent, PropertyList propertyList) {
+        super(parent, propertyList);
+        this.name = "svg:circle";
+    }
 
-	public SVGElement createGraphic()
-	{
-		/* retrieve properties */
-		SVGLength cx = ((SVGLengthProperty)this.properties.get("cx")).getSVGLength();
-		SVGLength cy = ((SVGLengthProperty)this.properties.get("cy")).getSVGLength();
-		SVGLength r = ((SVGLengthProperty)this.properties.get("r")).getSVGLength();
-		SVGCircleElementImpl graph = new SVGCircleElementImpl();
-		graph.setCx(new SVGAnimatedLengthImpl(cx));
-		graph.setCy(new SVGAnimatedLengthImpl(cy));
-		graph.setR(new SVGAnimatedLengthImpl(r));
-		graph.setStyle(((SVGStyle)this.properties.get("style")).getStyle());
-		graph.setTransform(((SVGTransform)this.properties.get("transform")).getTransform());
-		graph.setId(this.properties.get("id").getString());
-		return graph;
-	}
+    public SVGElement createGraphic() {
+        /* retrieve properties */
+        SVGLength cx = ((SVGLengthProperty) this.properties.get("cx")).
+                       getSVGLength();
+        if (cx == null)
+            cx = new SVGLengthImpl();
+        SVGLength cy = ((SVGLengthProperty) this.properties.get("cy")).
+                       getSVGLength();
+        if (cy == null)
+            cy = new SVGLengthImpl();
+        SVGLength r = ((SVGLengthProperty) this.properties.get("r")).
+                      getSVGLength();
+        if (r == null)
+            r = new SVGLengthImpl();
+        SVGCircleElementImpl graph = new SVGCircleElementImpl();
+        graph.setCx(new SVGAnimatedLengthImpl(cx));
+        graph.setCy(new SVGAnimatedLengthImpl(cy));
+        graph.setR(new SVGAnimatedLengthImpl(r));
+        graph.setStyle(
+          ((SVGStyle) this.properties.get("style")).getStyle());
+        graph.setTransform(
+          ((SVGTransform) this.properties.get("transform")).
+          getTransform());
+        graph.setId(this.properties.get("id").getString());
+        return graph;
+    }
 }
