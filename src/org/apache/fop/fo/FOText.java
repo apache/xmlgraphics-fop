874bfa666db33cd746486b1c67e9da1593d4d34c
fotis <fotis@apache.org>
2000-11-15 21:44:16 +0000

    renames white-space-treatment to white-space-collapse
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193812 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/FOText.java
+++ src/org/apache/fop/fo/FOText.java
@@ -1,36 +1,36 @@
-/*-- $Id$ -- 
+/*-- $Id$ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
     Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "FOP" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,12 +41,12 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- James Tauber <jtauber@jtauber.com>. For more  information on the Apache 
+ James Tauber <jtauber@jtauber.com>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
 
 package org.apache.fop.fo;
@@ -74,7 +74,7 @@ public class FOText extends FONode {
     float green;
     float blue;
     int wrapOption;
-    int whiteSpaceTreatment;
+    int whiteSpaceCollapse;
 
     // Textdecoration
     protected boolean underlined = false;
@@ -83,12 +83,12 @@ public class FOText extends FONode {
 
 
     public FOText(char[] chars, int s, int e, FObj parent) {
-	super(parent);
-	this.start = 0;
-	this.ca = new char[e - s];
-	for (int i = s; i < e; i++)
-	    this.ca[i - s] = chars[i];
-	this.length = e - s;
+        super(parent);
+        this.start = 0;
+        this.ca = new char[e - s];
+        for (int i = s; i < e; i++)
+            this.ca[i - s] = chars[i];
+        this.length = e - s;
     }
 
     public void setUnderlined(boolean ul) {
@@ -96,50 +96,48 @@ public class FOText extends FONode {
     }
 
     public Status layout(Area area) throws FOPException {
-	if (!(area instanceof BlockArea)) {
-	    MessageHandler.errorln("WARNING: text outside block area" + new String(ca, start, length));
-	    return new Status(Status.OK);
-	}
-	if (this.marker == START) {
-	    String fontFamily =
-		this.parent.properties.get("font-family").getString(); 
-	    String fontStyle =
-		this.parent.properties.get("font-style").getString(); 
-	    String fontWeight =
-		this.parent.properties.get("font-weight").getString(); 
-	    int fontSize =
-		this.parent.properties.get("font-size").getLength().mvalue(); 
-	    
-	    this.fs = new FontState(area.getFontInfo(), fontFamily, fontStyle,
-				    fontWeight, fontSize); 
-	    
-	    ColorType c =
-		this.parent.properties.get("color").getColorType();
-	    this.red = c.red();
-	    this.green = c.green();
-	    this.blue = c.blue();
-	    
-	    this.wrapOption =
-		this.parent.properties.get("wrap-option").getEnum(); 
-	    this.whiteSpaceTreatment =
-		this.parent.properties.get("white-space-treatment").getEnum();
+        if (!(area instanceof BlockArea)) {
+            MessageHandler.errorln("WARNING: text outside block area" +
+                                   new String(ca, start, length));
+            return new Status(Status.OK);
+        }
+        if (this.marker == START) {
+            String fontFamily =
+              this.parent.properties.get("font-family").getString();
+            String fontStyle =
+              this.parent.properties.get("font-style").getString();
+            String fontWeight =
+              this.parent.properties.get("font-weight").getString();
+            int fontSize = this.parent.properties.get(
+                             "font-size").getLength().mvalue();
 
-	    this.marker = this.start;
-	}
-	int orig_start = this.marker;
-	this.marker = ((BlockArea) area).addText(fs, red, green, blue,
-						 wrapOption,
-						 this.getLinkSet(),
-						 whiteSpaceTreatment,
-						 ca, this.marker, length,
-						 underlined);
-	if (this.marker == -1) {
-	    this.marker = 0;
-	    return new Status(Status.OK);
-	} else if (this.marker != orig_start) {
-	    return new Status(Status.AREA_FULL_SOME);
-	} else {
-	    return new Status(Status.AREA_FULL_NONE);
-	}
+            this.fs = new FontState(area.getFontInfo(), fontFamily,
+                                    fontStyle, fontWeight, fontSize);
+
+            ColorType c =
+              this.parent.properties.get("color").getColorType();
+            this.red = c.red();
+            this.green = c.green();
+            this.blue = c.blue();
+
+            this.wrapOption =
+              this.parent.properties.get("wrap-option").getEnum();
+            this.whiteSpaceCollapse = this.parent.properties.get(
+                                        "white-space-collapse").getEnum();
+
+            this.marker = this.start;
+        }
+        int orig_start = this.marker;
+        this.marker = ((BlockArea) area).addText(fs, red, green, blue,
+                      wrapOption, this.getLinkSet(), whiteSpaceCollapse, ca,
+                      this.marker, length, underlined);
+        if (this.marker == -1) {
+            this.marker = 0;
+            return new Status(Status.OK);
+        } else if (this.marker != orig_start) {
+            return new Status(Status.AREA_FULL_SOME);
+        } else {
+            return new Status(Status.AREA_FULL_NONE);
+        }
     }
 }
--- src/org/apache/fop/fo/flow/PageNumber.java
+++ src/org/apache/fop/fo/flow/PageNumber.java
@@ -1,36 +1,36 @@
-/*-- $Id$ -- 
+/*-- $Id$ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
     Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "FOP" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,12 +41,12 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- James Tauber <jtauber@jtauber.com>. For more  information on the Apache 
+ James Tauber <jtauber@jtauber.com>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
 
 package org.apache.fop.fo.flow;
@@ -65,14 +65,14 @@ import java.util.Enumeration;
 public class PageNumber extends FObj {
 
     public static class Maker extends FObj.Maker {
-	public FObj make(FObj parent, PropertyList propertyList)
-	    throws FOPException {
-	    return new PageNumber(parent, propertyList);
-	}
+        public FObj make(FObj parent,
+                         PropertyList propertyList) throws FOPException {
+            return new PageNumber(parent, propertyList);
+        }
     }
 
     public static FObj.Maker maker() {
-	return new PageNumber.Maker();
+        return new PageNumber.Maker();
     }
 
     FontState fs;
@@ -80,50 +80,51 @@ public class PageNumber extends FObj {
     float green;
     float blue;
     int wrapOption;
-    int whiteSpaceTreatment;
+    int whiteSpaceCollapse;
 
     public PageNumber(FObj parent, PropertyList propertyList) {
-	super(parent, propertyList);
-	this.name = "fo:page-number";
+        super(parent, propertyList);
+        this.name = "fo:page-number";
     }
 
     public Status layout(Area area) throws FOPException {
-	if (!(area instanceof BlockArea)) {
-	    MessageHandler.errorln("WARNING: page-number outside block area");
-	    return new Status(Status.OK);
-	}
-	if (this.marker == START) {
-	    String fontFamily = this.properties.get("font-family").getString();
-	    String fontStyle = this.properties.get("font-style").getString();
-	    String fontWeight = this.properties.get("font-weight").getString();
-	    int fontSize = this.properties.get("font-size").getLength().mvalue();
-		
-	    this.fs = new FontState(area.getFontInfo(), fontFamily,
-				     fontStyle, fontWeight, fontSize);
+        if (!(area instanceof BlockArea)) {
+            MessageHandler.errorln("WARNING: page-number outside block area");
+            return new Status(Status.OK);
+        }
+        if (this.marker == START) {
+            String fontFamily =
+              this.properties.get("font-family").getString();
+            String fontStyle =
+              this.properties.get("font-style").getString();
+            String fontWeight =
+              this.properties.get("font-weight").getString();
+            int fontSize =
+              this.properties.get("font-size").getLength().mvalue();
 
-	    ColorType c = this.properties.get("color").getColorType();
-	    this.red = c.red();
-	    this.green = c.green();
-	    this.blue = c.blue();
+            this.fs = new FontState(area.getFontInfo(), fontFamily,
+                                    fontStyle, fontWeight, fontSize);
 
-	    this.wrapOption = this.properties.get("wrap-option").getEnum();
-	    this.whiteSpaceTreatment = this.properties.get("white-space-treatment").getEnum();
-	    
-	    this.marker = 0;
+            ColorType c = this.properties.get("color").getColorType();
+            this.red = c.red();
+            this.green = c.green();
+            this.blue = c.blue();
 
-            // initialize id                       
-            String id = this.properties.get("id").getString();            
-            area.getIDReferences().initializeID(id,area);                        
-	}
-        
-	String p = Integer.toString(area.getPage().getNumber());
-	this.marker = ((BlockArea) area).addText(fs, red, green, blue,
-						 wrapOption,
-						 null,
-						 whiteSpaceTreatment,
-						 p.toCharArray(), 0,
-						 p.length(),
-						 false);
-	return new Status(Status.OK);
+            this.wrapOption = this.properties.get("wrap-option").getEnum();
+            this.whiteSpaceCollapse =
+              this.properties.get("white-space-collapse").getEnum();
+
+            this.marker = 0;
+
+            // initialize id
+            String id = this.properties.get("id").getString();
+            area.getIDReferences().initializeID(id, area);
+        }
+
+        String p = Integer.toString(area.getPage().getNumber());
+        this.marker = ((BlockArea) area).addText(fs, red, green, blue,
+                      wrapOption, null, whiteSpaceCollapse, p.toCharArray(),
+                      0, p.length(), false);
+        return new Status(Status.OK);
     }
 }
