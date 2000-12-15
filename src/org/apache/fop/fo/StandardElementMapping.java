78b7343b0c374203a87d85460c62378166417681
fotis <fotis@apache.org>
2000-12-15 21:35:36 +0000

    adds support for fo:character and property character
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@193874 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/StandardElementMapping.java
+++ src/org/apache/fop/fo/StandardElementMapping.java
@@ -1,36 +1,36 @@
-/*-- $Id$ -- 
+/*-- $Id$ --
 
  ============================================================================
                    The Apache Software License, Version 1.1
  ============================================================================
- 
+
     Copyright (C) 1999 The Apache Software Foundation. All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modifica-
  tion, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of  source code must  retain the above copyright  notice,
     this list of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
- 
+
  3. The end-user documentation included with the redistribution, if any, must
     include  the following  acknowledgment:  "This product includes  software
     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
     Alternately, this  acknowledgment may  appear in the software itself,  if
     and wherever such third-party acknowledgments normally appear.
- 
+
  4. The names "FOP" and  "Apache Software Foundation"  must not be used to
     endorse  or promote  products derived  from this  software without  prior
     written permission. For written permission, please contact
     apache@apache.org.
- 
+
  5. Products  derived from this software may not  be called "Apache", nor may
     "Apache" appear  in their name,  without prior written permission  of the
     Apache Software Foundation.
- 
+
  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
@@ -41,12 +41,12 @@
  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  This software  consists of voluntary contributions made  by many individuals
  on  behalf of the Apache Software  Foundation and was  originally created by
- James Tauber <jtauber@jtauber.com>. For more  information on the Apache 
+ James Tauber <jtauber@jtauber.com>. For more  information on the Apache
  Software Foundation, please see <http://www.apache.org/>.
- 
+
  */
 
 package org.apache.fop.fo;
@@ -60,41 +60,41 @@ public class StandardElementMapping implements ElementMapping {
 
 	String uri = "http://www.w3.org/1999/XSL/Format";
 
-	builder.addMapping(uri, "root", Root.maker()); 
+	builder.addMapping(uri, "root", Root.maker());
 	builder.addMapping(uri, "layout-master-set",
-			   LayoutMasterSet.maker()); 
+			   LayoutMasterSet.maker());
 	builder.addMapping(uri, "simple-page-master",
-			   SimplePageMaster.maker()); 
-	builder.addMapping(uri, "region-body", RegionBody.maker()); 
-	builder.addMapping(uri, "region-before", RegionBefore.maker()); 
-	builder.addMapping(uri, "region-after", RegionAfter.maker()); 
-	builder.addMapping(uri, "page-sequence", PageSequence.maker()); 
+			   SimplePageMaster.maker());
+	builder.addMapping(uri, "region-body", RegionBody.maker());
+	builder.addMapping(uri, "region-before", RegionBefore.maker());
+	builder.addMapping(uri, "region-after", RegionAfter.maker());
+	builder.addMapping(uri, "page-sequence", PageSequence.maker());
 	builder.addMapping(uri, "page-sequence-master",
-			   PageSequenceMaster.maker()); 
+			   PageSequenceMaster.maker());
 	builder.addMapping(uri, "single-page-master-reference",
-			   SinglePageMasterReference.maker()); 
+			   SinglePageMasterReference.maker());
 	builder.addMapping(uri, "repeatable-page-master-reference",
-			   RepeatablePageMasterReference.maker()); 
+			   RepeatablePageMasterReference.maker());
 	builder.addMapping(uri, "conditional-page-master-reference",
-			   ConditionalPageMasterReference.maker()); 
+			   ConditionalPageMasterReference.maker());
 	builder.addMapping(uri, "repeatable-page-master-alternatives",
-			   RepeatablePageMasterAlternatives.maker()); 
-	builder.addMapping(uri, "flow", Flow.maker()); 
+			   RepeatablePageMasterAlternatives.maker());
+	builder.addMapping(uri, "flow", Flow.maker());
 	builder.addMapping(uri, "static-content",
 			   StaticContent.maker());
-	builder.addMapping(uri, "block", Block.maker()); 
-	builder.addMapping(uri, "block-container", BlockContainer.maker()); 
+	builder.addMapping(uri, "block", Block.maker());
+	builder.addMapping(uri, "block-container", BlockContainer.maker());
 	builder.addMapping(uri, "list-block", ListBlock.maker());
 	builder.addMapping(uri, "list-item", ListItem.maker());
 	builder.addMapping(uri, "list-item-label",
-			   ListItemLabel.maker()); 
+			   ListItemLabel.maker());
 	builder.addMapping(uri, "list-item-body", ListItemBody.maker());
 	builder.addMapping(uri, "page-number", PageNumber.maker());
 	builder.addMapping(uri, "page-number-citation", PageNumberCitation.maker());
 	builder.addMapping(uri, "display-sequence",
-			   DisplaySequence.maker()); 
+			   DisplaySequence.maker());
 	builder.addMapping(uri, "inline",
-			   Inline.maker()); 
+			   Inline.maker());
   builder.addMapping(uri, "external-graphic",
                            ExternalGraphic.maker());
 	builder.addMapping(uri, "table", Table.maker());
@@ -107,5 +107,6 @@ public class StandardElementMapping implements ElementMapping {
 	builder.addMapping(uri, "basic-link", BasicLink.maker());
 	builder.addMapping(uri, "instream-foreign-object", InstreamForeignObject.maker());
 	builder.addMapping(uri, "leader", Leader.maker());
+	builder.addMapping(uri, "character", org.apache.fop.fo.flow.Character.maker());
     }
 }
