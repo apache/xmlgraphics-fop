673317bbb1e805e24157594f3b8dfd5a6c92e160
arved <arved@apache.org>
2001-02-14 11:38:20 +0000

    No longer required
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194072 13f79535-47bb-0310-9956-ffa450edef68

--- jpfop-0.17.0/makesample_jpfop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-XERCES=../lib/xerces.jar
-XALAN=../lib/xalan.jar
-BSF=../lib/bsf.jar
-
-$JAVA_HOME/bin/java -cp ../build/fop-0.16.0-jp.jar:$XERCES:$XALAN:$BSF:../lib/w3c.jar org.apache.fop.apps.CommandLine -d sample_jpfop.fo sample_jpfop.pdf
-
