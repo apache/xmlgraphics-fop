f2f52e413f05f4426611d80c016522c4874769a6
arved <arved@apache.org>
2001-03-13 02:43:51 +0000

    Added license
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194154 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/render/PrintRenderer.java
+++ src/org/apache/fop/render/PrintRenderer.java
@@ -1,3 +1,9 @@
+/* $Id$
+ * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources."
+ */
+
 // PrintRenderer is an abstract base class for renderers that produce printed type output.
 // Subclasses would be PDFRenderer, PCLRenderer and similar renderers.
 
--- src/org/apache/fop/render/pcl/PCLRenderer.java
+++ src/org/apache/fop/render/pcl/PCLRenderer.java
@@ -1,3 +1,9 @@
+/* $Id$
+ * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources."
+ */
+
 //package com.eastpoint.chrysalis;
 package org.apache.fop.render.pcl;
 
