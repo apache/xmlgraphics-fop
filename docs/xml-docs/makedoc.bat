b9b97f6b53e137e980ee0b85ac1bdf7906565bd5
Kelly Campbell <kellyc@apache.org>
2001-03-28 05:21:16 +0000

    Fixed documentation build. Stylebook requires Xalan 1. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194177 13f79535-47bb-0310-9956-ffa450edef68

--- docs/xml-docs/makedoc.bat
+++ docs/xml-docs/makedoc.bat
@@ -8,15 +8,14 @@ echo ----------------
 if "%JAVA_HOME%" == "" goto error
 
 set LIBDIR=..\..\lib
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant.jar;%LIBDIR%\w3c.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xerces-1.2.3.jar;%LIBDIR%\xalan-2.0.0.jar;%LIBDIR%\xalanj1compat.jar;%LIBDIR%\bsf.jar;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\stylebook.jar;%LIBDIR%\..\build\fop.jar
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant.jar;%LIBDIR%\w3c.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xerces-1.2.3.jar;%LIBDIR%\xalan-1.2.2.jar;%LIBDIR%\bsf.jar;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\stylebook.jar;%LIBDIR%\..\build\fop.jar
 set ANT_HOME=%LIBDIR%
 
 
 %JAVA_HOME%\bin\java.exe -Dant.home=%ANT_HOME% -classpath %LOCALCLASSPATH%;%CLASSPATH% org.apache.tools.ant.Main %1 %2 %3 %4 %5
 
-echo sorry, production of html documentation is broken at the moment
 
-rem %JAVA_HOME%\bin\java.exe -classpath %LOCALCLASSPATH%;%CLASSPATH% org.apache.stylebook.StyleBook "targetDirectory=../html-docs/" fop.xml style/
+%JAVA_HOME%\bin\java.exe -classpath %LOCALCLASSPATH%;%CLASSPATH% org.apache.stylebook.StyleBook "targetDirectory=../html-docs/" fop.xml style/
 
 goto end
 
@@ -27,8 +26,4 @@ echo Please, set the JAVA_HOME variable in your environment to match the
 echo location of the Java Virtual Machine you want to use.
 
 :end
-
-rem set LOCALCLASSPATH=
-
-
 
