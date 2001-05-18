ac2ee9f42b1215418973993c94ec92371c6ac62a
Keiron Liddle <keiron@apache.org>
2001-05-18 09:56:05 +0000

    fixed classpath for batik jar
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194255 13f79535-47bb-0310-9956-ffa450edef68

--- test/createpdf.sh
+++ test/createpdf.sh
@@ -1,3 +1,3 @@
 # this can be used to create pdf output of the testsuite files
 
-$JAVA_HOME/bin/java -cp ../build/fop.jar:../lib/w3c.jar:../lib/xalan-2.0.0.jar:../lib/xerces-1.2.3.jar:../lib/jimi-1.0.jar org.apache.fop.tools.TestConverter -pdf -b ./ basictests.xml
+$JAVA_HOME/bin/java -cp ../build/fop.jar:../lib/batik.jar:../lib/xalan-2.0.0.jar:../lib/xerces-1.2.3.jar:../lib/jimi-1.0.jar org.apache.fop.tools.TestConverter -pdf -b ./ basictests.xml
