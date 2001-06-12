ce1d9f4c04262abcc17c9c73d0b6437803f13ca8
Keiron Liddle <keiron@apache.org>
2001-06-12 11:38:05 +0000

    changed to small license
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194287 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/layout/PageMaster.java
+++ src/org/apache/fop/layout/PageMaster.java
@@ -1,8 +1,7 @@
-/*-- $Id$ --
- *
+/* $Id$
  * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the 
- * LICENSE file included with these sources."
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
  */
  
 package org.apache.fop.layout;
