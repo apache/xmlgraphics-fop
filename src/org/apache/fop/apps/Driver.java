b4aceba502b859ac073e509d5590f6dd4da5dc15
Keiron Liddle <keiron@apache.org>
2001-06-22 09:18:24 +0000

    command line support for new ps renderer also cleaned up code Submitted by:  Jeremias Maerki <jeremias.maerki@outline.ch>
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194311 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/apps/Driver.java
+++ src/org/apache/fop/apps/Driver.java
@@ -1,9 +1,8 @@
 /* $Id$
  * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the 
- * LICENSE file included with these sources."
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
  */
-
 
 package org.apache.fop.apps;
 
@@ -49,7 +48,7 @@ import java.io.*;
  * Here is an example use of Driver which outputs PDF:
  *
  * <PRE>
- *   Driver driver = new Driver(new InputSource (args[0]), 
+ *   Driver driver = new Driver(new InputSource (args[0]),
  *                              new FileOutputStream(args[1]));
  *   driver.setRenderer(RENDER_PDF);
  *   driver.run();
@@ -88,10 +87,10 @@ import java.io.*;
  * </PRE>
  */
 public class Driver {
-    
+
     /** Render to PDF. OutputStream must be set */
     public static final int RENDER_PDF = 1;
-    
+
     /** Render to a GUI window. No OutputStream neccessary */
     public static final int RENDER_AWT = 2;
 
@@ -100,15 +99,18 @@ public class Driver {
 
     /** Render to XML. OutputStream must be set */
     public static final int RENDER_XML = 4;
-   
+
     /** Render to PRINT. No OutputStream neccessary */
     public static final int RENDER_PRINT = 5;
 
     /** Render to PCL. OutputStream must be set */
     public static final int RENDER_PCL = 6;
 
+    /** Render to Postscript. OutputStream must be set */
+    public static final int RENDER_PS = 7;
+
     /** Render to Text. OutputStream must be set */
-    public static final int RENDER_TXT = 7;
+    public static final int RENDER_TXT = 8;
 
     /** the FO tree builder */
     private FOTreeBuilder _treeBuilder;
@@ -121,13 +123,13 @@ public class Driver {
 
     /** the source of the FO file */
     private InputSource _source;
-    
+
     /** the stream to use to output the results of the renderer */
     private OutputStream _stream;
 
     /** The XML parser to use when building the FO tree */
     private XMLReader _reader;
-    
+
     /** If true, full error stacks are reported */
     private boolean _errorDump = false;
 
@@ -136,36 +138,34 @@ public class Driver {
 
     /** create a new Driver */
     public Driver() {
- 	_stream = null;
-  	_bufferManager = new BufferManager();
- 	_treeBuilder = new FOTreeBuilder();
-  	_treeBuilder.setBufferManager(_bufferManager);
-	setupDefaultMappings();
+        _stream = null;
+        _bufferManager = new BufferManager();
+        _treeBuilder = new FOTreeBuilder();
+        _treeBuilder.setBufferManager(_bufferManager);
+        setupDefaultMappings();
     }
 
     public Driver(InputSource source, OutputStream stream) {
-	this();
-	_source = source;
-	_stream = stream;
+        this();
+        _source = source;
+        _stream = stream;
     }
 
     /**
-     * Resets the Driver so it can be reused. Property and element 
+     * Resets the Driver so it can be reused. Property and element
      * mappings are reset to defaults.
      * The output stream is cleared. The renderer is cleared.
      */
-    public synchronized void reset() 
-    {
-	_areaTree = null;
-	_treeBuilder.reset();
+    public synchronized void reset() {
+        _areaTree = null;
+        _treeBuilder.reset();
     }
-    
-    public boolean hasData()
-    {
-	return(_treeBuilder.hasData());
+
+    public boolean hasData() {
+        return(_treeBuilder.hasData());
     }
-    
-    /** 
+
+    /**
      * Set the error dump option
      * @param dump if true, full stacks will be reported to the error log
      */
@@ -184,85 +184,84 @@ public class Driver {
     }
 
     /**
-     * Set the source for the FO document. This can be a normal SAX 
+     * Set the source for the FO document. This can be a normal SAX
      * InputSource, or an DocumentInputSource containing a DOM document.
      * @see DocumentInputSource
      */
-    public void setInputSource(InputSource source) 
-    {
-	_source = source;
+    public void setInputSource(InputSource source) {
+        _source = source;
     }
-    
+
     /**
      * Sets the reader used when reading in the source. If not set,
      * this defaults to a basic SAX parser.
      */
-    public void setXMLReader(XMLReader reader) 
-    {
-	_reader = reader;
+    public void setXMLReader(XMLReader reader) {
+        _reader = reader;
     }
-    
+
 
     /**
      * Sets all the element and property list mappings to their default values.
      *
      */
-    public void setupDefaultMappings() 
-    {
-	addElementMapping("org.apache.fop.fo.StandardElementMapping");
-	addPropertyList  ("org.apache.fop.fo.StandardPropertyListMapping");
+    public void setupDefaultMappings() {
+        addElementMapping("org.apache.fop.fo.StandardElementMapping");
+        addPropertyList ("org.apache.fop.fo.StandardPropertyListMapping");
 
-	addElementMapping("org.apache.fop.svg.SVGElementMapping");
-	addPropertyList  ("org.apache.fop.svg.SVGPropertyListMapping");
+        addElementMapping("org.apache.fop.svg.SVGElementMapping");
+        addPropertyList ("org.apache.fop.svg.SVGPropertyListMapping");
 
         addElementMapping("org.apache.fop.extensions.ExtensionElementMapping");
-	addPropertyList  ("org.apache.fop.extensions.ExtensionPropertyListMapping");
+        addPropertyList ("org.apache.fop.extensions.ExtensionPropertyListMapping");
     }
 
     /**
-     * Set the rendering type to use. Must be one of 
+     * Set the rendering type to use. Must be one of
      * <ul>
      *   <li>RENDER_PDF
      *   <li>RENDER_AWT
      *   <li>RENDER_MIF
      *   <li>RENDER_XML
      *   <li>RENDER_PCL
+     *   <li>RENDER_PS
      *   <li>RENDER_TXT
      * </ul>
      * @param renderer the type of renderer to use
      */
-    public void setRenderer(int renderer) 
-	throws IllegalArgumentException
-    {
-	switch (renderer) {
-	case RENDER_PDF:
-	    setRenderer(new org.apache.fop.render.pdf.PDFRenderer());
-	    break;
-	case RENDER_AWT:
-	    throw new IllegalArgumentException("Use renderer form of setRenderer() for AWT");
-	case RENDER_PRINT:
-	    throw new IllegalArgumentException("Use renderer form of setRenderer() for PRINT");
-    case RENDER_PCL:
-        setRenderer(new org.apache.fop.render.pcl.PCLRenderer());
-        break;
-    case RENDER_TXT:
-        setRenderer(new org.apache.fop.render.txt.TXTRenderer());
-		break;
-	case RENDER_MIF:
-	    setRenderer(new org.apache.fop.render.mif.MIFRenderer());
-	    break;
-	case RENDER_XML:
-	    setRenderer(new org.apache.fop.render.xml.XMLRenderer());
-	    break;
-	default:
-	    throw new IllegalArgumentException("Unknown renderer type");
-	}
-	
-    }
-    
+    public void setRenderer(int renderer) throws IllegalArgumentException {
+        switch (renderer) {
+            case RENDER_PDF:
+                setRenderer(new org.apache.fop.render.pdf.PDFRenderer());
+                break;
+            case RENDER_AWT:
+                throw new IllegalArgumentException("Use renderer form of setRenderer() for AWT");
+            case RENDER_PRINT:
+                throw new IllegalArgumentException("Use renderer form of setRenderer() for PRINT");
+            case RENDER_PCL:
+                setRenderer(new org.apache.fop.render.pcl.PCLRenderer());
+                break;
+            case RENDER_PS:
+                setRenderer(new org.apache.fop.render.ps.PSRenderer());
+                break;
+            case RENDER_TXT:
+                setRenderer(new org.apache.fop.render.txt.TXTRenderer());
+                break;
+            case RENDER_MIF:
+                setRenderer(new org.apache.fop.render.mif.MIFRenderer());
+                break;
+            case RENDER_XML:
+                setRenderer(new org.apache.fop.render.xml.XMLRenderer());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown renderer type");
+        }
 
-    /** 
-     * Set the Renderer to use 
+    }
+
+
+    /**
+     * Set the Renderer to use
      * @param renderer the renderer instance to use
      */
     public void setRenderer(Renderer renderer) {
@@ -278,36 +277,35 @@ public class Driver {
      * @see #setRenderer(int)
      * @see #setRenderer(Renderer)
      */
-    public void setRenderer(String rendererClassName, String version) 
-    {
-	setRenderer(rendererClassName);
+    public void setRenderer(String rendererClassName, String version) {
+        setRenderer(rendererClassName);
     }
-    
+
     /**
      * Set the class name of the Renderer to use as well as the
      * producer string for those renderers that can make use of it.
-     * @param rendererClassName classname of the renderer to use such as 
+     * @param rendererClassName classname of the renderer to use such as
      * "org.apache.fop.render.pdf.PDFRenderer"
      * @exception IllegalArgumentException if the classname was invalid.
      * @see #setRenderer(int)
      */
-    public void setRenderer(String rendererClassName) 
-	throws IllegalArgumentException
-    {
-	try {
-	    _renderer = (Renderer) Class.forName(rendererClassName).newInstance();
-	    _renderer.setProducer(Version.getVersion());
-	}
-	catch (ClassNotFoundException e) {
-            throw new IllegalArgumentException("Could not find " + 
-					       rendererClassName);
+    public void setRenderer(String rendererClassName)
+    throws IllegalArgumentException {
+        try {
+            _renderer = (Renderer) Class.forName(
+                          rendererClassName).newInstance();
+            _renderer.setProducer(Version.getVersion());
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException("Could not find " +
+                                               rendererClassName);
         }
         catch (InstantiationException e) {
-            throw new IllegalArgumentException("Could not instantiate " +
-                                   rendererClassName);
+            throw new IllegalArgumentException(
+              "Could not instantiate " + rendererClassName);
         }
         catch (IllegalAccessException e) {
-            throw new IllegalArgumentException("Could not access " + rendererClassName);
+            throw new IllegalArgumentException("Could not access " +
+                                               rendererClassName);
         }
         catch (ClassCastException e) {
             throw new IllegalArgumentException(rendererClassName + " is not a renderer");
@@ -321,67 +319,68 @@ public class Driver {
      * @param mapping the element mappingto add
      */
     public void addElementMapping(ElementMapping mapping) {
-	mapping.addToBuilder(_treeBuilder);
+        mapping.addToBuilder(_treeBuilder);
     }
 
     /**
      * add the element mapping with the given class name
      */
-    public void addElementMapping(String mappingClassName) 
-	throws IllegalArgumentException
-    {
-	try {
+    public void addElementMapping(String mappingClassName)
+    throws IllegalArgumentException {
+        try {
             ElementMapping mapping = (ElementMapping) Class.forName(
-                     mappingClassName).newInstance();
-	    addElementMapping(mapping);
-	} catch (ClassNotFoundException e) {
-            throw new IllegalArgumentException("Could not find " + mappingClassName);
-	}
+                                       mappingClassName).newInstance();
+            addElementMapping(mapping);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException("Could not find " +
+                                               mappingClassName);
+        }
         catch (InstantiationException e) {
-            throw new IllegalArgumentException("Could not instantiate " +
-                                   mappingClassName);
-	}
+            throw new IllegalArgumentException(
+              "Could not instantiate " + mappingClassName);
+        }
         catch (IllegalAccessException e) {
-            throw new IllegalArgumentException("Could not access " + mappingClassName);
-	}
+            throw new IllegalArgumentException("Could not access " +
+                                               mappingClassName);
+        }
         catch (ClassCastException e) {
             throw new IllegalArgumentException(mappingClassName + " is not an ElementMapping");
-	}
+        }
     }
 
     /**
      * Add the PropertyListMapping.
      */
-    public void addPropertyList(PropertyListMapping mapping) 
-    {
-	mapping.addToBuilder(_treeBuilder);
+    public void addPropertyList(PropertyListMapping mapping) {
+        mapping.addToBuilder(_treeBuilder);
     }
-    
 
     /**
      * Add the PropertyListMapping with the given class name.
      */
-    public void addPropertyList(String listClassName) 
-	throws IllegalArgumentException
-    {
-	try {
-            PropertyListMapping mapping = (PropertyListMapping) Class.forName(
-                     listClassName).newInstance();
-	    addPropertyList(mapping);
-	    
-	} catch (ClassNotFoundException e) {
-            throw new IllegalArgumentException("Could not find " + listClassName);
-	}
+    public void addPropertyList(String listClassName)
+    throws IllegalArgumentException {
+        try {
+            PropertyListMapping mapping =
+              (PropertyListMapping) Class.forName(
+                listClassName).newInstance();
+            addPropertyList(mapping);
+
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException("Could not find " +
+                                               listClassName);
+        }
         catch (InstantiationException e) {
-            throw new IllegalArgumentException("Could not instantiate " +
-					       listClassName);
-	}
+            throw new IllegalArgumentException(
+              "Could not instantiate " + listClassName);
+        }
         catch (IllegalAccessException e) {
-            throw new IllegalArgumentException("Could not access " + listClassName);
-	}
+            throw new IllegalArgumentException("Could not access " +
+                                               listClassName);
+        }
         catch (ClassCastException e) {
             throw new IllegalArgumentException(listClassName + " is not an ElementMapping");
-	}
+        }
     }
 
     /**
@@ -400,9 +399,8 @@ public class Driver {
      * SAX InputSource
      */
     public synchronized void buildFOTree(XMLReader parser,
-                            InputSource source)
-	throws FOPException 
-    {
+                                         InputSource source)
+        throws FOPException {
 
         parser.setContentHandler(_treeBuilder);
         try {
@@ -422,21 +420,21 @@ public class Driver {
     /**
      * Build the formatting object tree using the given DOM Document
      */
-    public synchronized void buildFOTree(Document document) 
-	throws FOPException 
-    {
-	try {
-	    DocumentInputSource source = new DocumentInputSource(document);
-	    DocumentReader reader = new DocumentReader();
-	    reader.setContentHandler(_treeBuilder);
-	    reader.parse(source);
-	} catch (SAXException e) {
+    public synchronized void buildFOTree(Document document)
+        throws FOPException {
+        try {
+            DocumentInputSource source = new DocumentInputSource(document);
+            DocumentReader reader = new DocumentReader();
+            reader.setContentHandler(_treeBuilder);
+            reader.parse(source);
+        } catch (SAXException e) {
             throw new FOPException(e);
-	} catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new FOPException(e);
-	}
-	
-    } 
+        }
+
+    }
 
     /**
      * Dumps an error
@@ -448,29 +446,28 @@ public class Driver {
                 if (((SAXException) e).getException() != null) {
                     ((SAXException) e).getException().printStackTrace();
                 }
-            }
-            else if (e instanceof FOPException) {
+            } else if (e instanceof FOPException) {
                 e.printStackTrace();
                 if (((FOPException) e).getException() != null) {
                     ((FOPException) e).getException().printStackTrace();
                 }
-            }
-	    else {
+            } else {
                 e.printStackTrace();
             }
         }
     }
 
     /* Set up the system buffers */
-   
+
     public void setBufferFile(File bufferFile) {
-    	this._bufferManager.addBufferFile(bufferFile);
+        this._bufferManager.addBufferFile(bufferFile);
     }
-    
+
     /**
      * format the formatting object tree into an area tree
      */
-    public synchronized void format() throws FOPException {
+    public synchronized void format()
+        throws FOPException {
         FontInfo fontInfo = new FontInfo();
         _renderer.setupFontInfo(fontInfo);
 
@@ -483,7 +480,8 @@ public class Driver {
     /**
      * render the area tree to the output form
      */
-    public synchronized void render() throws IOException, FOPException {
+    public synchronized void render()
+        throws IOException, FOPException {
         _renderer.render(_areaTree, _stream);
     }
 
@@ -492,27 +490,25 @@ public class Driver {
      * inputsource and outputstream
      */
     public synchronized void run()
-	throws IOException, FOPException 
-    {
-	if (_renderer == null) {
-	    setRenderer(RENDER_PDF);
-	}
-	if (_source == null) {
-	    throw new FOPException("InputSource is not set.");
-	}
-	if (_reader == null) {
-	    if (!(_source instanceof DocumentInputSource)) {
-		_reader = ConfigurationReader.createParser();
-	    }
-	}
-	if (_source instanceof DocumentInputSource) {
-	    buildFOTree(((DocumentInputSource)_source).getDocument());
-	}
-	else {
-	    buildFOTree(_reader, _source);
-	}
-	format();
-	render();
+        throws IOException, FOPException {
+        if (_renderer == null) {
+            setRenderer(RENDER_PDF);
+        }
+        if (_source == null) {
+            throw new FOPException("InputSource is not set.");
+        }
+        if (_reader == null) {
+            if (!(_source instanceof DocumentInputSource)) {
+                _reader = ConfigurationReader.createParser();
+            }
+        }
+        if (_source instanceof DocumentInputSource) {
+            buildFOTree(((DocumentInputSource)_source).getDocument());
+        } else {
+            buildFOTree(_reader, _source);
+        }
+        format();
+        render();
     }
-    
+
 }
