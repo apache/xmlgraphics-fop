7c3c058593fd7fb5a01675dd1a32f95b878f9654
Keiron Liddle <keiron@apache.org>
2001-07-11 11:36:03 +0000

    indicates to be implemented status for properties no implemented
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194346 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/datatypes/ToBeImplementedProperty.java
@@ -0,0 +1,34 @@
+/* $Id$ --
+ * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
+ */
+
+package org.apache.fop.datatypes;
+
+import org.apache.fop.fo.*;
+import org.apache.fop.messaging.MessageHandler;
+
+public class ToBeImplementedProperty extends Property {
+
+    public static class Maker extends Property.Maker {
+
+        public Maker(String propName) {
+            super(propName);
+        }
+
+        public Property convertProperty(Property p,
+                                        PropertyList propertyList, FObj fo) {
+            if (p instanceof ToBeImplementedProperty)
+                return p;
+            ToBeImplementedProperty val =
+              new ToBeImplementedProperty(getPropName());
+            return val;
+        }
+    }
+
+    public ToBeImplementedProperty(String propName) {
+        MessageHandler.errorln("Warning: property - \"" + propName + "\" is not implemented yet.");
+    }
+
+}
