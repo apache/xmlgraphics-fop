dc22b820449a8d0593ce49c870059c28a3baff1b
Keiron Liddle <keiron@apache.org>
2001-07-13 12:36:18 +0000

    added all remaining elements currently are indicated as not implemented. removed old elements
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194353 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/FOTreeBuilder.java
+++ src/org/apache/fop/fo/FOTreeBuilder.java
@@ -1,7 +1,7 @@
 /* $Id$
  * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the 
- * LICENSE file included with these sources."
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
  */
 
 package org.apache.fop.fo;
@@ -52,62 +52,11 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
     protected FObj rootFObj = null;
 
     public BufferManager bufferManager;
-	
-	/**
+
+    /**
      * set of names of formatting objects encountered but unknown
      */
     protected Hashtable unknownFOs = new Hashtable();
-
-    // namespace implementation ideas pinched from John Cowan
-    //      protected static class NSMap {
-    //  	String prefix;
-    //  	String uri;
-    //  	int level;
-
-    //  	NSMap(String prefix, String uri, int level) {
-    //  	    this.prefix = prefix;
-    //  	    this.uri = uri;
-    //  	    this.level = level;
-    //  	}
-    //      }
-
-    //      protected int level = 0;
-    //      protected Stack namespaceStack = new Stack();
-
-    //      {
-    //  	namespaceStack.push(new NSMap("xml",
-    //  				      "http://www.w3.org/XML/1998/namespace",
-    //  				      -1));
-    //  	namespaceStack.push(new NSMap("", "", -1));
-    //      }
-
-    //      protected String findURI(String prefix) {
-    //  	for (int i = namespaceStack.size() - 1; i >= 0; i--) {
-    //  	    NSMap nsMap = (NSMap) (namespaceStack.elementAt(i));
-    //  	    if (prefix.equals(nsMap.prefix)) return nsMap.uri;
-    //  	}
-    //  	return null;
-    //      }
-
-    //      protected String mapName(String name)
-    //  	throws SAXException {
-    //  	int colon = name.indexOf(':');
-    //  	String prefix = "";
-    //  	String localPart = name;
-    //  	if (colon != -1) {
-    //  	    prefix = name.substring(0, colon);
-    //  	    localPart = name.substring(colon + 1);
-    //  	}
-    //  	String uri = findURI(prefix);
-    //  	if (uri == null) {
-    //  	    if (prefix.equals("")) {
-    //  		return name;
-    //  	    } else {
-    //  		throw new SAXException(new FOPException("Unknown namespace prefix " + prefix));
-    //  	    }
-    //  	}
-    //  	return uri + "^" + localPart;
-    //      }
 
     /**
      * add a mapping from element name to maker.
@@ -115,18 +64,18 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
      * @param namespaceURI namespace URI of formatting object element
      * @param localName local name of formatting object element
      * @param maker Maker for class representing formatting object
-    */
+     */
     public void addMapping(String namespaceURI, String localName,
                            FObj.Maker maker) {
         this.fobjTable.put(namespaceURI + "^" + localName, maker);
     }
 
     /**
-      * add a mapping from element name to maker.
-      *
-      * @param namespaceURI namespace URI of formatting object element
-      * @param localName local name of formatting object element
-      * @param maker Maker for class representing formatting object
+     * add a mapping from element name to maker.
+     *
+     * @param namespaceURI namespace URI of formatting object element
+     * @param localName local name of formatting object element
+     * @param maker Maker for class representing formatting object
      */
     public void addPropertyList(String namespaceURI, Hashtable list) {
         PropertyListBuilder plb;
@@ -142,11 +91,11 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
     }
 
     /**
-      * add a mapping from element name to maker.
-      *
-      * @param namespaceURI namespace URI of formatting object element
-      * @param localName local name of formatting object element
-      * @param maker Maker for class representing formatting object
+     * add a mapping from element name to maker.
+     *
+     * @param namespaceURI namespace URI of formatting object element
+     * @param localName local name of formatting object element
+     * @param maker Maker for class representing formatting object
      */
     public void addElementPropertyList(String namespaceURI,
                                        String localName, Hashtable list) {
@@ -171,15 +120,11 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
     public void endElement(String uri, String localName, String rawName) {
         currentFObj.end();
         currentFObj = (FObj) currentFObj.getParent();
-        //      level--;
-        //  	while (((NSMap) namespaceStack.peek()).level > level) {
-        //  	    namespaceStack.pop();
-        //  	}
     }
 
     /** SAX Handler for the start of the document */
     public void startDocument() {
-		rootFObj = null; // allows FOTreeBuilder to be reused
+        rootFObj = null; // allows FOTreeBuilder to be reused
         MessageHandler.logln("building formatting object tree");
     }
 
@@ -191,21 +136,6 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
 
         /* the maker for the formatting object started */
         FObj.Maker fobjMaker;
-
-        //  	level++;
-        //  	int length = attlist.getLength();
-        //  	for (int i = 0; i < length; i++) {
-        //  	    String att = attlist.getQName(i);
-        //  	    if (att.equals("xmlns")) {
-        //  		namespaceStack.push( new NSMap("",
-        //  					       attlist.getValue(i),
-        //  					       level));
-        //  	    } else if (att.startsWith("xmlns:")) {
-        //  		String value = attlist.getValue(i);
-        //  		namespaceStack.push(new NSMap(att.substring(6), value,
-        //  					      level));
-        //  	    }
-        //  	}
 
         //String fullName = mapName(rawName);
         String fullName = uri + "^" + localName;
@@ -238,7 +168,7 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
 
         if (rootFObj == null) {
             rootFObj = fobj;
-			rootFObj.setBufferManager(this.bufferManager);	
+            rootFObj.setBufferManager(this.bufferManager);
             if (!fobj.getName().equals("fo:root")) {
                 throw new SAXException(
                   new FOPException("Root element must" +
@@ -252,28 +182,26 @@ public class FOTreeBuilder extends DefaultHandler implements TreeBuilder {
     }
 
     /**
-      * format this formatting object tree
-      *
-      * @param areaTree the area tree to format into
-      */
+     * format this formatting object tree
+     *
+     * @param areaTree the area tree to format into
+     */
     public void format(AreaTree areaTree) throws FOPException {
         MessageHandler.logln("formatting FOs into areas");
-		this.bufferManager.readComplete();
+        this.bufferManager.readComplete();
         ((Root) this.rootFObj).format(areaTree);
     }
 
-    public void reset() 
-    {
-	currentFObj = null;
-	rootFObj = null;
+    public void reset() {
+        currentFObj = null;
+        rootFObj = null;
     }
-    
-    public boolean hasData() 
-    {
-	return (rootFObj != null);
+
+    public boolean hasData() {
+        return (rootFObj != null);
     }
 
     public void setBufferManager(BufferManager bufferManager) {
-     	this.bufferManager = bufferManager;
-    }	    
+        this.bufferManager = bufferManager;
+    }
 }
--- src/org/apache/fop/fo/StandardElementMapping.java
+++ src/org/apache/fop/fo/StandardElementMapping.java
@@ -19,9 +19,23 @@ public class StandardElementMapping implements ElementMapping {
 
         String uri = "http://www.w3.org/1999/XSL/Format";
 
+        // Declarations and Pagination and Layout Formatting Objects
         builder.addMapping(uri, "root", Root.maker());
+        builder.addMapping(uri, "declarations", Declarations.maker());
+        builder.addMapping(uri, "color-profile", ColorProfile.maker());
+        builder.addMapping(uri, "page-sequence", PageSequence.maker());
         builder.addMapping(uri, "layout-master-set",
                            LayoutMasterSet.maker());
+        builder.addMapping(uri, "page-sequence-master",
+                           PageSequenceMaster.maker());
+        builder.addMapping(uri, "single-page-master-reference",
+                           SinglePageMasterReference.maker());
+        builder.addMapping(uri, "repeatable-page-master-reference",
+                           RepeatablePageMasterReference.maker());
+        builder.addMapping(uri, "repeatable-page-master-alternatives",
+                           RepeatablePageMasterAlternatives.maker());
+        builder.addMapping(uri, "conditional-page-master-reference",
+                           ConditionalPageMasterReference.maker());
         builder.addMapping(uri, "simple-page-master",
                            SimplePageMaster.maker());
         builder.addMapping(uri, "region-body", RegionBody.maker());
@@ -29,50 +43,64 @@ public class StandardElementMapping implements ElementMapping {
         builder.addMapping(uri, "region-after", RegionAfter.maker());
         builder.addMapping(uri, "region-start", RegionStart.maker());
         builder.addMapping(uri, "region-end", RegionEnd.maker());
-        builder.addMapping(uri, "page-sequence", PageSequence.maker());
-        builder.addMapping(uri, "page-sequence-master",
-                           PageSequenceMaster.maker());
-        builder.addMapping(uri, "single-page-master-reference",
-                           SinglePageMasterReference.maker());
-        builder.addMapping(uri, "repeatable-page-master-reference",
-                           RepeatablePageMasterReference.maker());
-        builder.addMapping(uri, "conditional-page-master-reference",
-                           ConditionalPageMasterReference.maker());
-        builder.addMapping(uri, "repeatable-page-master-alternatives",
-                           RepeatablePageMasterAlternatives.maker());
         builder.addMapping(uri, "flow", Flow.maker());
         builder.addMapping(uri, "static-content", StaticContent.maker());
+        builder.addMapping(uri, "title", Title.maker());
+
+        // Block-level Formatting Objects
         builder.addMapping(uri, "block", Block.maker());
         builder.addMapping(uri, "block-container", BlockContainer.maker());
-        builder.addMapping(uri, "list-block", ListBlock.maker());
-        builder.addMapping(uri, "list-item", ListItem.maker());
-        builder.addMapping(uri, "list-item-label", ListItemLabel.maker());
-        builder.addMapping(uri, "list-item-body", ListItemBody.maker());
+
+        // Inline-level Formatting Objects
+        builder.addMapping(uri, "bidi-override", BidiOverride.maker());
+        builder.addMapping(uri, "character",
+                           org.apache.fop.fo.flow.Character.maker());
+        builder.addMapping(uri, "initial-property-set", InitialPropertySet.maker());
+        builder.addMapping(uri, "external-graphic",
+                           ExternalGraphic.maker());
+        builder.addMapping(uri, "instream-foreign-object",
+                           InstreamForeignObject.maker());
+        builder.addMapping(uri, "inline", Inline.maker());
+        builder.addMapping(uri, "inline-container", InlineContainer.maker());
+        builder.addMapping(uri, "leader", Leader.maker());
         builder.addMapping(uri, "page-number", PageNumber.maker());
         builder.addMapping(uri, "page-number-citation",
                            PageNumberCitation.maker());
-        builder.addMapping(uri, "display-sequence",
-                           DisplaySequence.maker());
-        builder.addMapping(uri, "inline", Inline.maker());
-        builder.addMapping(uri, "external-graphic",
-                           ExternalGraphic.maker());
+
+        // Formatting Objects for Tables
+        builder.addMapping(uri, "table-and-caption", TableAndCaption.maker());
         builder.addMapping(uri, "table", Table.maker());
         builder.addMapping(uri, "table-column", TableColumn.maker());
+        builder.addMapping(uri, "table-caption", TableCaption.maker());
         builder.addMapping(uri, "table-header", TableHeader.maker());
-        builder.addMapping(uri, "table-body", TableBody.maker());
         builder.addMapping(uri, "table-footer", TableFooter.maker());
+        builder.addMapping(uri, "table-body", TableBody.maker());
         builder.addMapping(uri, "table-row", TableRow.maker());
         builder.addMapping(uri, "table-cell", TableCell.maker());
+
+        // Formatting Objects for Lists
+        builder.addMapping(uri, "list-block", ListBlock.maker());
+        builder.addMapping(uri, "list-item", ListItem.maker());
+        builder.addMapping(uri, "list-item-body", ListItemBody.maker());
+        builder.addMapping(uri, "list-item-label", ListItemLabel.maker());
+
+        // Dynamic Effects: Link and Multi Formatting Objects
         builder.addMapping(uri, "basic-link", BasicLink.maker());
-        builder.addMapping(uri, "instream-foreign-object",
-                           InstreamForeignObject.maker());
-        builder.addMapping(uri, "leader", Leader.maker());
-        builder.addMapping(uri, "character",
-                           org.apache.fop.fo.flow.Character.maker());
+        builder.addMapping(uri, "multi-switch", MultiSwitch.maker());
+        builder.addMapping(uri, "multi-case", MultiCase.maker());
+        builder.addMapping(uri, "multi-toggle", MultiToggle.maker());
+        builder.addMapping(uri, "multi-properties", MultiProperties.maker());
+        builder.addMapping(uri, "multi-property-set", MultiPropertySet.maker());
+
+        // Out-of-Line Formatting Objects
+        builder.addMapping(uri, "float", org.apache.fop.fo.flow.Float.maker());
         builder.addMapping(uri, "footnote", Footnote.maker());
         builder.addMapping(uri, "footnote-body", FootnoteBody.maker());
-        builder.addMapping(uri, "wrapper", Wrapper.maker());
 
+        // Other Formatting Objects
+        builder.addMapping(uri, "wrapper", Wrapper.maker());
+        builder.addMapping(uri, "marker", Marker.maker());
+        builder.addMapping(uri, "retrieve-marker", RetrieveMarker.maker());
 
         builder.addPropertyList(uri,
                                 FOPropertyMapping.getGenericMappings());
