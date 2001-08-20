caed7c99273501c34785288f0b93a4fcc11bfaf0
Steve Coffman <gears@apache.org>
2001-08-20 21:11:02 +0000

    Fixes script classpaths to use avalon and logkit. Fixes last xml-doc bug that prevented generating the PDF.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194435 13f79535-47bb-0310-9956-ffa450edef68

--- fop.sh
+++ fop.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-java -cp build/fop.jar:lib/batik.jar:lib/xalan-2.0.0.jar:lib/xerces-1.2.3.jar:lib/jimi-1.0.jar org.apache.fop.apps.Fop "$@"
+LIBDIR=./lib
+LOCALCLASSPATH=$LIBDIR/../build/fop.jar:$LIBDIR/batik.jar:$LIBDIR/xalan-2.0.0.jar:$LIBDIR/xerces-1.2.3.jar:$LIBDIR/avalon-framework-4.0.jar:$LIBDIR/logkit-1.0b4.jar:$LIBDIR/jimi-1.0.jar 
+java -cp $CLASSPATH:$LOCALCLASSPATH org.apache.fop.apps.Fop "$@"
 
