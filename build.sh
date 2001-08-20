e1d8bf21713daa6e1ecb867e7dcbe82bea82e812
Keiron Liddle <keiron@apache.org>
2001-08-20 11:33:44 +0000

    adjusted classpath libs for logging
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194431 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ if [ "$JAVA_HOME" = "" ] ; then
   exit 1
 fi
 LIBDIR=lib
-LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/classes.zip:$LIBDIR/ant.jar:$LIBDIR/batik.jar:$LIBDIR/buildtools.jar:$LIBDIR/xerces-1.2.3.jar:$LIBDIR/xalan-2.0.0.jar:$LIBDIR/xalanj1compat.jar:$LIBDIR/bsf.jar:$LIBDIR/jimi-1.0.jar:$LIBDIR/logkit-1.0b4.jar
+LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/classes.zip:$LIBDIR/ant.jar:$LIBDIR/batik.jar:$LIBDIR/buildtools.jar:$LIBDIR/xerces-1.2.3.jar:$LIBDIR/xalan-2.0.0.jar:$LIBDIR/xalanj1compat.jar:$LIBDIR/bsf.jar
+LOCALCLASSPATH=$LOCALCLASSPATH:$LIBDIR/jimi-1.0.jar:$LIBDIR/logkit-1.0b4.jar:$LIBDIR/avalon-framework-4.0.jar
+
 ANT_HOME=$LIBDIR
 
 echo
