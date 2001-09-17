f3e9ea78369184de439b240b613f337b67898a35
Keiron Liddle <keiron@apache.org>
2001-09-17 11:04:50 +0000

    put a few common methods in the abstract renderer
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194465 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/render/AbstractRenderer.java
+++ src/org/apache/fop/render/AbstractRenderer.java
@@ -2,7 +2,7 @@
  * $Id$
  * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
  * For details on use and redistribution please refer to the
- * LICENSE file included with these sources."
+ * LICENSE file included with these sources.
  */
 
 package org.apache.fop.render;
@@ -18,8 +18,6 @@ import org.apache.fop.layout.inline.*;
 import org.apache.fop.datatypes.*;
 import org.apache.fop.render.pdf.FontSetup;
 
-import org.apache.fop.svg.SVGArea;
-
 import org.apache.log.Logger;
 
 // Java
@@ -34,8 +32,86 @@ import java.util.Enumeration;
 public abstract class AbstractRenderer implements Renderer {
     protected Logger log;
 
+    /**
+     * the current vertical position in millipoints from bottom
+     */
+    protected int currentYPosition = 0;
+
+    /**
+     * the current horizontal position in millipoints from left
+     */
+    protected int currentXPosition = 0;
+
+    /**
+     * the horizontal position of the current area container
+     */
+    protected int currentAreaContainerXPosition = 0;
+
     public void setLogger(Logger logger) {
         log = logger;
     }
 
+    public void renderSpanArea(SpanArea area) {
+        Enumeration e = area.getChildren().elements();
+        while (e.hasMoreElements()) {
+            org.apache.fop.layout.Box b =
+                (org.apache.fop.layout.Box)e.nextElement();
+            b.render(this);    // column areas
+        }
+
+    }
+
+    protected abstract void doFrame(Area area);
+
+    /**
+     * render block area
+     *
+     * @param area the block area to render
+     */
+    public void renderBlockArea(BlockArea area) {
+        // KLease: Temporary test to fix block positioning
+        // Offset ypos by padding and border widths
+        this.currentYPosition -= (area.getPaddingTop()
+                                  + area.getBorderTopWidth());
+        doFrame(area);
+        Enumeration e = area.getChildren().elements();
+        while (e.hasMoreElements()) {
+            Box b = (Box)e.nextElement();
+            b.render(this);
+        }
+        this.currentYPosition -= (area.getPaddingBottom()
+                                  + area.getBorderBottomWidth());
+    }
+
+    /**
+     * render line area
+     *
+     * @param area area to render
+     */
+    public void renderLineArea(LineArea area) {
+        int rx = this.currentAreaContainerXPosition + area.getStartIndent();
+        int ry = this.currentYPosition;
+        int w = area.getContentWidth();
+        int h = area.getHeight();
+
+        this.currentYPosition -= area.getPlacementOffset();
+        this.currentXPosition = rx;
+
+        int bl = this.currentYPosition;
+
+        Enumeration e = area.getChildren().elements();
+        while (e.hasMoreElements()) {
+            Box b = (Box)e.nextElement();
+            if (b instanceof InlineArea) {
+                InlineArea ia = (InlineArea)b;
+                this.currentYPosition = ry - ia.getYOffset();
+            } else {
+                this.currentYPosition = ry - area.getPlacementOffset();
+            }
+            b.render(this);
+        }
+
+        this.currentYPosition = ry - h;
+        this.currentXPosition = rx;
+    }
 }
