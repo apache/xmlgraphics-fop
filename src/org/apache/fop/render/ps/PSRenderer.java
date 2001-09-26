692a69774d19362a7c39f7df56bd60b88aef9b4d
Keiron Liddle <keiron@apache.org>
2001-09-26 12:00:43 +0000

    catches exception when building svg
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194486 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/render/ps/PSRenderer.java
+++ src/org/apache/fop/render/ps/PSRenderer.java
@@ -295,13 +295,21 @@ public class PSRenderer extends AbstractRenderer {
         int y = this.currentYPosition;
         Document doc = area.getSVGDocument();
 
-        UserAgent userAgent = new org.apache.fop.svg.SVGUserAgent(new AffineTransform());
+        org.apache.fop.svg.SVGUserAgent userAgent
+            = new org.apache.fop.svg.SVGUserAgent(new AffineTransform());
+        userAgent.setLogger(log);
 
         GVTBuilder builder = new GVTBuilder();
         BridgeContext ctx = new BridgeContext(userAgent);
 
         GraphicsNode root;
-        root = builder.build(ctx, doc);
+        try {        
+            root = builder.build(ctx, doc);
+        } catch (Exception e) {        
+            log.error("svg graphic could not be built: "
+                                   + e.getMessage(), e);        
+            return;
+        }
         // get the 'width' and 'height' attributes of the SVG document
         float w = (float)ctx.getDocumentSize().getWidth() * 1000f;
         float h = (float)ctx.getDocumentSize().getHeight() * 1000f;
