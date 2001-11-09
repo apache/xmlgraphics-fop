671cd7fedc4e62aa6f75e240bbb020e79ce921f5
Karen Lease <klease@apache.org>
2001-11-09 22:11:14 +0000

    Comment out all layout code for now
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194546 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/flow/StaticContent.java
+++ src/org/apache/fop/fo/flow/StaticContent.java
@@ -31,50 +31,50 @@ public class StaticContent extends Flow {
 
     public Status layout(Area area, Region region) throws FOPException {
 
-        int numChildren = this.children.size();
-        // Set area absolute height so that link rectangles will be drawn correctly in xsl-before and xsl-after
-        String regionClass = "none";
-        if (region != null) {
-            regionClass = region.getRegionClass();
-        } else {
-            if (getFlowName().equals("xsl-region-before")) {
-                regionClass = RegionBefore.REGION_CLASS;
-            } else if (getFlowName().equals("xsl-region-after")) {
-                regionClass = RegionAfter.REGION_CLASS;
-            } else if (getFlowName().equals("xsl-region-start")) {
-                regionClass = RegionStart.REGION_CLASS;
-            } else if (getFlowName().equals("xsl-region-end")) {
-                regionClass = RegionEnd.REGION_CLASS;
-            }
+//         int numChildren = this.children.size();
+//         // Set area absolute height so that link rectangles will be drawn correctly in xsl-before and xsl-after
+//         String regionClass = "none";
+//         if (region != null) {
+//             regionClass = region.getRegionClass();
+//         } else {
+//             if (getFlowName().equals("xsl-region-before")) {
+//                 regionClass = RegionBefore.REGION_CLASS;
+//             } else if (getFlowName().equals("xsl-region-after")) {
+//                 regionClass = RegionAfter.REGION_CLASS;
+//             } else if (getFlowName().equals("xsl-region-start")) {
+//                 regionClass = RegionStart.REGION_CLASS;
+//             } else if (getFlowName().equals("xsl-region-end")) {
+//                 regionClass = RegionEnd.REGION_CLASS;
+//             }
 
-        }
+//         }
 
-        if (area instanceof org.apache.fop.layout.AreaContainer)
-            ((org.apache.fop.layout.AreaContainer)area).setAreaName(regionClass);
+//         if (area instanceof org.apache.fop.layout.AreaContainer)
+//             ((org.apache.fop.layout.AreaContainer)area).setAreaName(regionClass);
 
-        if (regionClass.equals(RegionBefore.REGION_CLASS)) {
-            area.setAbsoluteHeight(-area.getMaxHeight());
-        } else if (regionClass.equals(RegionAfter.REGION_CLASS)) {
-            area.setAbsoluteHeight(area.getPage().getBody().getMaxHeight());
-        }
-	setContentWidth(area.getContentWidth());
+//         if (regionClass.equals(RegionBefore.REGION_CLASS)) {
+//             area.setAbsoluteHeight(-area.getMaxHeight());
+//         } else if (regionClass.equals(RegionAfter.REGION_CLASS)) {
+//             area.setAbsoluteHeight(area.getPage().getBody().getMaxHeight());
+//         }
+// 	setContentWidth(area.getContentWidth());
 
-        for (int i = 0; i < numChildren; i++) {
-            FObj fo = (FObj)children.elementAt(i);
+//         for (int i = 0; i < numChildren; i++) {
+//             FObj fo = (FObj)children.elementAt(i);
 
-            Status status;
-            if ((status = fo.layout(area)).isIncomplete()) {
-                // in fact all should be laid out and clip, error etc depending on 'overflow'
-                log.warn("Some static content could not fit in the area.");
-                this.marker = i;
-                if ((i != 0) && (status.getCode() == Status.AREA_FULL_NONE)) {
-                    status = new Status(Status.AREA_FULL_SOME);
-                }
-                return (status);
-            }
-        }
-        resetMarker();
-        return new Status(Status.OK);
+//             Status status;
+//             if ((status = fo.layout(area)).isIncomplete()) {
+//                 // in fact all should be laid out and clip, error etc depending on 'overflow'
+//                 log.warn("Some static content could not fit in the area.");
+//                 this.marker = i;
+//                 if ((i != 0) && (status.getCode() == Status.AREA_FULL_NONE)) {
+//                     status = new Status(Status.AREA_FULL_SOME);
+//                 }
+//                 return (status);
+//             }
+//         }
+//         resetMarker();
+         return new Status(Status.OK);
     }
 
     protected String getElementName() {
