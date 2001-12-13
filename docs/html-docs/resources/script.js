5a512c046dc8d8847ba9d940841563e005790d6b
Keiron Liddle <keiron@apache.org>
2001-12-13 08:14:44 +0000

    updated generated html docs
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194601 13f79535-47bb-0310-9956-ffa450edef68

--- docs/html-docs/resources/script.js
+++ docs/html-docs/resources/script.js
@@ -2,15 +2,11 @@ rolloverImagesOn=new Array();
 rolloverImagesOff=new Array();
 
 function rolloverOn(name) {
-  if(rolloverImagesOn[name]){
-    document.images[name].src=rolloverImagesOn[name].src;
-  }
+  document.images[name].src=rolloverImagesOn[name].src;
 }
 
 function rolloverOff(name) {
-  if(rolloverImagesOff[name]){
-      document.images[name].src=rolloverImagesOff[name].src;
-  }
+  document.images[name].src=rolloverImagesOff[name].src;
 }
 
 function rolloverLoad(name,on,off) {
