70cf835a0b1613b3aede95a7c712239b24b19636
Christian Geisert <chrisg@apache.org>
2002-05-02 11:01:22 +0000

    Added czech translation for AWT viewer Submitted by: Michal Buchtik <BuchtikM@dlsystem.cz>
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@194761 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/viewer/resources/resources.cs
@@ -0,0 +1,17 @@
+File=Soubor
+Print=Tisk
+Exit=Konec
+View=Zobrazit
+First page=První strana
+Previous page=Předchozí strana
+Next page=Další strana
+Last page=Poslední strana
+Zoom=Lupa
+Default zoom=Standardní lupa
+Help=Nápověda
+Index=Rejstřík
+Introduction=Představení
+About=O aplikaci
+Page=Strana
+
+
