655c0a09dc797bdbc8971afa250fc78c10e38baf
Christian Geisert <chrisg@apache.org>
2002-06-04 17:53:59 +0000

    fixed problem with Win98/ME commandline (just wrong linefeed?)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@194854 13f79535-47bb-0310-9956-ffa450edef68

--- fop.bat
+++ fop.bat
@@ -1,2 +1,1 @@
 java -cp build\fop.jar;lib\batik.jar;lib\xalan-2.3.1.jar;lib\xercesImpl-2.0.1.jar;lib\xml-apis.jar;lib\avalon-framework-cvs-20020315.jar;lib\logkit-1.0.jar;lib\jimi-1.0.jar org.apache.fop.apps.Fop %1 %2 %3 %4 %5 %6 %7 %8
-
