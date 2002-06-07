4ced289897839f590e8544788c9f81e3831d84fa
Jeremias Maerki <jeremias@apache.org>
2002-06-07 20:14:30 +0000

    Fixed classpath. Xerces was wrong.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@194859 13f79535-47bb-0310-9956-ffa450edef68

--- docs/examples/runtests.bat
+++ docs/examples/runtests.bat
@@ -7,7 +7,7 @@ if "%JAVA_HOME%" == "" goto error
 
 
 set LIBDIR=..\..\lib
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant-1.4.1.jar;%LIBDIR%\batik.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xercesImpl.jar;%LIBDIR%\xml-apis.jar;%LIBDIR%\xalan-2.3.1.jar;%LIBDIR%\bsf.jar;%LIBDIR%\avalon-framework-cvs-20020315.jar;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\..\build\fop.jar
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant-1.4.1.jar;%LIBDIR%\batik.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xercesImpl-2.0.1.jar;%LIBDIR%\xml-apis.jar;%LIBDIR%\xalan-2.3.1.jar;%LIBDIR%\bsf.jar;%LIBDIR%\avalon-framework-cvs-20020315.jar;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\..\build\fop.jar
 set ANT_HOME=%LIBDIR%
 
 echo Starting Tests ...
