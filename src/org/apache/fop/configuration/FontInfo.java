c4614f5a34c7fecc027be6185647ad605ec48f19
Christian Geisert <chrisg@apache.org>
2002-06-10 15:02:32 +0000

    BaseDir property is now used for loading custom fonts (Bug #7608) (thanks to Arnd Beissner and Brian O'Kelley)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@194867 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/configuration/FontInfo.java
+++ src/org/apache/fop/configuration/FontInfo.java
@@ -8,16 +8,25 @@
 
 package org.apache.fop.configuration;
 
+// Java
+import java.io.File;
+import java.net.URL;
+import java.net.MalformedURLException;
 import java.util.Vector;
+
+// Fop
+import org.apache.fop.apps.FOPException;
 
 /**
  * FontInfo contains meta information on fonts (where is the metrics file etc.)
  */
 
 public class FontInfo {
+
     private String metricsFile, embedFile, name;
     private boolean kerning;
     private Vector fontTriplets;
+    private String baseDir;
 
     public FontInfo(String name, String metricsFile, boolean kerning,
                     Vector fontTriplets, String embedFile) {
@@ -28,12 +37,38 @@ public class FontInfo {
         this.fontTriplets = fontTriplets;
     }
 
-    public String getMetricsFile() {
-        return metricsFile;
+    /**
+     * @return the (absolute) file name of the metrics file
+     */
+    public String getMetricsFile() throws FOPException {
+        // check if it's a URL and convert it to a filename
+        try {
+            metricsFile = new URL(metricsFile).getFile();
+        } catch (MalformedURLException mue) {}
+
+        // check if filename is absolute
+        if ((new File(metricsFile).isAbsolute())) {
+            return metricsFile;
+        } else {
+            return getBaseDir() + metricsFile;
+        }
     }
 
-    public String getEmbedFile() {
-        return embedFile;
+    /**
+     * @return the (absolute) file name of the font
+     */
+    public String getEmbedFile() throws FOPException {
+        // check if it's a URL and convert it to a filename
+        try {
+            embedFile = new URL(embedFile).getFile();
+        } catch (MalformedURLException mue) {}
+        
+        // check if filename is absolute
+        if ((new File(embedFile).isAbsolute())) {
+            return embedFile;
+        } else {
+            return getBaseDir() + embedFile;
+        }
     }
 
     public boolean getKerning() {
@@ -43,6 +78,28 @@ public class FontInfo {
     public Vector getFontTriplets() {
         return fontTriplets;
     }
+
+    /**
+     * @return BaseDir (path)
+     */
+    private String getBaseDir() throws FOPException {
+        baseDir = Configuration.getStringValue("baseDir");
+        URL baseURL = null;
+        try {
+            baseURL = new URL(baseDir);
+        } catch (MalformedURLException mue) {
+            // if the href contains only a path then file is assumed
+            try {
+                baseURL = new URL("file:" + baseDir);
+            } catch (MalformedURLException mue2) {
+                throw new FOPException("Error with baseDir: "
+                                             + mue2.getMessage());
+            }
+        }
+        baseDir = baseURL.getFile();
+        return baseDir;
+    }
+
 
 }
 
