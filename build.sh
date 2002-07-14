8841f67d4b87761e0ac7990eca2f479460850185
Joerg Pietschmann <pietsch@apache.org>
2002-07-14 15:23:47 +0000

    Removed traces of buffermanager A few cosmetical changes in CommandLineOptions
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@194996 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -12,11 +12,30 @@ if [ "$JAVA_HOME" = "" ] ; then
   echo "location of the Java Virtual Machine you want to use."
   exit 1
 fi
+
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false;
+case "`uname`" in
+  CYGWIN*) cygwin=true ;;
+esac
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+fi
+
 LIBDIR=lib
 LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/classes.zip:$LIBDIR/ant.jar:$LIBDIR/batik.jar:$LIBDIR/buildtools.jar:$LIBDIR/xerces-1.4.3.jar:$LIBDIR/xalan-2.3.1.jar:$LIBDIR/xml-apis.jar
 LOCALCLASSPATH=$LOCALCLASSPATH:$LIBDIR/jimi-1.0.jar:$LIBDIR/avalon-framework-cvs-20020315.jar
 
 ANT_HOME=$LIBDIR
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+  JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+  LOCALCLASSPATH=`cygpath --path --windows "$LOCALCLASSPATH"`
+fi
 
 echo
 echo Building with classpath $LOCALCLASSPATH
