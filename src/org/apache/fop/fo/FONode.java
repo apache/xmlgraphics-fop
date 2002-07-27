872c7869cfbd133cd170c1831118daa5f2b87aa5
Joerg Pietschmann <pietsch@apache.org>
2002-07-27 20:08:28 +0000

    Removed some unused code
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195031 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/FONode.java
+++ src/org/apache/fop/fo/FONode.java
@@ -61,9 +61,6 @@ abstract public class FONode {
     protected int forcedStartOffset = 0;
     protected int forcedWidth = 0;
 
-    protected int widows = 0;
-    protected int orphans = 0;
-
     protected LinkSet linkSet;
 
     // count of areas generated-by/returned-by
@@ -117,14 +114,6 @@ abstract public class FONode {
         for (int i = 0; i < numChildren; i++) {
             ((FONode)children.elementAt(i)).resetMarker();
         }
-    }
-
-    public void setWidows(int wid) {
-        widows = wid;
-    }
-
-    public void setOrphans(int orph) {
-        orphans = orph;
     }
 
     public void removeAreas() {
