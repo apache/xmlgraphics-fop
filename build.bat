893643aa17c2d0b4f53b2e11e215880ffe3a2ac8
Jeremias Maerki <jeremias@apache.org>
2002-08-07 10:31:37 +0000

    Adjust for new Avalon Framework JAR
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195064 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ if "%JAVA_HOME%" == "" goto error
 
 set LIBDIR=lib
 set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant-1.4.1.jar;%LIBDIR%\batik.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xercesImpl-2.0.1.jar;%LIBDIR%\xml-apis.jar;%LIBDIR%\xalan-2.3.1.jar;%LIBDIR%\bsf.jar
-set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\avalon-framework-cvs-20020315.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\avalon-framework-cvs-20020806.jar
 
 set ANT_HOME=%LIBDIR%
 
