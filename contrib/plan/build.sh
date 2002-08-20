87043b9e254777402d1df0f308f8766777583ba4
Keiron Liddle <keiron@apache.org>
2002-08-20 08:09:00 +0000

    updated classpath, fixed some small errors
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@195109 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/plan/build.sh
+++ contrib/plan/build.sh
@@ -12,7 +12,7 @@ if [ "$JAVA_HOME" = "" ] ; then
   exit 1
 fi
 LIBDIR=../../lib
-LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/classes.zip:$LIBDIR/ant.jar:$LIBDIR/batik.jar:$LIBDIR/buildtools.jar:$LIBDIR/xerces-1.4.3.jar:$LIBDIR/xalan-2.2D11.jar:../../build/fop.jar:$LIBDIR/avalon-framework-cvs-20020806.jar
+LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/classes.zip:$LIBDIR/ant.jar:$LIBDIR/batik.jar:$LIBDIR/buildtools.jar:$LIBDIR/xercesImpl-2.0.1.jar:$LIBDIR/xalan-2.2D11.jar:$LIBDIR/xml-apis.jar:../../build/fop.jar:$LIBDIR/avalon-framework-cvs-20020806.jar
 ANT_HOME=$LIBDIR
 
 echo
