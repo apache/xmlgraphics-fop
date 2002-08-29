386a0f2adc74d321a70299d277e4d0cd6a8e7475
Keiron Liddle <keiron@apache.org>
2002-08-29 07:58:37 +0000

    moved filed area to its own class
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@195124 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/area/inline/FilledArea.java
@@ -0,0 +1,50 @@
+/*
+ * $Id$
+ * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
+ */
+
+package org.apache.fop.area.inline;
+
+import org.apache.fop.area.MinOptMax;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Filled area.
+ * This inline area contains some inline areas.
+ * When the renderer gets the child areas to render
+ * the inline areas are repeated to fill the ipd of
+ * this inline parent.
+ */
+public class FilledArea extends InlineParent {
+    MinOptMax alloc;
+    int unitWidth;
+
+    public FilledArea() {
+    }
+
+    public void setUnitWidth(int w) {
+        unitWidth = w;
+    }
+
+    public void setAllocationIPD(MinOptMax all) {
+        alloc = all;
+    }
+
+    public MinOptMax getAllocationIPD() {
+        return alloc;
+    }
+
+    public List getChildAreas() {
+        int units = (int)(getWidth() / unitWidth);
+        ArrayList newList = new ArrayList();
+        for (int count = 0; count < units; count++) {
+            newList.addAll(inlines);
+        }
+        return newList;
+    }
+}
+
