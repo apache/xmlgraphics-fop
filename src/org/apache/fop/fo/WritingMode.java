381ee03743d5b6666984701205661b70ae82d0ef
Peter Bernard West <pbwest@apache.org>
2002-09-03 03:23:48 +0000

    Constants interface
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195140 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/fo/WritingMode.java
@@ -0,0 +1,20 @@
+package org.apache.fop.fo;
+
+/*
+ * $Id$
+ *
+ * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
+ * @author <a href="mailto:pbwest@powerup.com.au">Peter B. West</a>
+ * @version $Revision$ $Name$
+ */
+/**
+ * Constants interface for all writing modes.
+ */
+
+public interface WritingMode {
+    public final static int LR_TB = 1;
+    public final static int RL_TB = 2;
+    public final static int TB_RL = 3;
+}
