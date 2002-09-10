b8078952f668b38bc6cc0717e4d0b6c921abd5d0
Jeremias Maerki <jeremias@apache.org>
2002-09-10 11:29:03 +0000

    Oops, forgot one little import. Sorry.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@195165 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/image/analyser/XMLReader.java
+++ src/org/apache/fop/image/analyser/XMLReader.java
@@ -14,6 +14,7 @@ import java.util.Map;
 // XML
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 // FOP
 import org.apache.fop.image.FopImage;
