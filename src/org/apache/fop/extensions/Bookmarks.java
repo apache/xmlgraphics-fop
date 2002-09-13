6c903f9d6d7f55465c3a36e7aa421cd9ccfdb347
Keiron Liddle <keiron@apache.org>
2002-09-13 09:29:49 +0000

    fixed style errors
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@195178 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/extensions/Bookmarks.java
+++ src/org/apache/fop/extensions/Bookmarks.java
@@ -1,6 +1,6 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * Copyright (C) 2001-2002 The Apache Software Foundation. All rights reserved.
  * For details on use and redistribution please refer to the
  * LICENSE file included with these sources.
  */
@@ -11,37 +11,61 @@ import org.apache.fop.apps.LayoutHandler;
 import org.apache.fop.fo.FONode;
 import org.apache.fop.area.AreaTree;
 
-import java.util.*;
+import java.util.ArrayList;
 
-import org.xml.sax.Attributes;
-
+/**
+ * Bookmarks data is the top level element of the pdf bookmark extension.
+ * This handles the adding of outlines. When the element is ended it
+ * creates the bookmark data and adds to the area tree.
+ */
 public class Bookmarks extends ExtensionObj {
     private ArrayList outlines = new ArrayList();
     private BookmarkData data;
 
+    /**
+     * Create a new Bookmarks object.
+     *
+     * @param parent the parent fo node
+     */
     public Bookmarks(FONode parent) {
         super(parent);
     }
 
+    /**
+     * Add the child to the top level.
+     * This handles all Outline objects added and ignores others.
+     *
+     * @param obj the child to add
+     */
     protected void addChild(FONode obj) {
         if (obj instanceof Outline) {
             outlines.add(obj);
         }
     }
 
+    /**
+     * Get the data created for this bookmark.
+     *
+     * @return the bookmark data
+     */
     public BookmarkData getData() {
         return data;
     }
 
+    /**
+     * When this element is finished then it can create
+     * the bookmark data from the child elements and add
+     * the extension to the area tree.
+     */
     public void end() {
         getLogger().debug("adding bookmarks to area tree");
         data = new BookmarkData();
-        for(int count = 0; count < outlines.size(); count++) {
+        for (int count = 0; count < outlines.size(); count++) {
             Outline out = (Outline)outlines.get(count);
             data.addSubData(out.getData());
         }
         // add data to area tree for resolving and handling
-        if(structHandler instanceof LayoutHandler) {
+        if (structHandler instanceof LayoutHandler) {
             AreaTree at = ((LayoutHandler)structHandler).getAreaTree();
             at.addTreeExtension(data);
             data.setAreaTree(at);
--- src/org/apache/fop/extensions/Outline.java
+++ src/org/apache/fop/extensions/Outline.java
@@ -1,6 +1,6 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * Copyright (C) 2001-2002 The Apache Software Foundation. All rights reserved.
  * For details on use and redistribution please refer to the
  * LICENSE file included with these sources.
  */
@@ -10,10 +10,14 @@ package org.apache.fop.extensions;
 import org.apache.fop.fo.FONode;
 import org.apache.fop.apps.FOPException;
 
-import java.util.*;
+import java.util.ArrayList;
 
 import org.xml.sax.Attributes;
 
+/**
+ * The outline object for the pdf bookmark extension.
+ * The outline element contains a label and optionally more outlines.
+ */
 public class Outline extends ExtensionObj {
     private Label label;
     private ArrayList outlines = new ArrayList();
@@ -21,16 +25,28 @@ public class Outline extends ExtensionObj {
     private String internalDestination;
     private String externalDestination;
 
+    /**
+     * Create a new outline object.
+     *
+     * @param parent the parent fo node
+     */
     public Outline(FONode parent) {
         super(parent);
     }
 
+    /**
+     * The attribues on the outline object are the internal and external
+     * destination. One of these is required.
+     *
+     * @param attlist the attribute list
+     * @throws FOPException a fop exception if there is an error
+     */
     public void handleAttrs(Attributes attlist) throws FOPException {
         internalDestination =
             attlist.getValue("internal-destination");
         externalDestination =
             attlist.getValue("external-destination");
-        if (externalDestination != null &&!externalDestination.equals("")) {
+        if (externalDestination != null && !externalDestination.equals("")) {
             getLogger().warn("fox:outline external-destination not supported currently.");
         }
 
@@ -40,6 +56,12 @@ public class Outline extends ExtensionObj {
 
     }
 
+    /**
+     * Add the child to this outline.
+     * This checks for the type, label or outline and handles appropriately.
+     *
+     * @param obj the child object
+     */
     protected void addChild(FONode obj) {
         if (obj instanceof Label) {
             label = (Label)obj;
@@ -48,16 +70,29 @@ public class Outline extends ExtensionObj {
         }
     }
 
+    /**
+     * Get the bookmark data for this outline.
+     * This creates a bookmark data with the destination
+     * and adds all the data from child outlines.
+     *
+     * @return the new bookmark data
+     */
     public BookmarkData getData() {
         BookmarkData data = new BookmarkData(internalDestination);
         data.setLabel(getLabel());
-        for(int count = 0; count < outlines.size(); count++) {
+        for (int count = 0; count < outlines.size(); count++) {
             Outline out = (Outline)outlines.get(count);
             data.addSubData(out.getData());
         }
         return data;
     }
 
+    /**
+     * Get the label string.
+     * This gets the label string from the child label element.
+     *
+     * @return the label string or empty if not found
+     */
     public String getLabel() {
         return label == null ? "" : label.toString();
     }
