90efd879f361b83ff8bd43fa3355c12b016ed610
Peter Bernard West <pbwest@apache.org>
2002-10-19 03:09:04 +0000

    DEFAULTBUFSIZE increased to 128.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195337 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/datastructs/SyncedCircularBuffer.java
+++ src/org/apache/fop/datastructs/SyncedCircularBuffer.java
@@ -28,7 +28,7 @@ import java.lang.IndexOutOfBoundsException;
  */
 public class SyncedCircularBuffer {
 
-    private final static int DEFAULTBUFSIZE = 32;
+    private final static int DEFAULTBUFSIZE = 128;
 
     private Object[] buf;
     private int size = 0;
