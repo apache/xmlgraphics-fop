981c646e6bed1130bc744f43736103daa69ee49f
Peter Bernard West <pbwest@apache.org>
2002-10-31 01:53:40 +0000

    New property in Errata.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195389 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/fo/properties/UsageContextOfSuppressAtLineBreak.java
@@ -0,0 +1,27 @@
+package org.apache.fop.fo.properties;
+
+import org.apache.fop.fo.expr.PropertyException;
+import org.apache.fop.datastructs.ROStringArray;
+import org.apache.fop.fo.properties.Property;
+
+public class UsageContextOfSuppressAtLineBreak extends Property  {
+    public static final int dataTypes = AUTO | ENUM | INHERIT;
+    public static final int traitMapping = FORMATTING;
+    public static final int initialValueType = AUTO_IT;
+    public static final int OBSERVE = 1;
+    public static final int IGNORE = 2;
+    public static final int inherited = COMPUTED;
+
+    private static final String[] rwEnums = {
+        null
+        ,"observe"
+        ,"ignore"
+    };
+    public int getEnumIndex(String enum) throws PropertyException {
+        return enumValueToIndex(enum, rwEnums);
+    }
+    public String getEnumText(int index) {
+        return rwEnums[index];
+    }
+}
+
