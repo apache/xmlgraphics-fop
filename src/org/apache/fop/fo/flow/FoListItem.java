cdb510150bfaa349d52efff16c071c68e09ef6e2
Peter Bernard West <pbwest@apache.org>
2002-11-08 11:33:46 +0000

    Initial FOs as holders for properties.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195456 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/fo/flow/FoListItem.java
@@ -0,0 +1,106 @@
+/*
+ * $Id$
+ * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
+ *
+ * @author <a href="mailto:pbwest@powerup.com.au">Peter B. West</a>
+ */
+
+package org.apache.fop.fo.flow;
+
+// FOP
+import org.apache.fop.fo.PropNames;
+import org.apache.fop.fo.FOPropertySets;
+import org.apache.fop.fo.PropertySets;
+import org.apache.fop.fo.FObjectNames;
+import org.apache.fop.fo.FONode;
+import org.apache.fop.fo.FOTree;
+import org.apache.fop.fo.expr.PropertyException;
+import org.apache.fop.xml.FoXMLEvent;
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.datastructs.TreeException;
+import org.apache.fop.datatypes.PropertyValue;
+import org.apache.fop.datatypes.Ints;
+
+import java.util.HashMap;
+import java.util.BitSet;
+
+/**
+ * Implements the fo:simple-page-master flow object
+ */
+public class FoListItem extends FONode {
+
+    private static final String tag = "$Name$";
+    private static final String revision = "$Revision$";
+
+    /** Map of <tt>Integer</tt> indices of <i>sparsePropsSet</i> array.
+        It is indexed by the FO index of the FO associated with a given
+        position in the <i>sparsePropsSet</i> array. See
+        {@link org.apache.fop.fo.FONode#sparsePropsSet FONode.sparsePropsSet}.
+     */
+    private static final HashMap sparsePropsMap;
+
+    /** An <tt>int</tt> array of of the applicable property indices, in
+        property index order. */
+    private static final int[] sparseIndices;
+
+    /** The number of applicable properties.  This is the size of the
+        <i>sparsePropsSet</i> array. */
+    private static final int numProps;
+
+    static {
+        // Collect the sets of properties that apply
+        BitSet propsets = new BitSet();
+        propsets.or(PropertySets.accessibilitySet);
+        propsets.or(PropertySets.auralSet);
+        propsets.or(PropertySets.backgroundSet);
+        propsets.or(PropertySets.borderSet);
+        propsets.or(PropertySets.marginBlockSet);
+        propsets.or(PropertySets.paddingSet);
+        propsets.or(PropertySets.relativePositionSet);
+        propsets.set(PropNames.BREAK_AFTER);
+        propsets.set(PropNames.BREAK_BEFORE);
+        propsets.set(PropNames.ID);
+        propsets.set(PropNames.INTRUSION_DISPLACE);
+        propsets.set(PropNames.KEEP_TOGETHER);
+        propsets.set(PropNames.KEEP_WITH_NEXT);
+        propsets.set(PropNames.KEEP_WITH_PREVIOUS);
+        propsets.set(PropNames.RELATIVE_ALIGN);
+
+        // Map these properties into sparsePropsSet
+        // sparsePropsSet is a HashMap containing the indicies of the
+        // sparsePropsSet array, indexed by the FO index of the FO slot
+        // in sparsePropsSet.
+        sparsePropsMap = new HashMap();
+        numProps = propsets.cardinality();
+        sparseIndices = new int[numProps];
+        int propx = 0;
+        for (int next = propsets.nextSetBit(0);
+                next >= 0;
+                next = propsets.nextSetBit(next + 1)) {
+            sparseIndices[propx] = next;
+            sparsePropsMap.put
+                        (Ints.consts.get(next), Ints.consts.get(propx++));
+        }
+    }
+
+    /**
+     * @param foTree the FO tree being built
+     * @param parent the parent FONode of this node
+     * @param event the <tt>FoXMLEvent</tt> that triggered the creation of
+     * this node
+     */
+    public FoListItem
+                (FOTree foTree, FONode parent, FoXMLEvent event, int attrSet)
+        throws TreeException, FOPException
+    {
+        super(foTree, FObjectNames.LIST_ITEM, parent, event,
+                          attrSet, sparsePropsMap, sparseIndices, numProps);
+        FoXMLEvent ev;
+        String nowProcessing;
+
+        makeSparsePropsSet();
+    }
+
+}
