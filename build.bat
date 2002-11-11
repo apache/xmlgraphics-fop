b99db4bfdd28745095735d8e0a775d56e970b34f
Jeremias Maerki <jeremias@apache.org>
2002-11-11 10:24:39 +0000

    Updated and improved readability of classpath
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195477 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -6,8 +6,15 @@ echo ----------------
 if "%JAVA_HOME%" == "" goto error
 
 set LIBDIR=lib
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant-1.4.1.jar;%LIBDIR%\batik.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xercesImpl-2.0.1.jar;%LIBDIR%\xml-apis.jar;%LIBDIR%\xalan-2.3.1.jar;%LIBDIR%\bsf.jar
-set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jimi-1.0.jar;%LIBDIR%\avalon-framework-cvs-20020806.jar
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xercesImpl-2.2.0.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xalan-2.4.1.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\ant-1.5.1.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\batik.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\avalon-framework-cvs-20020806.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\bsf.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jimi-1.0.jar
 
 set ANT_HOME=%LIBDIR%
 
