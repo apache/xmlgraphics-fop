fbaee329f30474238b604383ba79f0052c11fea8
Jeremias Maerki <jeremias@apache.org>
2002-11-11 10:22:34 +0000

    Updated and improved readability of classpath fix line endings for build.bat
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195475 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/servlet/build.bat
+++ contrib/servlet/build.bat
@@ -6,7 +6,11 @@ echo ----------------
 if "%JAVA_HOME%" == "" goto error
 
 set LIBDIR=../../lib
-set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip;%LIBDIR%\ant-1.4.1.jar;%LIBDIR%\buildtools.jar;%LIBDIR%\xercesImpl-2.0.1.jar;%LIBDIR%\xml-apis.jar;%LIBDIR%\xalan-2.3.1.jar
+set LOCALCLASSPATH=%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\classes.zip
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\ant-1.5.1.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xercesImpl-2.2.0.jar
+set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xalan-2.4.1.jar
 
 set ANT_HOME=%LIBDIR%
 
