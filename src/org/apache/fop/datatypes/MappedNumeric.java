b5c11204a1c7f2949318344495646fe210a58e7c
Peter Bernard West <pbwest@apache.org>
2002-11-12 02:31:39 +0000

    Added FONode arg to constructor. Added FONode arg to PropertyConsts getMappedNumeric call.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195506 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/datatypes/MappedNumeric.java
+++ src/org/apache/fop/datatypes/MappedNumeric.java
@@ -5,7 +5,7 @@ import org.apache.fop.fo.expr.PropertyException;
 import org.apache.fop.datatypes.PropertyValue;
 import org.apache.fop.fo.PropertyConsts;
 import org.apache.fop.fo.properties.*;
-import org.apache.fop.fo.FOTree;
+import org.apache.fop.fo.FONode;
 
 /*
  * MappedEnumType.java
@@ -34,37 +34,39 @@ public class MappedNumeric extends EnumType {
     private Numeric mappedNum;
 
     /**
+     * @param foNode the <tt>FONode</tt> being built
      * @param property the <tt>int</tt> index of the property on which
      * this value is being defined.
      * @param enumText the <tt>String</tt> containing the enumeration text.
-     * @param foTree the <tt>FOTree</tt> being built
      * An <i>NCName</i>.
      * @exception PropertyException
      */
-    public MappedNumeric(int property, String enumText, FOTree foTree)
+    public MappedNumeric(FONode foNode, int property, String enumText)
         throws PropertyException
     {
         // Set property index in AbstractPropertyValue
         // and enumValue enum constant in EnumType
         super(property, enumText, PropertyValue.MAPPED_NUMERIC);
-        mappedNum = propertyConsts.getMappedNumeric(property, enumValue);
+        mappedNum =
+                propertyConsts.getMappedNumeric(foNode, property, enumValue);
     }
 
     /**
+     * @param foNode the <tt>FONode</tt> being built
      * @param propertyName the <tt>String</tt> name of the property on which
      * this value is being defined.
      * @param enumText the <tt>String</tt> containing the enumeration text.
-     * @param foTree the <tt>FOTree</tt> being built
      * An <i>NCName</i>.
      * @exception PropertyException
      */
-    public MappedNumeric(String propertyName, String enumText, FOTree foTree)
+    public MappedNumeric(FONode foNode, String propertyName, String enumText)
         throws PropertyException
     {
         // Set property index in AbstractPropertyValue
         // and enumValue enum constant in EnumType
         super(propertyName, enumText, PropertyValue.MAPPED_NUMERIC);
-        mappedNum = propertyConsts.getMappedNumeric(property, enumValue);
+        mappedNum =
+                propertyConsts.getMappedNumeric(foNode, property, enumValue);
     }
 
     /**
