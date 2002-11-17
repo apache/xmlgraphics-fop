2714d42e12f5d96c1fcf28b2145c8652561db95e
Peter Bernard West <pbwest@apache.org>
2002-11-17 09:48:22 +0000

    Dummy class to ensure compilation of classes nto named in code.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195541 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/apache/fop/fo/flow/Dummy.java
@@ -0,0 +1,51 @@
+package org.apache.fop.fo.flow;
+
+import org.apache.fop.fo.flow.FoBasicLink;
+import org.apache.fop.fo.flow.FoBidiOverride;
+import org.apache.fop.fo.flow.FoBlockContainer;
+import org.apache.fop.fo.flow.FoBlock;
+import org.apache.fop.fo.flow.FoCharacter;
+import org.apache.fop.fo.flow.FoExternalGraphic;
+import org.apache.fop.fo.flow.FoFloat;
+import org.apache.fop.fo.flow.FoFlow;
+import org.apache.fop.fo.flow.FoFootnoteBody;
+import org.apache.fop.fo.flow.FoFootnote;
+import org.apache.fop.fo.flow.FoInitialPropertySet;
+import org.apache.fop.fo.flow.FoInlineContainer;
+import org.apache.fop.fo.flow.FoInline;
+import org.apache.fop.fo.flow.FoInstreamForeignObject;
+import org.apache.fop.fo.flow.FoLeader;
+import org.apache.fop.fo.flow.FoListBlock;
+import org.apache.fop.fo.flow.FoListItemBody;
+import org.apache.fop.fo.flow.FoListItem;
+import org.apache.fop.fo.flow.FoListItemLabel;
+import org.apache.fop.fo.flow.FoMarker;
+import org.apache.fop.fo.flow.FoMultiCase;
+import org.apache.fop.fo.flow.FoMultiProperties;
+import org.apache.fop.fo.flow.FoMultiPropertySet;
+import org.apache.fop.fo.flow.FoMultiSwitch;
+import org.apache.fop.fo.flow.FoMultiToggle;
+import org.apache.fop.fo.flow.FoNoFo;
+import org.apache.fop.fo.flow.FoPageNumberCitation;
+import org.apache.fop.fo.flow.FoPageNumber;
+import org.apache.fop.fo.flow.FoPageSequence;
+import org.apache.fop.fo.flow.FoPcdata;
+import org.apache.fop.fo.flow.FoRetrieveMarker;
+import org.apache.fop.fo.flow.FoStaticContent;
+import org.apache.fop.fo.flow.FoTableAndCaption;
+import org.apache.fop.fo.flow.FoTableBody;
+import org.apache.fop.fo.flow.FoTableCaption;
+import org.apache.fop.fo.flow.FoTableCell;
+import org.apache.fop.fo.flow.FoTableColumn;
+import org.apache.fop.fo.flow.FoTableFooter;
+import org.apache.fop.fo.flow.FoTableHeader;
+import org.apache.fop.fo.flow.FoTable;
+import org.apache.fop.fo.flow.FoTableRow;
+import org.apache.fop.fo.flow.FoTitle;
+import org.apache.fop.fo.flow.FoWrapper;
+
+public class Dummy {
+    public static void Main(String[] args) {
+        System.out.println("Crash!!!");
+    }
+}
