e8a4777fcd2b8e256a5c7f261aaeb5edee209e0a
Oleg Tkachenko <olegt@apache.org>
2002-11-18 21:04:58 +0000

    Updated classpath (new xalan and xerces libraries file names).
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195557 13f79535-47bb-0310-9956-ffa450edef68

--- fop.bat
+++ fop.bat
@@ -1,1 +1,1 @@
-java -cp build\fop.jar;lib\batik.jar;lib\xalan-2.3.1.jar;lib\xercesImpl-2.0.1.jar;lib\xml-apis.jar;lib\avalon-framework-cvs-20020806.jar;lib\logkit-1.0.jar;lib\jimi-1.0.jar org.apache.fop.apps.Fop %1 %2 %3 %4 %5 %6 %7 %8
+java -cp build\fop.jar;lib\batik.jar;lib\xalan-2.4.1.jar;lib\xercesImpl-2.2.0.jar;lib\xml-apis.jar;lib\avalon-framework-cvs-20020806.jar;lib\logkit-1.0.jar;lib\jimi-1.0.jar org.apache.fop.apps.Fop %1 %2 %3 %4 %5 %6 %7 %8
