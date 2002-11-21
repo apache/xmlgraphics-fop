544434256c969727d74ef087e794503153501309
Peter Bernard West <pbwest@apache.org>
2002-11-21 07:56:29 +0000

    Fixed exception message. Fixed MC_OUT_OF_LINE.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@195586 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/flow/FoBidiOverride.java
+++ src/org/apache/fop/fo/flow/FoBidiOverride.java
@@ -116,10 +116,10 @@ public class FoBidiOverride extends FONode {
                         ev = xmlevents.getEndElement(ev);
                 }
             } catch(UnexpectedStartElementException e) {
+                ev = xmlevents.getStartElement();
                 MessageHandler.logln
                         ("Ignoring unexpected Start Element: "
                                                          + ev.getQName());
-                ev = xmlevents.getStartElement();
                 ev = xmlevents.getEndElement(ev);
             }
         } while (ev != null);
