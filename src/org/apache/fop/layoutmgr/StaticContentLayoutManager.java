5ffc4b23b7d914c86628936cc3579bb205b6ceb4
Keiron Liddle <keiron@apache.org>
2002-11-28 14:14:53 +0000

    clear old breaks
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@195642 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/layoutmgr/StaticContentLayoutManager.java
+++ src/org/apache/fop/layoutmgr/StaticContentLayoutManager.java
@@ -79,7 +79,9 @@ public class StaticContentLayoutManager extends BlockStackingLayoutManager {
             }
         }
 
+        blockBreaks.clear();
         flush();
+        region = null;
     }
 
 
