ecc00ee256f3be0fa6383d07c00d38c8e4a7bfc5
Jeremias Maerki <jeremias@apache.org>
2002-12-02 14:04:16 +0000

    Added missing APL header Improved style
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195706 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fonts/type1/PFBData.java
+++ src/org/apache/fop/fonts/type1/PFBData.java
@@ -1,3 +1,9 @@
+/*
+ * $Id$
+ * Copyright (C) 2002 The Apache Software Foundation. All rights reserved.
+ * For details on use and redistribution please refer to the
+ * LICENSE file included with these sources.
+ */
 package org.apache.fop.fonts.type1;
 
 import java.io.OutputStream;
@@ -44,7 +50,7 @@ public class PFBData {
             case PFB_MAC:
                 throw new UnsupportedOperationException("Mac format is not yet implemented");
             default:
-                throw new IllegalArgumentException("Invalid value for PFB format: "+format);
+                throw new IllegalArgumentException("Invalid value for PFB format: " + format);
         }
     }
 
