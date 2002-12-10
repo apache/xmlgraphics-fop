971d048dc97ab416afe749176adef3643f210f06
Oleg Tkachenko <olegt@apache.org>
2002-12-10 22:28:02 +0000

    Removed footprints of bsf.jar.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195764 13f79535-47bb-0310-9956-ffa450edef68

--- test/createpdf.sh
+++ test/createpdf.sh
@@ -2,7 +2,7 @@
 
 
 LIBDIR=../lib
-LOCALCLASSPATH=$LIBDIR/batik.jar:$LIBDIR/xercesImpl-2.0.1.jar:$LIBDIR/xalan-2.3.1.jar:$LIBDIR/xml-apis.jar:$LIBDIR/bsf.jar
+LOCALCLASSPATH=$LIBDIR/batik.jar:$LIBDIR/xercesImpl-2.0.1.jar:$LIBDIR/xalan-2.3.1.jar:$LIBDIR/xml-apis.jar
 LOCALCLASSPATH=$LOCALCLASSPATH:$LIBDIR/jimi-1.0.jar:$LIBDIR/jai_core.jar:$LIBDIR/jai_codec.jar:$LIBDIR/avalon-framework-cvs-20020806.jar:$LIBDIR/logkit-1.0.jar
 
 $JAVA_HOME/bin/java -cp ../build/fop.jar:$LOCALCLASSPATH org.apache.fop.tools.TestConverter -pdf -b ./ basictests.xml
