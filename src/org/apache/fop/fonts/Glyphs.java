1ae240838fd4162092adcc7c096533a5053584a8
Jeremias Maerki <jeremias@apache.org>
2003-02-25 13:13:21 +0000

    Switched to long licence Cleanup of tab characters where necessary Cleanup of unused imports where necessary
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195990 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fonts/Glyphs.java
+++ src/org/apache/fop/fonts/Glyphs.java
@@ -1,16 +1,59 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.fonts;
 
 public class Glyphs {
-    public static String notdef = ".notdef";
+    public static final String notdef = ".notdef";
 
-    public static String mac_glyph_names[] = {
+    public static final String mac_glyph_names[] = {
         /* 0x00 */
         notdef, ".null", "CR", "space", "exclam", "quotedbl", "numbersign",
                 "dollar", "percent", "ampersand", "quotesingle", "parenleft",
@@ -68,7 +111,7 @@ public class Glyphs {
         "ccaron", "dmacron"
     };
 
-    public static String[] tex8r = {
+    public static final String[] tex8r = {
         // 0x00
         ".notdef", "dotaccent", "fi", "fl", "fraction", "hungarumlaut",
                    "Lslash", "lslash", "ogonek", "ring", ".notdef", "breve",
@@ -127,7 +170,7 @@ public class Glyphs {
     /**
      * The characters in WinAnsiEncoding
      */
-    public static char[] winAnsiEncoding = {
+    public static final char[] winAnsiEncoding = {
         // not used until char 32
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
            0, 0, 0, 0, 0, 0, 0, 0, 0, // 0x20
@@ -179,7 +222,7 @@ public class Glyphs {
              '\u00fc', '\u00fd', '\u00fe', '\u00ff'
     };
 
-    public static String[] unicode_glyphs = {
+    public static final String[] unicode_glyphs = {
               "\u0041", "A",
         "\u00C6", "AE",
         "\u01FC", "AEacute",
