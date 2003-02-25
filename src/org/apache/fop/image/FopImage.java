4678592ca8ae10afdf44dd533fc92a66f7721a8a
Jeremias Maerki <jeremias@apache.org>
2003-02-25 13:38:24 +0000

    Switched to long licence Cleanup of tab characters where necessary Cleanup of unused imports where necessary
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195992 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/image/FopImage.java
+++ src/org/apache/fop/image/FopImage.java
@@ -1,19 +1,63 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
-// Author:       Eric SCHAEFFER
-// Description:  represent an image object
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.image;
 
 import org.apache.fop.datatypes.ColorSpace;
 import org.apache.fop.pdf.PDFColor;
 import org.apache.fop.pdf.PDFFilter;
 
+/**
+ * Class which represents an image
+ * @author Eric SCHAEFFER
+ */
 public interface FopImage {
     // Init the object.
     // If href protocol isn't file://, can load the entire image
@@ -25,40 +69,40 @@ public interface FopImage {
     // Methods throw exception because they can retrieve data
     // when needed.
 
-    public boolean invertImage();
+    boolean invertImage();
 
     // Resource location
-    public String getURL();
+    String getURL();
 
     // image size
-    public int getWidth() throws FopImageException;
-    public int getHeight() throws FopImageException;
+    int getWidth() throws FopImageException;
+    int getHeight() throws FopImageException;
 
     // DeviceGray, DeviceRGB, or DeviceCMYK
-    public ColorSpace getColorSpace() throws FopImageException;
+    ColorSpace getColorSpace() throws FopImageException;
 
     // bits per pixel
-    public int getBitsPerPixel() throws FopImageException;
+    int getBitsPerPixel() throws FopImageException;
 
     // For transparent images
-    public boolean isTransparent() throws FopImageException;
-    public PDFColor getTransparentColor() throws FopImageException;
+    boolean isTransparent() throws FopImageException;
+    PDFColor getTransparentColor() throws FopImageException;
 
     // get the image bytes, and bytes properties
 
     // get uncompressed image bytes
-    public byte[] getBitmaps() throws FopImageException;
+    byte[] getBitmaps() throws FopImageException;
     // width * (bitsPerPixel / 8) * height, no ?
-    public int getBitmapsSize() throws FopImageException;
+    int getBitmapsSize() throws FopImageException;
 
     // get compressed image bytes
     // I don't know if we really need it, nor if it
     // should be changed...
-    public byte[] getRessourceBytes() throws FopImageException;
-    public int getRessourceBytesSize() throws FopImageException;
+    byte[] getRessourceBytes() throws FopImageException;
+    int getRessourceBytesSize() throws FopImageException;
     // return null if no corresponding PDFFilter
-    public PDFFilter getPDFFilter() throws FopImageException;
+    PDFFilter getPDFFilter() throws FopImageException;
 
     // release memory
-    public void close();
+    void close();
 }
--- src/org/apache/fop/image/analyser/JPEGReader.java
+++ src/org/apache/fop/image/analyser/JPEGReader.java
@@ -1,10 +1,53 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.image.analyser;
 
 // Java
@@ -25,17 +68,17 @@ public class JPEGReader extends AbstractImageReader {
      * inside the APPn marker. And we don't want to confuse those dimensions with
      * the image dimensions.
      */
-    static protected final int MARK = 0xff;    // Beginneing of a Marker
-    static protected final int NULL = 0x00;    // Special case for 0xff00
-    static protected final int SOF1 = 0xc0;    // Baseline DCT
-    static protected final int SOF2 = 0xc1;    // Extended Sequential DCT
-    static protected final int SOF3 = 0xc2;    // Progrssive DCT only PDF 1.3
-    static protected final int SOFA = 0xca;    // Progressice DCT only PDF 1.3
-    static protected final int APP0 = 0xe0;    // Application marker, JFIF
-    static protected final int APPF = 0xef;    // Application marker
-    static protected final int SOS = 0xda;     // Start of Scan
-    static protected final int SOI = 0xd8;     // start of Image
-    static protected final int JPG_SIG_LENGTH = 2;
+    protected static final int MARK = 0xff;    // Beginneing of a Marker
+    protected static final int NULL = 0x00;    // Special case for 0xff00
+    protected static final int SOF1 = 0xc0;    // Baseline DCT
+    protected static final int SOF2 = 0xc1;    // Extended Sequential DCT
+    protected static final int SOF3 = 0xc2;    // Progrssive DCT only PDF 1.3
+    protected static final int SOFA = 0xca;    // Progressice DCT only PDF 1.3
+    protected static final int APP0 = 0xe0;    // Application marker, JFIF
+    protected static final int APPF = 0xef;    // Application marker
+    protected static final int SOS = 0xda;     // Start of Scan
+    protected static final int SOI = 0xd8;     // start of Image
+    protected static final int JPG_SIG_LENGTH = 2;
 
     protected byte[] header;
 
