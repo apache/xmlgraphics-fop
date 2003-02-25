d0ed229aadaffd55f7a9aed53413047c5e1c7879
Jeremias Maerki <jeremias@apache.org>
2003-02-25 12:57:05 +0000

    Switched to long licence Cleanup of tab characters where necessary Cleanup of unused imports where necessary
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@195989 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/flow/TableColumn.java
+++ src/org/apache/fop/fo/flow/TableColumn.java
@@ -1,10 +1,53 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.fo.flow;
 
 // FOP
@@ -88,15 +131,15 @@ public class TableColumn extends FObj {
         // this.properties.get("visibility");
 
         this.iColumnNumber =
-	    this.properties.get("column-number").getNumber().intValue();
+        this.properties.get("column-number").getNumber().intValue();
 
         this.numColumnsRepeated =
-            this.properties.get("number-columns-repeated").getNumber().intValue();
+                this.properties.get("number-columns-repeated").getNumber().intValue();
 
         this.columnWidthPropVal =
-            this.properties.get("column-width").getLength();
-	// This won't include resolved table-units or % values yet.
-	this.columnWidth = columnWidthPropVal.mvalue();
+                this.properties.get("column-width").getLength();
+        // This won't include resolved table-units or % values yet.
+        this.columnWidth = columnWidthPropVal.mvalue();
 
         // initialize id
         String id = this.properties.get("id").getString();
@@ -115,18 +158,18 @@ public class TableColumn extends FObj {
                 doSetup(area);
             }
         }
-	if (columnWidth > 0) {
-	    this.areaContainer =
-		new AreaContainer(propMgr.getFontState(area.getFontInfo()),
-				  columnOffset, 0, columnWidth,
-				  area.getContentHeight(), Position.RELATIVE);
-	    areaContainer.foCreator = this;    // G Seshadri
-	    areaContainer.setPage(area.getPage());
-	    areaContainer.setBorderAndPadding(propMgr.getBorderAndPadding());
-	    areaContainer.setBackground(propMgr.getBackgroundProps());
-	    areaContainer.setHeight(area.getHeight());
-	    area.addChild(areaContainer);
-	}
+        if (columnWidth > 0) {
+            this.areaContainer =
+                    new AreaContainer(propMgr.getFontState(area.getFontInfo()),
+                            columnOffset, 0, columnWidth,
+                            area.getContentHeight(), Position.RELATIVE);
+            areaContainer.foCreator = this;    // G Seshadri
+            areaContainer.setPage(area.getPage());
+            areaContainer.setBorderAndPadding(propMgr.getBorderAndPadding());
+            areaContainer.setBackground(propMgr.getBackgroundProps());
+            areaContainer.setHeight(area.getHeight());
+            area.addChild(areaContainer);
+        }
         return Status.OK;
     }
 
@@ -135,10 +178,10 @@ public class TableColumn extends FObj {
     }
 
     public void setHeight(int height) {
-	if (areaContainer != null) {
-	    areaContainer.setMaxHeight(height);
-	    areaContainer.setHeight(height);
-	}
+        if (areaContainer != null) {
+            areaContainer.setMaxHeight(height);
+            areaContainer.setHeight(height);
+        }
     }
 
 }
