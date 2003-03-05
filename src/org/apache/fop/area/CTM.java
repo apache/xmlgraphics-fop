ffbbd945e41d5bade0d5a24929b77b29fbf538ed
Jeremias Maerki <jeremias@apache.org>
2003-03-05 15:19:33 +0000

    Switched to long licence Some general checkstyle fixing
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196014 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/area/CTM.java
+++ src/org/apache/fop/area/CTM.java
@@ -1,9 +1,53 @@
 /*
  * $Id$
- * Copyright (C) 2001-2002 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.area;
 
 import java.awt.geom.Rectangle2D;
@@ -18,11 +62,12 @@ import org.apache.fop.fo.properties.WritingMode;
  * system used to render pages.
  */
 public class CTM implements Serializable {
+    
     private double a, b, c, d, e, f;
 
-    private static CTM s_CTM_lrtb = new CTM(1, 0, 0, 1, 0, 0);
-    private static CTM s_CTM_rltb = new CTM(-1, 0, 0, 1, 0, 0);
-    private static CTM s_CTM_tbrl = new CTM(0, 1, -1, 0, 0, 0);
+    private static final CTM CTM_LRTB = new CTM(1, 0, 0, 1, 0, 0);
+    private static final CTM CTM_RLTB = new CTM(-1, 0, 0, 1, 0, 0);
+    private static final CTM CTM_TBRL = new CTM(0, 1, -1, 0, 0, 0);
 
     /**
      * Create the identity matrix
@@ -100,21 +145,21 @@ public class CTM implements Serializable {
         CTM wmctm;
         switch (wm) {
             case WritingMode.LR_TB:
-                return new CTM(s_CTM_lrtb);
+                return new CTM(CTM_LRTB);
             case WritingMode.RL_TB:
                 {
-                    wmctm = new CTM(s_CTM_rltb);
+                    wmctm = new CTM(CTM_RLTB);
                     wmctm.e = ipd;
                     return wmctm;
                 }
-                //return  s_CTM_rltb.translate(ipd, 0);
+                //return  CTM_RLTB.translate(ipd, 0);
             case WritingMode.TB_RL: // CJK
                 {
-                    wmctm = new CTM(s_CTM_tbrl);
+                    wmctm = new CTM(CTM_TBRL);
                     wmctm.e = bpd;
                     return wmctm;
                 }
-                //return s_CTM_tbrl.translate(0, ipd);
+                //return CTM_TBRL.translate(0, ipd);
             default:
                 return null;
         }
--- src/org/apache/fop/layoutmgr/TraitSetter.java
+++ src/org/apache/fop/layoutmgr/TraitSetter.java
@@ -1,10 +1,53 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.layoutmgr;
 
 import org.apache.fop.layout.BorderAndPadding;
@@ -13,8 +56,16 @@ import org.apache.fop.area.Area;
 import org.apache.fop.area.Trait;
 import org.apache.fop.layout.BackgroundProps;
 
+/**
+ * This is a helper class used for setting common traits on areas.
+ */
 public class TraitSetter {
 
+    /**
+     * Sets border and padding traits on areas.
+     * @param area area to set the traits on
+     * @param bpProps border and padding properties
+     */
     public static void setBorderPaddingTraits(Area area,
             BorderAndPadding bpProps, boolean bNotFirst, boolean bNotLast) {
         int iBP;
@@ -52,8 +103,14 @@ public class TraitSetter {
                        Trait.BORDER_AFTER);
     }
 
+    /**
+     * Sets border traits on an area.
+     * @param area area to set the traits on
+     * @param bpProps border and padding properties
+     */
     private static void addBorderTrait(Area area,
-                                       BorderAndPadding bpProps, boolean bDiscard, int iSide,
+                                       BorderAndPadding bpProps, 
+                                       boolean bDiscard, int iSide,
                                        Object oTrait) {
         int iBP = bpProps.getBorderWidth(iSide, bDiscard);
         if (iBP > 0) {
@@ -71,22 +128,24 @@ public class TraitSetter {
      * Add borders to an area.
      * Layout managers that create areas with borders can use this to
      * add the borders to the area.
+     * @param curBlock area to set the traits on
+     * @param bordProps border properties
      */
     public static void addBorders(Area curBlock, BorderAndPadding bordProps) {
         BorderProps bps = getBorderProps(bordProps, BorderAndPadding.TOP);
-        if(bps.width != 0) {
+        if (bps.width != 0) {
             curBlock.addTrait(Trait.BORDER_BEFORE, bps);
         }
         bps = getBorderProps(bordProps, BorderAndPadding.BOTTOM);
-        if(bps.width != 0) {
+        if (bps.width != 0) {
             curBlock.addTrait(Trait.BORDER_AFTER, bps);
         }
         bps = getBorderProps(bordProps, BorderAndPadding.LEFT);
-        if(bps.width != 0) {
+        if (bps.width != 0) {
             curBlock.addTrait(Trait.BORDER_START, bps);
         }
         bps = getBorderProps(bordProps, BorderAndPadding.RIGHT);
-        if(bps.width != 0) {
+        if (bps.width != 0) {
             curBlock.addTrait(Trait.BORDER_END, bps);
         }
     }
@@ -103,23 +162,25 @@ public class TraitSetter {
      * Add background to an area.
      * Layout managers that create areas with a background can use this to
      * add the background to the area.
+     * @param curBlock the current block
+     * @param backProps the background properties
      */
     public static void addBackground(Area curBlock, BackgroundProps backProps) {
         Trait.Background back = new Trait.Background();
-        back.color = backProps.backColor;
+        back.setColor(backProps.backColor);
 
-        if(backProps.backImage != null) {
-            back.url = backProps.backImage;
-            back.repeat = backProps.backRepeat;
-            if(backProps.backPosHorizontal != null) {
-                back.horiz = backProps.backPosHorizontal.mvalue();
+        if (backProps.backImage != null) {
+            back.setURL(backProps.backImage);
+            back.setRepeat(backProps.backRepeat);
+            if (backProps.backPosHorizontal != null) {
+                back.setHoriz(backProps.backPosHorizontal.mvalue());
             }
-            if(backProps.backPosVertical != null) {
-                back.vertical = backProps.backPosVertical.mvalue();
+            if (backProps.backPosVertical != null) {
+                back.setVertical(backProps.backPosVertical.mvalue());
             }
         }
 
-        if(back.color != null || back.url != null) {
+        if (back.getColor() != null || back.getURL() != null) {
             curBlock.addTrait(Trait.BACKGROUND, back);
         }
     }
