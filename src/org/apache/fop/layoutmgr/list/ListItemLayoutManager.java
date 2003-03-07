f0f033dbfc06353937d8d031ef6ac7fa0d21b27e
Jeremias Maerki <jeremias@apache.org>
2003-03-07 07:58:52 +0000

    Switched to long licence Some general checkstyle fixing
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196037 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/layoutmgr/list/ListItemLayoutManager.java
+++ src/org/apache/fop/layoutmgr/list/ListItemLayoutManager.java
@@ -1,10 +1,53 @@
 /*
  * $Id$
- * Copyright (C) 2002 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.layoutmgr.list;
 
 import org.apache.fop.fo.PropertyManager;
@@ -56,16 +99,27 @@ public class ListItemLayoutManager extends BlockStackingLayoutManager {
     public ListItemLayoutManager() {
     }
 
+    /**
+     * @see org.apache.fop.layoutmgr.AbstractLayoutManager#initProperties(PropertyManager)
+     */
     protected void initProperties(PropertyManager propMgr) {
         borderProps = propMgr.getBorderAndPadding();
         backgroundProps = propMgr.getBackgroundProps();
     }
 
+    /**
+     * Sets the label of the list item
+     * @param item the label item
+     */
     public void setLabel(Item item) {
         label = item;
         label.setParent(this);
     }
 
+    /**
+     * Sets the body of the list item
+     * @param item the body item
+     */
     public void setBody(Item item) {
         body = item;
         body.setParent(this);
@@ -94,7 +148,7 @@ public class ListItemLayoutManager extends BlockStackingLayoutManager {
         int stage = 0;
         boolean over = false;
         while (true) {
-            if(stage == 0) {
+            if (stage == 0) {
                 curLM = label;
             } else if (stage == 1) {
                 curLM = body;
@@ -205,7 +259,7 @@ public class ListItemLayoutManager extends BlockStackingLayoutManager {
                 breakPosIter = new BreakPossPosIter(cellsbr, 0, cellsbr.size());
 
                 while ((childLM = (Item)breakPosIter.getNextChildLM()) != null) {
-                    if(childLM == body) {
+                    if (childLM == body) {
                         childLM.setXOffset(24000);
                     }
                     childLM.addAreas(breakPosIter, lc);
@@ -264,7 +318,6 @@ public class ListItemLayoutManager extends BlockStackingLayoutManager {
      * This simply adds the area to the parent layout manager.
      *
      * @param childArea the child area
-     * @return unused
      */
     public void addChild(Area childArea) {
         if (curBlockArea != null) {
