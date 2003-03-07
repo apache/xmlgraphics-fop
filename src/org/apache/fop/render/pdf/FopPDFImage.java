a9174e65273dde4629aa3568ee031eb81e989e68
Jeremias Maerki <jeremias@apache.org>
2003-03-07 09:48:05 +0000

    Switched to long licence Some general checkstyle fixing
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196040 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/render/pdf/FopPDFImage.java
+++ src/org/apache/fop/render/pdf/FopPDFImage.java
@@ -1,10 +1,53 @@
 /*
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
- */
-
+ * ============================================================================
+ *                    The Apache Software License, Version 1.1
+ * ============================================================================
+ * 
+ * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modifica-
+ * tion, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment: "This product includes software
+ *    developed by the Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 
+ * 4. The names "FOP" and "Apache Software Foundation" must not be used to
+ *    endorse or promote products derived from this software without prior
+ *    written permission. For written permission, please contact
+ *    apache@apache.org.
+ * 
+ * 5. Products derived from this software may not be called "Apache", nor may
+ *    "Apache" appear in their name, without prior written permission of the
+ *    Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLU-
+ * DING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ============================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation and was originally created by
+ * James Tauber <jtauber@jtauber.com>. For more information on the Apache
+ * Software Foundation, please see <http://www.apache.org/>.
+ */ 
 package org.apache.fop.render.pdf;
 
 import org.apache.fop.pdf.PDFImage;
@@ -22,31 +65,45 @@ import org.apache.fop.image.EPSImage;
 
 import java.io.IOException;
 import java.awt.color.ColorSpace;
-import java.awt.color.ICC_ColorSpace;
 import java.awt.color.ICC_Profile;
 import java.util.Map;
 
+/**
+ * PDFImage implementation for the PDF renderer.
+ */
 public class FopPDFImage implements PDFImage {
-    FopImage fopImage;
-    PDFICCStream pdfICCStream = null;
-    PDFFilter pdfFilter = null;
-    String maskRef;
-    String softMaskRef;
-    boolean isPS = false;
-    Map filters;
-    String key;
+    
+    private FopImage fopImage;
+    private PDFICCStream pdfICCStream = null;
+    private PDFFilter pdfFilter = null;
+    private String maskRef;
+    private String softMaskRef;
+    private boolean isPS = false;
+    private Map filters;
+    private String key;
 
-    public FopPDFImage(FopImage im, String k) {
-        fopImage = im;
-        key = k;
+    /**
+     * Creates a new PDFImage from a FopImage
+     * @param image Image
+     * @param key XObject key
+     */
+    public FopPDFImage(FopImage image, String key) {
+        fopImage = image;
+        this.key = key;
         isPS = (fopImage instanceof EPSImage);
     }
 
-    // key to look up XObject
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getKey()
+     */
     public String getKey() {
-        return key;
+        // key to look up XObject
+        return this.key;
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#setup(PDFDocument)
+     */
     public void setup(PDFDocument doc) {
         filters = doc.getFilterMap();
         if ("image/jpeg".equals(fopImage.getMimeType())) {
@@ -64,47 +121,75 @@ public class FopPDFImage implements PDFImage {
         }
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#isPS()
+     */
     public boolean isPS() {
         return isPS;
     }
 
-    // image size
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getWidth()
+     */
     public int getWidth() {
         return fopImage.getWidth();
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getHeight()
+     */
     public int getHeight() {
         return fopImage.getHeight();
     }
 
-    // DeviceGray, DeviceRGB, or DeviceCMYK
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getColorSpace()
+     */
     public PDFColorSpace getColorSpace() {
+        // DeviceGray, DeviceRGB, or DeviceCMYK
         return toPDFColorSpace(fopImage.getColorSpace());
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getBitsPerPixel()
+     */
     public int getBitsPerPixel() {
         return fopImage.getBitsPerPixel();
     }
 
-    // For transparent images
+    /**
+     * @see org.apache.fop.pdf.PDFImage#isTransparent()
+     */
     public boolean isTransparent() {
         return fopImage.isTransparent();
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getTransparentColor()
+     */
     public PDFColor getTransparentColor() {
         return fopImage.getTransparentColor();
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getMask()
+     */
     public String getMask() {
         return maskRef;
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getSoftMask()
+     */
     public String getSoftMask() {
         return softMaskRef;
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getDataStream()
+     */
     public PDFStream getDataStream() throws IOException {
-        if(isPS) {
+        if (isPS) {
             return getPSDataStream();
         } else {
             // delegate the stream work to PDFStream
@@ -125,6 +210,11 @@ public class FopPDFImage implements PDFImage {
         }
     }
 
+    /**
+     * Returns a PDFStream for an EPS image.
+     * @return PDFStream the newly creates PDFStream
+     * @throws IOException in case of an I/O problem
+     */
     protected PDFStream getPSDataStream() throws IOException {
         int length = 0;
         int i = 0;
@@ -146,11 +236,11 @@ public class FopPDFImage implements PDFImage {
         preamble.append("/showpage {} def\n");
 
 
-        preamble.append((double)(1f / (double) bboxw) + " " +
-                        (double)(1f / (double) bboxh) + " scale\n");
+        preamble.append((double)(1f / (double) bboxw) + " "
+                      + (double)(1f / (double) bboxh) + " scale\n");
         preamble.append(-bbox[0] + " " + (-bbox[1]) + " translate\n");
-        preamble.append(bbox[0] + " " + bbox[1] + " " + bboxw + " " +
-                        bboxh + " rectclip\n");
+        preamble.append(bbox[0] + " " + bbox[1] + " " 
+                        + bboxw + " " + bboxh + " rectclip\n");
         preamble.append("newpath\n");
 
         StringBuffer post = new StringBuffer();
@@ -164,8 +254,9 @@ public class FopPDFImage implements PDFImage {
         byte[] postBytes = post.toString().getBytes();
         byte[] epsBytes = ((EPSImage)fopImage).getEPSImage();
         int epsLength = epsBytes.length;
-        byte[] imgData = new byte[preBytes.length + postBytes.length +
-                                  epsLength];
+        byte[] imgData = new byte[preBytes.length 
+                                + postBytes.length 
+                                + epsLength];
 
         System.arraycopy (preBytes, 0, imgData, 0, preBytes.length);
         System.arraycopy (epsBytes, 0, imgData,
@@ -181,12 +272,22 @@ public class FopPDFImage implements PDFImage {
         return imgStream;
     }
 
+    /**
+     * @see org.apache.fop.pdf.PDFImage#getICCStream()
+     */
     public PDFICCStream getICCStream() {
         return pdfICCStream;
     }
 
-    protected PDFColorSpace toPDFColorSpace(ColorSpace cs) {
-        if(cs == null) return null;
+    /**
+     * Converts a ColorSpace object to a PDFColorSpace object.
+     * @param cs ColorSpace instance
+     * @return PDFColorSpace new converted object
+     */
+    public static PDFColorSpace toPDFColorSpace(ColorSpace cs) {
+        if (cs == null) {
+            return null;
+        }
 
         PDFColorSpace pdfCS = new PDFColorSpace(0);
         switch(cs.getType()) {
