479658dbc0b8511b87820c52ab4d5939086f1da4
William Victor Mote <vmote@apache.org>
2003-03-11 04:08:17 +0000

    non-ASCII character choking JBuilder
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196057 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fonts/CIDFont.java
+++ src/org/apache/fop/fonts/CIDFont.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fonts;
 
 /**
@@ -61,33 +61,33 @@ public abstract class CIDFont extends CustomFont {
      * @return the name of the base font
      */
     public abstract String getCidBaseFont();
-    
+
     /**
      * Returns the type of the CID font.
      * @return the type of the CID font
      */
     public abstract CIDFontType getCIDType();
-    
+
     /**
      * Returns the name of the issuer of the font.
-     * @return a String identifying an issuer of character collections — 
+     * @return a String identifying an issuer of character collections -
      * for example, Adobe
      */
     public abstract String getRegistry();
-    
+
     /**
      * Returns a font name for use within a registry.
-     * @return a String that uniquely names a character collection issued by 
-     * a specific registry — for example, Japan1.
+     * @return a String that uniquely names a character collection issued by
+     * a specific registry - for example, Japan1.
      */
     public abstract String getOrdering();
-    
+
     /**
      * Returns the supplement number of the character collection.
      * @return the supplement number
      */
     public abstract int getSupplement();
-    
+
 
     // ---- Optional ----
     /**
