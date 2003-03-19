d4f3b7ee4000b533689e7f4af4bceff35d61cf54
Peter Bernard West <pbwest@apache.org>
2003-03-19 14:44:52 +0000

    Full LICENSE text.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@196122 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/fo/FONode.java
+++ src/org/apache/fop/fo/FONode.java
@@ -39,9 +39,54 @@ import java.lang.reflect.InvocationTargetException;
  * FONode.java
  * Created: Sat Nov 10 01:39:37 2001
  * $Id$
- * Copyright (C) 2001 The Apache Software Foundation. All rights reserved.
- * For details on use and redistribution please refer to the
- * LICENSE file included with these sources.
+ * 
+ *  ============================================================================
+ *                    The Apache Software License, Version 1.1
+ *  ============================================================================
+ *  
+ *  Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without modifica-
+ *  tion, are permitted provided that the following conditions are met:
+ *  
+ *  1. Redistributions of  source code must  retain the above copyright  notice,
+ *     this list of conditions and the following disclaimer.
+ *  
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *  
+ *  3. The end-user documentation included with the redistribution, if any, must
+ *     include  the following  acknowledgment:  "This product includes  software
+ *     developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+ *     Alternately, this  acknowledgment may  appear in the software itself,  if
+ *     and wherever such third-party acknowledgments normally appear.
+ *  
+ *  4. The names "FOP" and  "Apache Software Foundation"  must not be used to
+ *     endorse  or promote  products derived  from this  software without  prior
+ *     written permission. For written permission, please contact
+ *     apache@apache.org.
+ *  
+ *  5. Products  derived from this software may not  be called "Apache", nor may
+ *     "Apache" appear  in their name,  without prior written permission  of the
+ *     Apache Software Foundation.
+ *  
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ *  FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ *  APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ *  DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ *  OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ *  ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ *  (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ *  This software  consists of voluntary contributions made  by many individuals
+ *  on  behalf of the Apache Software  Foundation and was  originally created by
+ *  James Tauber <jtauber@jtauber.com>. For more  information on the Apache 
+ *  Software Foundation, please see <http://www.apache.org/>.
+ *  
  *
  * @author <a href="mailto:pbwest@powerup.com.au">Peter B. West</a>
  * @version $Revision$ $Name$
@@ -308,6 +353,19 @@ public class FONode extends Node{
         return prop.refineParsing(pv.getProperty(), this, pv);
     }
 
+    /**
+     * Reduce the properties currently associated with the node to a
+     * sparse propeties set of only those properties relevant to the node.
+     * During tree building, a node may have associated with it properties
+     * which have no direct relevance to the node, but which may be used
+     * by descendant nodes.  Once the tree building process is finished, these
+     * properties are no longer required.  The property set for the node can
+     * be reduced to the minimum required for this formatting object.
+     * This minimal set is maintained in a sparse array.
+     * @see sparsePropsSet
+     * @see sparsePropsMap
+     * @see sparseIndices
+     */
     public void makeSparsePropsSet() throws PropertyException {
         sparsePropsSet = new PropertyValue[numProps];
         // Scan the sparseIndices array, and copy the PropertyValue from
@@ -323,6 +381,26 @@ public class FONode extends Node{
         foProperties = null;
         foAttributes = null;
     }
+
+    /**
+     * Get the <i>sparsePropsSet</i> for this node.
+     * @return the <tt>PropertyValue[]</tt>.
+     */
+    /*  DEBUG
+    public PropertyValue[] getSparsePropsSet() {
+        return sparsePropsSet;
+    }
+    */
+
+    /**
+     * Get the <i>sparsePropsMap</i> for this node.
+     * @return the <tt>int[]</tt>.
+     */
+    /*  DEBUG
+    public int[] getSparsePropsMap() {
+        return sparsePropsMap;
+    }
+    */
 
     /**
      * Get the eclosing <tt>FOTree</tt> instance of this <tt>FONode</tt>.
