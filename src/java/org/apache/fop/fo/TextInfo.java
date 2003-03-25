1450b3da910352ff50ab103168b49a394263bfdd
William Victor Mote <vmote@apache.org>
2003-03-25 23:34:11 +0000

    Add support for text-transform.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196141 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/TextInfo.java
+++ src/java/org/apache/fop/fo/TextInfo.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,18 +42,19 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo;
 
 // FOP
 import org.apache.fop.layout.FontState;
 import org.apache.fop.datatypes.ColorType;
 import org.apache.fop.traits.SpaceVal;
+import org.apache.fop.fo.properties.TextTransform;
 
 /**
  * Collection of properties used in
@@ -66,6 +67,7 @@ public class TextInfo {
     public int whiteSpaceCollapse;
     public int verticalAlign;
     public int lineHeight;
+    public int textTransform = TextTransform.NONE;
 
     // Props used for calculating inline-progression-dimension
     public SpaceVal wordSpacing;
@@ -78,5 +80,6 @@ public class TextInfo {
     public boolean underlined = false;
     public boolean overlined = false;
     public boolean lineThrough = false;
+
 }
 
