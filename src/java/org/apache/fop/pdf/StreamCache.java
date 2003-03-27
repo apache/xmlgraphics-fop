49cdb7591d85a75b053806514c18d19e03eae8f5
Jeremias Maerki <jeremias@apache.org>
2003-03-27 10:27:27 +0000

    Streamlined. Do some simple memory allocation optimization.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196155 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/pdf/StreamCache.java
+++ src/java/org/apache/fop/pdf/StreamCache.java
@@ -54,96 +54,51 @@ import java.io.OutputStream;
 import java.io.IOException;
 
 /**
- * class used to store the bytes for a PDFStream. It's actually a generic
- * cached byte array, along with a factory that returns either an
- * in-memory or tempfile based implementation based on the global
+ * Interface used to store the bytes for a PDFStream. It's actually a generic
+ * cached byte array. There's a factory that returns either an
+ * in-memory or tempfile based implementation based on a
  * cacheToFile setting.
  */
-public abstract class StreamCache {
-
-    /**
-     * Global setting; controls whether to use tempfiles or not.
-     */
-    private static boolean cacheToFile = false;
-
-    /**
-     * Change the global cacheToFile flag.
-     *
-     * @param tizit true if cache to file
-     */
-    public static void setCacheToFile(boolean tizit) {
-        cacheToFile = tizit;
-    }
-
-    /**
-     * Get the value of the global cacheToFile flag.
-     *
-     * @return the current cache to file flag
-     */
-    public static boolean getCacheToFile() {
-        return cacheToFile;
-    }
-
-    /**
-     * Get the correct implementation (based on cacheToFile) of
-     * StreamCache.
-     *
-     * @throws IOException if there is an IO error
-     * @return a new StreamCache for caching streams
-     */
-    public static StreamCache createStreamCache() throws IOException {
-        if (cacheToFile) {
-            return new TempFileStreamCache();
-        } else {
-            return new InMemoryStreamCache();
-        }
-    }
+public interface StreamCache {
 
     /**
      * Get the current OutputStream. Do not store it - it may change
      * from call to call.
      *
-     * @throws IOException if there is an IO error
      * @return an output stream for this cache
+     * @throws IOException if there is an IO error
      */
-    public abstract OutputStream getOutputStream() throws IOException;
+    OutputStream getOutputStream() throws IOException;
 
     /**
-     * Filter the cache with the supplied PDFFilter.
-     *
-     * @param filter the filter to apply
+     * Convenience method for writing data to the stream cache.
+     * @param data byte array to write
      * @throws IOException if there is an IO error
      */
-    public abstract void applyFilter(PDFFilter filter) throws IOException;
+    void write(byte[] data) throws IOException;
 
     /**
      * Outputs the cached bytes to the given stream.
      *
-     * @param stream the stream to write to
+     * @param out the stream to write to
+     * @return the number of bytes written
      * @throws IOException if there is an IO error
      */
-    public abstract void outputStreamData(OutputStream stream) throws IOException;
+    int outputContents(OutputStream out) throws IOException;
 
     /**
      * Returns the current size of the stream.
      *
-     * @throws IOException if there is an IO error
      * @return the size of the cache
-     */
-    public abstract int getSize() throws IOException;
-
-    /**
-     * Closes the cache and frees resources.
-     *
      * @throws IOException if there is an IO error
      */
-    public abstract void close() throws IOException;
+    int getSize() throws IOException;
 
     /**
      * Clears and resets the cache.
      *
      * @throws IOException if there is an IO error
      */
-    public abstract void reset() throws IOException;
+    void clear() throws IOException;
 }
 
