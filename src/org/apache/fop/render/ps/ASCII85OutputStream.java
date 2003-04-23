38236861596fe728c82418cdd8accba6e642ce65
Jeremias Maerki <jeremias@apache.org>
2003-04-23 20:50:04 +0000

    Fix bug where EOF marker got split over two lines resulting in an illegal stream. Submitted by: Larry Moore <lmoore@providerlink.com>
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/fop-0_20_2-maintain@196328 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/fop/render/ps/ASCII85OutputStream.java
+++ src/org/apache/fop/render/ps/ASCII85OutputStream.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.render.ps;
 
 import java.io.OutputStream;
@@ -119,13 +119,23 @@ public class ASCII85OutputStream extends FilterOutputStream
 
 
     private void checkedWrite(byte[] buf) throws IOException {
-        checkedWrite(buf, buf.length);
+        checkedWrite(buf, buf.length, false);
+    }
+
+
+    private void checkedWrite(byte[] buf, boolean nosplit) throws IOException {
+        checkedWrite(buf, buf.length, nosplit);
     }
 
 
     private void checkedWrite(byte[] buf , int len) throws IOException {
+        checkedWrite(buf, len, false);
+    }
+
+
+    private void checkedWrite(byte[] buf , int len, boolean nosplit) throws IOException {
         if (posinline + len > 80) {
-            int firstpart = len - (posinline + len - 80);
+            int firstpart = (nosplit ? 0 : len - (posinline + len - 80));
             if (firstpart > 0) out.write(buf, 0, firstpart);
             out.write(EOL); bw++;
             int rest = len - firstpart;
@@ -226,7 +236,7 @@ public class ASCII85OutputStream extends FilterOutputStream
             checkedWrite(conv, rest + 1);
         }
         // finally write the two character end of data marker
-        checkedWrite(EOD);
+        checkedWrite(EOD, true);
 
         flush();
         if (out instanceof Finalizable) {
