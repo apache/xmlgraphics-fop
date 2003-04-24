3b05fa0eb73a6cefc2a642286244f597c89440d5
William Victor Mote <vmote@apache.org>
2003-04-24 02:48:08 +0000

    javadoc changes to clean up gump warnings.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196335 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fonts/truetype/TTFFile.java
+++ src/java/org/apache/fop/fonts/truetype/TTFFile.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fonts.truetype;
 
 import java.io.IOException;
@@ -129,7 +129,7 @@ public class TTFFile extends AbstractLogEnabled {
 
     private int ansiWidth[];
     private Map ansiIndex;
-    
+
     private TTFDirTabEntry currentDirTab;
 
     /**
@@ -745,8 +745,8 @@ public class TTFFile extends AbstractLogEnabled {
 
     /**
      * Read the "hhea" table to find the ascender and descender and
-     * size of "hmtx" table, i.e. a fixed size font might have only
-     * one width
+     * size of "hmtx" table, as a fixed size font might have only
+     * one width.
      * @param in FontFileReader to read the hhea table from
      * @throws IOException in case of an I/O problem
      */
@@ -841,12 +841,12 @@ public class TTFFile extends AbstractLogEnabled {
         case 0x00020000:
             getLogger().debug("PostScript format 2");
             int numGlyphStrings = 0;
-            
+
             // Read Number of Glyphs
-            int l = in.readTTFUShort();      
-            
+            int l = in.readTTFUShort();
+
             // Read indexes
-            for (int i = 0; i < l; i++) {    
+            for (int i = 0; i < l; i++) {
                 mtxTab[i].setIndex(in.readTTFUShort());
 
                 if (mtxTab[i].getIndex() > 257) {
@@ -858,7 +858,7 @@ public class TTFFile extends AbstractLogEnabled {
                     getLogger().debug("PostScript index: " + mtxTab[i].getIndexAsString());
                 }
             }
-            
+
             // firstChar=minIndex;
             String[] psGlyphsBuffer = new String[numGlyphStrings];
             if (getLogger().isDebugEnabled()) {
