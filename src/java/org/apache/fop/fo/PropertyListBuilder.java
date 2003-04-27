fd8439f329882689ecc9e7314c3a6b3f4f648d67
William Victor Mote <vmote@apache.org>
2003-04-27 23:48:02 +0000

    Refactor: make local variable names more meaningful.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196357 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/PropertyListBuilder.java
+++ src/java/org/apache/fop/fo/PropertyListBuilder.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo;
 
 // Java
@@ -117,20 +117,21 @@ public class PropertyListBuilder {
         return b;
     }
 
-    public PropertyList makeList(String ns, String elementName, Attributes attributes,
+    public PropertyList makeList(String nameSpaceURI, String elementName,
+                                 Attributes attributes,
                                  PropertyList parentPropertyList,
                                  FObj parentFO) throws FOPException {
-        String space = "http://www.w3.org/TR/1999/XSL/Format";
-        if (ns != null) {
-            space = ns;
+        String nameSpaceURIToUse = "http://www.w3.org/TR/1999/XSL/Format";
+        if (nameSpaceURI != null) {
+            nameSpaceURIToUse = nameSpaceURI;
         }
 
         PropertyList par = null;
         if (parentPropertyList != null
-                && space.equals(parentPropertyList.getNameSpace())) {
+                && nameSpaceURIToUse.equals(parentPropertyList.getNameSpace())) {
             par = parentPropertyList;
         }
-        PropertyList p = new PropertyList(par, space,
+        PropertyList p = new PropertyList(par, nameSpaceURIToUse,
                                           elementName);
         p.setBuilder(this);
         HashMap table;
