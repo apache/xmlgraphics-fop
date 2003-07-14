0998ab06a070b1e6adcc9362639a49546a177462
William Victor Mote <vmote@apache.org>
2003-07-14 01:53:41 +0000

    style changes only
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196700 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/BlockContainer.java
+++ src/java/org/apache/fop/fo/flow/BlockContainer.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo.flow;
 
 // FOP
@@ -65,6 +65,10 @@ import org.xml.sax.Attributes;
 
 import java.util.List;
 
+/**
+ * Class modelling the fo:block-container object. See Sec. 6.5.3 of the XSL-FO
+ * Standard.
+ */
 public class BlockContainer extends FObj {
 
     private ColorType backgroundColor;
@@ -79,16 +83,25 @@ public class BlockContainer extends FObj {
 
     private int span;
 
+    /**
+     * @param parent FONode that is the parent of this object
+     */
     public BlockContainer(FONode parent) {
         super(parent);
     }
 
+    /**
+     * @see org.apache.fop.fo.FObj#handleAttrs
+     */
     public void handleAttrs(Attributes attlist) throws FOPException {
         super.handleAttrs(attlist);
         this.span = this.properties.get("span").getEnum();
         setupID();
     }
 
+    /**
+     * @see org.apache.fop.fo.FObj#addLayoutManager
+     */
     public void addLayoutManager(List list) {
         BlockContainerLayoutManager blm = new BlockContainerLayoutManager();
         blm.setUserAgent(getUserAgent());
@@ -97,7 +110,7 @@ public class BlockContainer extends FObj {
         list.add(blm);
     }
 
-    public void setup() {
+    private void setup() {
 
             // Common Accessibility Properties
             AbsolutePositionProps mAbsProps = propMgr.getAbsolutePositionProps();
@@ -134,18 +147,30 @@ public class BlockContainer extends FObj {
 
     }
 
+    /**
+     * @return true (BlockContainer can generate Reference Areas)
+     */
     public boolean generatesReferenceAreas() {
         return true;
     }
 
+    /**
+     * @return false (BlockContainer cannot generate inline areas)
+     */
     public boolean generatesInlineAreas() {
         return false;
     }
 
+    /**
+     * @return true (BlockContainer can contain Markers)
+     */
     protected boolean containsMarkers() {
         return true;
     }
 
+    /**
+     * @return the span for this object
+     */
     public int getSpan() {
         return this.span;
     }
