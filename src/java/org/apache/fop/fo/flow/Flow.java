9b418cc52c482f13bf1508f4960e95c7cce6443d
William Victor Mote <vmote@apache.org>
2003-07-14 01:12:53 +0000

    style changes only
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196697 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/Flow.java
+++ src/java/org/apache/fop/fo/flow/Flow.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo.flow;
 
 // Java
@@ -64,6 +64,9 @@ import org.apache.fop.fo.pagination.PageSequence;
 import org.apache.fop.apps.FOPException;
 import org.apache.fop.layoutmgr.FlowLayoutManager;
 
+/**
+ * Class modelling the fo:flow object. See Sec. 6.4.18 in the XSL-FO Standard.
+ */
 public class Flow extends FObj {
 
     /**
@@ -86,11 +89,18 @@ public class Flow extends FObj {
      */
     private int contentWidth;
 
-
+    /**
+     * @param parent FONode that is the parent of this object
+     */
     public Flow(FONode parent) {
         super(parent);
     }
 
+    /**
+     * @see org.apache.fop.fo.FObj#handleAttrs
+     * @param attlist Collection of attributes passed to us from the parser.
+     * @throws FOPException if parent is not a page-sequence object
+     */
     public void handleAttrs(Attributes attlist) throws FOPException {
         super.handleAttrs(attlist);
         if (parent.getName().equals("fo:page-sequence")) {
@@ -121,10 +131,17 @@ public class Flow extends FObj {
         structHandler.startFlow(this);
     }
 
+    /**
+     * Tell the StructureRenderer that we are at the end of the flow.
+     */
     public void end() {
         structHandler.endFlow(this);
     }
 
+    /**
+     * @param name the name of the flow to set
+     * @throws FOPException for an empty name
+     */
     protected void setFlowName(String name) throws FOPException {
         if (name == null || name.equals("")) {
             throw new FOPException("A 'flow-name' is required for "
@@ -134,25 +151,37 @@ public class Flow extends FObj {
         }
     }
 
+    /**
+     * @return the name of this flow
+     */
     public String getFlowName() {
         return flowName;
     }
 
+    /**
+     * @param contentWidth content width of this flow, in millipoints (??)
+     */
     protected void setContentWidth(int contentWidth) {
         this.contentWidth = contentWidth;
     }
     /**
-     * Return the content width of this flow (really of the region
-     * in which it is flowing).
+     * @return the content width of this flow (really of the region
+     * in which it is flowing), in millipoints (??).
      */
     public int getContentWidth() {
         return this.contentWidth;
     }
 
+    /**
+     * @return true (Flow can generate reference areas)
+     */
     public boolean generatesReferenceAreas() {
         return true;
     }
 
+    /**
+     * @see org.apache.fop.fo.FObj#addLayoutManager
+     */
     public void addLayoutManager(List list) {
         FlowLayoutManager lm = new FlowLayoutManager();
         lm.setUserAgent(getUserAgent());
--- src/java/org/apache/fop/fo/flow/Footnote.java
+++ src/java/org/apache/fop/fo/flow/Footnote.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo.flow;
 
 // Java
@@ -57,15 +57,25 @@ import java.util.List;
 import org.apache.fop.fo.FONode;
 import org.apache.fop.fo.FObj;
 
+/**
+ * Class modelling the fo:footnote object. See Sec. 6.10.3 of the XSL-FO
+ * Standard.
+ */
 public class Footnote extends FObj {
-    
+
     private Inline inlineFO = null;
     private FootnoteBody body;
 
+    /**
+     * @param parent FONode that is the parent of this object
+     */
     public Footnote(FONode parent) {
         super(parent);
     }
 
+    /**
+     * @see org.apache.fop.fo.FObj#addLayoutManager
+     */
     public void addLayoutManager(List lms) {
         // add inlines layout manager
         if (inlineFO == null) {
@@ -75,6 +85,9 @@ public class Footnote extends FObj {
         inlineFO.addLayoutManager(lms);
     }
 
+    /**
+     * @param child child FONode to be added to this object
+     */
     public void addChild(FONode child) {
         String name = child.getName();
         if ("fo:inline".equals(name)) {
