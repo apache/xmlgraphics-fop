89063f4f2f7fdb0623765899f649a6359dda1e0e
William Victor Mote <vmote@apache.org>
2003-07-14 01:12:53 +0000

    style changes only
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196697 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/Inline.java
+++ src/java/org/apache/fop/fo/flow/Inline.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo.flow;
 
 // XML
@@ -67,18 +67,29 @@ import org.apache.fop.layout.MarginInlineProps;
 import org.apache.fop.layout.RelativePositionProps;
 import org.apache.fop.apps.FOPException;
 
+/**
+ * Class modelling the fo:inline object. See Sec. 6.6.7 of the XSL-FO Standard.
+ */
 public class Inline extends FObjMixed {
 
     // Textdecoration
+    /** is this text underlined? */
     protected boolean underlined = false;
+    /** is this text overlined? */
     protected boolean overlined = false;
+    /** is this text lined through? */
     protected boolean lineThrough = false;
 
-
+    /**
+     * @param parent FONode that is the parent of this object
+     */
     public Inline(FONode parent) {
         super(parent);
     }
 
+    /**
+     * @see org.apache.fop.fo.FObj#handleAttrs
+     */
     public void handleAttrs(Attributes attlist) throws FOPException {
         super.handleAttrs(attlist);
 
@@ -136,10 +147,16 @@ public class Inline extends FObjMixed {
         }
     }
 
+    /**
+     * @return true (Inline can contain Markers)
+     */
     protected boolean containsMarkers() {
         return true;
     }
 
+    /**
+     * @see org.apache.fop.fo.FObjMixed#charIterator
+     */
     public CharIterator charIterator() {
         return new InlineCharIterator(this, propMgr.getBorderAndPadding());
     }
