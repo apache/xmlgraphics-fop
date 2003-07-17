8dae4ba006049cc7848f8579b91024d22bc674be
William Victor Mote <vmote@apache.org>
2003-07-17 07:21:08 +0000

    style changes only
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196723 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/LengthRangeProperty.java
+++ src/java/org/apache/fop/fo/LengthRangeProperty.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,20 +42,29 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo;
 
 import org.apache.fop.datatypes.LengthRange;
 
+/**
+ * Superclass for properties that contain LengthRange values
+ */
 public class LengthRangeProperty extends Property {
 
+    /**
+     * Inner class for a Maker for LengthProperty objects
+     */
     public static class Maker extends LengthProperty.Maker {
 
+        /**
+         * @param name name of property for which to create Maker
+         */
         protected Maker(String name) {
             super(name);
         }
@@ -64,14 +73,23 @@ public class LengthRangeProperty extends Property {
 
     private LengthRange lengthRange;
 
+    /**
+     * @param lengthRange LengthRange object to wrap in this
+     */
     public LengthRangeProperty(LengthRange lengthRange) {
         this.lengthRange = lengthRange;
     }
 
+    /**
+     * @return this.lengthRange
+     */
     public LengthRange getLengthRange() {
         return this.lengthRange;
     }
 
+    /**
+     * @return this.lengthRange cast as an Object
+     */
     public Object getObject() {
         return this.lengthRange;
     }
--- src/java/org/apache/fop/fo/NumberProperty.java
+++ src/java/org/apache/fop/fo/NumberProperty.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,25 +42,38 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.fo;
 
 import org.apache.fop.datatypes.ColorType;
 import org.apache.fop.fo.expr.Numeric;
 
+/**
+ * Class for handling numeric properties
+ */
 public class NumberProperty extends Property {
 
+    /**
+     * Inner class for making NumberProperty objects
+     */
     public static class Maker extends Property.Maker {
 
+        /**
+         * Constructor for NumberProperty.Maker
+         * @param propName the name of the property
+         */
         public Maker(String propName) {
             super(propName);
         }
 
+        /**
+         * @see Property#convertProperty
+         */
         public Property convertProperty(Property p,
                                         PropertyList propertyList, FObj fo) {
             if (p instanceof NumberProperty) {
@@ -77,18 +90,33 @@ public class NumberProperty extends Property {
 
     private Number number;
 
+    /**
+     * Constructor for Number input
+     * @param num Number object value for property
+     */
     public NumberProperty(Number num) {
         this.number = num;
     }
 
+    /**
+     * Constructor for double input
+     * @param num double numeric value for property
+     */
     public NumberProperty(double num) {
         this.number = new Double(num);
     }
 
+    /**
+     * Constructor for integer input
+     * @param num integer numeric value for property
+     */
     public NumberProperty(int num) {
         this.number = new Integer(num);
     }
 
+    /**
+     * @return this.number cast as a Number
+     */
     public Number getNumber() {
         return this.number;
     }
@@ -102,14 +130,25 @@ public class NumberProperty extends Property {
      * }
      */
 
+    /**
+     * @return this.number cast as an Object
+     */
     public Object getObject() {
         return this.number;
     }
 
+    /**
+     * Convert NumberProperty to Numeric object
+     * @return Numeric object corresponding to this
+     */
     public Numeric getNumeric() {
         return new Numeric(this.number);
     }
 
+    /**
+     * Convert NumberProperty to a ColorType. Not sure why this is needed.
+     * @return ColorType that corresponds to black
+     */
     public ColorType getColorType() {
         // Convert numeric value to color ???
         // Convert to hexadecimal and then try to make it into a color?
