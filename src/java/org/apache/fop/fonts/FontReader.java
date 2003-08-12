33d8eb8cac1c473f570b2606cf253ab3b05d7e19
William Victor Mote <vmote@apache.org>
2003-08-12 18:27:35 +0000

    move render.pdf.FontReader to fonts.FontReader
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196791 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fonts/FontReader.java
+++ src/java/org/apache/fop/fonts/FontReader.java
@@ -1,36 +1,36 @@
 /*
- * $Id: FontReader.java,v 1.8 2003/03/07 09:46:32 jeremias Exp $
+ * $Id$
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,13 +42,13 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
-package org.apache.fop.render.pdf;
+ */
+package org.apache.fop.fonts;
 
 //Java
 import java.util.List;
@@ -66,13 +66,6 @@ import org.xml.sax.helpers.DefaultHandler;
 
 //FOP
 import org.apache.fop.apps.FOPException;
-import org.apache.fop.fonts.BFEntry;
-import org.apache.fop.fonts.CIDFontType;
-import org.apache.fop.fonts.CustomFont;
-import org.apache.fop.fonts.Font;
-import org.apache.fop.fonts.FontType;
-import org.apache.fop.fonts.MultiByteFont;
-import org.apache.fop.fonts.SingleByteFont;
 
 /**
  * Class for reading a metric.xml file and creating a font object.
@@ -85,7 +78,7 @@ import org.apache.fop.fonts.SingleByteFont;
  * </pre>
  */
 public class FontReader extends DefaultHandler {
-    
+
     private Locator locator = null;
     private boolean isCID = false;
     private CustomFont returnFont = null;
@@ -226,7 +219,7 @@ public class FontReader extends DefaultHandler {
             //singleFont.width = new int[256];
         } else if ("char".equals(localName)) {
             try {
-                singleFont.setWidth(Integer.parseInt(attributes.getValue("idx")), 
+                singleFont.setWidth(Integer.parseInt(attributes.getValue("idx")),
                         Integer.parseInt(attributes.getValue("wdt")));
             } catch (NumberFormatException ne) {
                 System.out.println("Malformed width in metric file: "
