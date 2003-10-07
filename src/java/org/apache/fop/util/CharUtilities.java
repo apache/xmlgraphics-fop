0e840c180251e8a8bead5d8a55e5886c1de5977c
William Victor Mote <vmote@apache.org>
2003-10-07 00:43:06 +0000

    port layout/FontState.getWordWidth from maintenance branch
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@196947 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/util/CharUtilities.java
+++ src/java/org/apache/fop/util/CharUtilities.java
@@ -3,34 +3,34 @@
  * ============================================================================
  *                    The Apache Software License, Version 1.1
  * ============================================================================
- * 
+ *
  * Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modifica-
  * tion, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if any, must
  *    include the following acknowledgment: "This product includes software
  *    developed by the Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself, if
  *    and wherever such third-party acknowledgments normally appear.
- * 
+ *
  * 4. The names "FOP" and "Apache Software Foundation" must not be used to
  *    endorse or promote products derived from this software without prior
  *    written permission. For written permission, please contact
  *    apache@apache.org.
- * 
+ *
  * 5. Products derived from this software may not be called "Apache", nor may
  *    "Apache" appear in their name, without prior written permission of the
  *    Apache Software Foundation.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
  * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many individuals
  * on behalf of the Apache Software Foundation and was originally created by
  * James Tauber <jtauber@jtauber.com>. For more information on the Apache
  * Software Foundation, please see <http://www.apache.org/>.
- */ 
+ */
 package org.apache.fop.util;
 
 import org.apache.fop.fonts.Font;
@@ -58,7 +58,7 @@ import org.apache.fop.fonts.Font;
  */
 public class CharUtilities {
 
-    /** 
+    /**
      * Character code used to signal a character boundary in
      * inline content, such as an inline with borders and padding
      * or a nested block object.
@@ -93,7 +93,7 @@ public class CharUtilities {
     protected CharUtilities() {
         throw new UnsupportedOperationException();
     }
-    
+
     /**
      * Return the appropriate CharClass constant for the type
      * of the passed character.
@@ -184,6 +184,22 @@ public class CharUtilities {
             }
         }
 
+        return width;
+    }
+
+    /**
+     * Calculates the word width.
+     */
+    public static int getWordWidth(String word, Font fs) {
+        if (word == null)
+            return 0;
+        int wordLength = word.length();
+        int width = 0;
+        char[] characters = new char[wordLength];
+        word.getChars(0, wordLength, characters, 0);
+        for (int i = 0; i < wordLength; i++) {
+            width += getCharWidth(characters[i], fs);
+        }
         return width;
     }
 
