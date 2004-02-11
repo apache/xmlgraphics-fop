6ee111a719168dfd1c5d722dc912ee3f896b08bf
Glen Mazza <gmazza@apache.org>
2004-02-11 22:48:32 +0000

    Modification made for collecting parameters to be compatible (hopefully) with Win9x.  Work from Apache Ant.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@197352 13f79535-47bb-0310-9956-ffa450edef68

--- fop.bat
+++ fop.bat
@@ -4,6 +4,23 @@ rem %~dp0 is the expanded pathname of the current script under NT
 set LOCAL_FOP_HOME=
 if "%OS%"=="Windows_NT" set LOCAL_FOP_HOME=%~dp0
 
+rem Code from Apache Ant project
+rem Slurp the command line arguments. This loop allows for an unlimited number
+rem of arguments (up to the command line limit, anyway).
+rem Could also do a "shift" and "%*" for all params, but apparently doesn't work 
+rem with Win9x.
+set FOP_CMD_LINE_ARGS=%1
+if ""%1""=="""" goto doneStart
+shift
+:setupArgs
+if ""%1""=="""" goto doneStart
+set FOP_CMD_LINE_ARGS=%FOP_CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+rem This label provides a place for the argument list loop to break out 
+rem and for NT handling to skip to.
+:doneStart
+
 set LIBDIR=%LOCAL_FOP_HOME%lib
 set LOCALCLASSPATH=%LOCAL_FOP_HOME%build\fop.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\xml-apis.jar
@@ -15,7 +32,6 @@ set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\commons-io-dev-20040206.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jimi-1.0.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jai_core.jar
 set LOCALCLASSPATH=%LOCALCLASSPATH%;%LIBDIR%\jai_codec.jar
-rem 'shift' removes %0 (i.e., the fop.bat filename)
-shift
-java -cp "%LOCALCLASSPATH%" org.apache.fop.apps.Fop %*
+
+java -cp "%LOCALCLASSPATH%" org.apache.fop.apps.Fop %FOP_CMD_LINE_ARGS%
 
