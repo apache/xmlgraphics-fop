aa3fe945aca3540dd46f81a2ee325f6c6f21b408
Peter Bernard West <pbwest@apache.org>
2004-02-25 23:40:29 +0000

    Cleaned up Javadoc link to inaccessible method.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@197376 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/FoTableRow.java
+++ src/java/org/apache/fop/fo/flow/FoTableRow.java
@@ -50,8 +50,7 @@ public class FoTableRow extends FOPageSeqNode {
 
     /** Map of <tt>Integer</tt> indices of <i>sparsePropsSet</i> array.
         It is indexed by the FO index of the FO associated with a given
-        position in the <i>sparsePropsSet</i> array. See
-        {@link org.apache.fop.fo.FONode#sparsePropsSet FONode.sparsePropsSet}.
+        position in the <i>sparsePropsSet</i> array.
      */
     private static final int[] sparsePropsMap;
 
