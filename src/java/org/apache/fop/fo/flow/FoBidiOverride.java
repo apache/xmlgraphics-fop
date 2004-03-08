4526cb27a9eb58c9c0fe90d35ce257c8863bb9d6
Peter Bernard West <pbwest@apache.org>
2004-03-08 21:14:50 +0000

    Logger name changed to 'log' in superclass
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@197429 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/FoBidiOverride.java
+++ src/java/org/apache/fop/fo/flow/FoBidiOverride.java
@@ -132,7 +132,7 @@ public class FoBidiOverride extends FOPageSeqNode {
                 }
             } catch(UnexpectedStartElementException e) {
                 ev = xmlevents.getStartElement();
-                logger.warning
+                log.warning
                         ("Ignoring unexpected Start Element: "
                                                          + ev.getQName());
                 ev = xmlevents.getEndElement(
--- src/java/org/apache/fop/fo/flow/FoMultiToggle.java
+++ src/java/org/apache/fop/fo/flow/FoMultiToggle.java
@@ -126,7 +126,7 @@ public class FoMultiToggle extends FOPageSeqNode {
                     }
                 } catch(UnexpectedStartElementException e) {
                     ev = xmlevents.getStartElement();
-                    logger.warning
+                    log.warning
                     ("Ignoring unexpected Start Element: "
                             + ev.getQName());
                     ev = xmlevents.getEndElement(
