c467eb90e46424b611cdd078d297c8b5a13b4963
Peter Bernard West <pbwest@apache.org>
2004-03-08 21:40:41 +0000

    Updated copyright dates
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@197430 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/configuration/Configuration.java
+++ src/java/org/apache/fop/configuration/Configuration.java
@@ -1,6 +1,5 @@
 /*
- *
- * Copyright 1999-2003 The Apache Software Foundation.
+ * Copyright 1999-2004 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *  
  * $Id$
  */
 
