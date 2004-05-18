f1456da09ee8778ff74c38462a6997f02cbc9048
Chris Bowditch <cbowditch@apache.org>
2004-05-18 10:12:38 +0000

    added ipd property to block element
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@197604 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/xml/XMLRenderer.java
+++ src/java/org/apache/fop/render/xml/XMLRenderer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-/* $Id$ */
+/* $Id: XMLRenderer.java,v 1.20 2004/04/25 04:45:28 gmazza Exp $ */
 
 package org.apache.fop.render.xml;
 
@@ -332,7 +332,8 @@ public class XMLRenderer extends AbstractRenderer {
      * @see org.apache.fop.render.AbstractRenderer#renderBlock(Block)
      */
     protected void renderBlock(Block block) {
-        String prop = " width=\"" + block.getWidth() + 
+        String prop = " width=\"" + block.getWidth() +
+                      "\" ipd=\"" + block.getIPD() +
                       "\" height=\"" + block.getHeight() + "\"";
         Map map = block.getTraits();
         if (map != null) {
