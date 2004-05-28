a59b1f163550b836cc2d5f183b7a8e66f66a01ac
Peter Bernard West <pbwest@apache.org>
2004-05-28 15:21:01 +0000

    For automatically generated line-areas
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@197645 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/area/LineArea.java
+++ src/java/org/apache/fop/area/LineArea.java
@@ -1,115 +1,43 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
- * 
+ *
+ * Copyright 2004 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Created on 28/05/2004
+ * $Id$
  */
-
-/* $Id$ */
- 
 package org.apache.fop.area;
 
-import org.apache.fop.area.inline.InlineArea;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.fop.datastructs.Node;
+import org.apache.fop.fo.FONode;
+import org.apache.fop.fo.flow.FoPageSequence;
 
 /**
- * The line area.
- * This is a line area that contains inline areas.
+ * @author pbw
+ * @version $Revision$ $Name$
  */
-public class LineArea extends Area {
-    private int stacking = LR;
-    // contains inline areas
-    // has start indent and length, dominant baseline, height
-    private int startIndent;
-    private int length;
-
-    private int lineHeight;
-    // this is the offset for the dominant baseline
-    private int baseLine;
-
-    // this class can contain the dominant char styling info
-    // this means that many renderers can optimise a bit
-
-    private List inlineAreas = new ArrayList();
-
+public class LineArea extends BlockArea {
     /**
-     * Set the height of this line area.
-     *
-     * @param height the height of the line area
+     * @param pageSeq
+     * @param generatedBy
+     * @param parent
+     * @param sync
      */
-    public void setHeight(int height) {
-        lineHeight = height;
+    public LineArea(FoPageSequence pageSeq, FONode generatedBy, Node parent,
+            Object sync) {
+        super(pageSeq, generatedBy, parent, sync);
+        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * Get the height of this line area.
-     *
-     * @return the height of the line area
-     */
-    public int getHeight() {
-        return lineHeight;
-    }
-
-    /**
-     * Add a child area to this line area.
-     *
-     * @param childArea the inline child area to add
-     */
-    public void addChild(Area childArea) {
-        if (childArea instanceof InlineArea) {
-            addInlineArea((InlineArea)childArea);
-        }
-    }
-
-    /**
-     * Add an inline child area to this line area.
-     *
-     * @param area the inline child area to add
-     */
-    public void addInlineArea(InlineArea area) {
-        inlineAreas.add(area);
-    }
-
-    /**
-     * Get the inline child areas of this line area.
-     *
-     * @return the list of inline areas
-     */
-    public List getInlineAreas() {
-        return inlineAreas;
-    }
-
-    /**
-     * Set the start indent of this line area.
-     * The start indent is used for offsetting the start of
-     * the inline areas for alignment or other indents.
-     *
-     * @param si the start indent value
-     */
-    public void setStartIndent(int si) {
-        startIndent = si;
-    }
-
-    /**
-     * Get the start indent of this line area.
-     * The start indent is used for offsetting the start of
-     * the inline areas for alignment or other indents.
-     *
-     * @return the start indent value
-     */
-    public int getStartIndent() {
-        return startIndent;
-    }
 }
-
