578a051ba41abd9a552718163bf4ecb317703ecb
Peter Bernard West <pbwest@apache.org>
2004-05-30 16:44:49 +0000

    Added prototype area creation. Changed FONode to FoPageSequence in signature.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/FOP_0-20-0_Alt-Design@197676 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/FoPcdata.java
+++ src/java/org/apache/fop/fo/flow/FoPcdata.java
@@ -23,8 +23,8 @@ package org.apache.fop.fo.flow;
 import java.util.Arrays;
 import java.util.BitSet;
 import org.apache.fop.apps.FOPException;
+import org.apache.fop.area.LineArea;
 import org.apache.fop.datastructs.TreeException;
-import org.apache.fop.fo.FONode;
 import org.apache.fop.fo.FOPageSeqNode;
 import org.apache.fop.fo.FOTree;
 import org.apache.fop.fo.FObjectNames;
@@ -121,13 +121,17 @@ public class FoPcdata extends FOPageSeqNode {
      * attribute set information.
      */
     public FoPcdata
-            (FOTree foTree, FONode pageSequence, FOPageSeqNode parent,
+            (FOTree foTree, FoPageSequence pageSequence, FOPageSeqNode parent,
                     XmlEvent event, int stateFlags)
         throws TreeException, FOPException
     {
         super(foTree, FObjectNames.PCDATA, pageSequence, parent, event,
                           stateFlags, sparsePropsMap, sparseIndices);
         characters = event.getChars();
+        // Generate the prototype LineArea
+        currentArea = new LineArea(
+                characters, pageSequence, this, layoutContext,
+                layoutContext.getSync());
         makeSparsePropsSet();
     }
 
