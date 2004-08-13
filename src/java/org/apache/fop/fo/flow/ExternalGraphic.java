c3b3f698406cac5c801bff65a6cb3b4e7ef04753
Glen Mazza <gmazza@apache.org>
2004-08-13 00:03:49 +0000

    1.) Layout initialization logic for fo:table, fo:table-header, and fo:table-footer moved from AddLMVisitor to fo.Table.
    2.) Layout logic for fo:external-graphic moved from fo.ExternalGraphic to
    layoutmgr.ExternalGraphicLayoutManager.
    
    3.) Partial implementation of validateChildNode() for fo:footnote.
    
    4.) fox:destination commented out from images.fo sample until purpose, content
    model better defined.  Also needs a Maker class.  (Does not appear to work
    in either maintenance or HEAD.)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@197867 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/flow/ExternalGraphic.java
+++ src/java/org/apache/fop/fo/flow/ExternalGraphic.java
@@ -20,7 +20,6 @@ package org.apache.fop.fo.flow;
 
 // Java
 import java.util.List;
-import java.awt.geom.Rectangle2D;
 
 // XML
 import org.xml.sax.Attributes;
@@ -28,12 +27,8 @@ import org.xml.sax.Locator;
 import org.xml.sax.SAXParseException;
 
 // FOP
-import org.apache.fop.area.inline.Image;
-import org.apache.fop.datatypes.Length;
 import org.apache.fop.fo.FONode;
 import org.apache.fop.fo.FObj;
-import org.apache.fop.image.FopImage;
-import org.apache.fop.image.ImageFactory;
 import org.apache.fop.layoutmgr.ExternalGraphicLayoutManager;
 
 /**
@@ -43,17 +38,6 @@ import org.apache.fop.layoutmgr.ExternalGraphicLayoutManager;
  */
 public class ExternalGraphic extends FObj {
     private String url;
-    private int breakAfter;
-    private int breakBefore;
-    private int align;
-    private int startIndent;
-    private int endIndent;
-    private int spaceBefore;
-    private int spaceAfter;
-    private int viewWidth = -1;
-    private int viewHeight = -1;
-    private boolean clip = false;
-    private Rectangle2D placement = null;
 
     /**
      * Create a new External graphic node.
@@ -78,182 +62,18 @@ public class ExternalGraphic extends FObj {
      */
     protected void addProperties(Attributes attlist) throws SAXParseException {
         super.addProperties(attlist);
-        getFOInputHandler().image(this);
-    }
-
-    /**
-     * Setup this image.
-     * This gets the sizes for the image and the dimensions and clipping.
-     * @todo move logic to LM class and/or addProperties() as appropriate
-     */
-    private void setup() {
         url = this.propertyList.get(PR_SRC).getString();
-        if (url == null) {
-            return;
-        }
-        url = ImageFactory.getURL(url);
-
-        // assume lr-tb for now and just use the .optimum value of the range
-        Length ipd = propertyList.get(PR_INLINE_PROGRESSION_DIMENSION).
-                                    getLengthRange().getOptimum().getLength();
-        if (!ipd.isAuto()) {
-            viewWidth = ipd.getValue();
-        } else {
-            ipd = propertyList.get(PR_WIDTH).getLength();
-            if (!ipd.isAuto()) {
-                viewWidth = ipd.getValue();
-            }
-        }
-        Length bpd = propertyList.get(PR_BLOCK_PROGRESSION_DIMENSION | CP_OPTIMUM).getLength();
-        if (!bpd.isAuto()) {
-            viewHeight = bpd.getValue();
-        } else {
-            bpd = propertyList.get(PR_HEIGHT).getLength();
-            if (!bpd.isAuto()) {
-                viewHeight = bpd.getValue();
-            }
-        }
-
-        // if we need to load this image to get its size
-        FopImage fopimage = null;
-
-        int cwidth = -1;
-        int cheight = -1;
-        Length ch = propertyList.get(PR_CONTENT_HEIGHT).getLength();
-        if (!ch.isAuto()) {
-            /*if (ch.scaleToFit()) {
-                if (viewHeight != -1) {
-                    cheight = viewHeight;
-                }
-            } else {*/
-            cheight = ch.getValue();
-        }
-        Length cw = propertyList.get(PR_CONTENT_WIDTH).getLength();
-        if (!cw.isAuto()) {
-            /*if (cw.scaleToFit()) {
-                if (viewWidth != -1) {
-                    cwidth = viewWidth;
-                }
-            } else {*/
-            cwidth = cw.getValue();
-        }
-
-        int scaling = propertyList.get(PR_SCALING).getEnum();
-        if ((scaling == Scaling.UNIFORM) || (cwidth == -1) || cheight == -1) {
-            ImageFactory fact = ImageFactory.getInstance();
-            fopimage = fact.getImage(url, getUserAgent());
-            if (fopimage == null) {
-                // error
-                url = null;
-                return;
-            }
-            // load dimensions
-            if (!fopimage.load(FopImage.DIMENSIONS)) {
-                // error
-                url = null;
-                return;
-            }
-            if (cwidth == -1 && cheight == -1) {
-                cwidth = (int)(fopimage.getWidth() * 1000);
-                cheight = (int)(fopimage.getHeight() * 1000);
-            } else if (cwidth == -1) {
-                cwidth = (int)(fopimage.getWidth() * cheight) / fopimage.getHeight();
-            } else if (cheight == -1) {
-                cheight = (int)(fopimage.getHeight() * cwidth) / fopimage.getWidth();
-            } else {
-                // adjust the larger
-                double rat1 = cwidth / (fopimage.getWidth() * 1000f);
-                double rat2 = cheight / (fopimage.getHeight() * 1000f);
-                if (rat1 < rat2) {
-                    // reduce cheight
-                    cheight = (int)(rat1 * fopimage.getHeight() * 1000);
-                } else {
-                    cwidth = (int)(rat2 * fopimage.getWidth() * 1000);
-                }
-            }
-        }
-
-        if (viewWidth == -1) {
-            viewWidth = cwidth;
-        }
-        if (viewHeight == -1) {
-            viewHeight = cheight;
-        }
-
-        if (cwidth > viewWidth || cheight > viewHeight) {
-            int overflow = propertyList.get(PR_OVERFLOW).getEnum();
-            if (overflow == Overflow.HIDDEN) {
-                clip = true;
-            } else if (overflow == Overflow.ERROR_IF_OVERFLOW) {
-                getLogger().error("Image: " + url
-                                  + " overflows the viewport, clipping to viewport");
-                clip = true;
-            }
-        }
-
-        int xoffset = 0;
-        int yoffset = 0;
-        int da = propertyList.get(PR_DISPLAY_ALIGN).getEnum();
-        switch(da) {
-            case DisplayAlign.BEFORE:
-            break;
-            case DisplayAlign.AFTER:
-                yoffset = viewHeight - cheight;
-            break;
-            case DisplayAlign.CENTER:
-                yoffset = (viewHeight - cheight) / 2;
-            break;
-            case DisplayAlign.AUTO:
-            default:
-            break;
-        }
-
-        int ta = propertyList.get(PR_TEXT_ALIGN).getEnum();
-        switch(ta) {
-            case TextAlign.CENTER:
-                xoffset = (viewWidth - cwidth) / 2;
-            break;
-            case TextAlign.END:
-                xoffset = viewWidth - cwidth;
-            break;
-            case TextAlign.START:
-            break;
-            case TextAlign.JUSTIFY:
-            default:
-            break;
-        }
-        placement = new Rectangle2D.Float(xoffset, yoffset, cwidth, cheight);
-    }
-
-    /**
-     * @return the ViewHeight (in millipoints??)
-     * @todo check that each of these accessors are needed
-     */
-    public int getViewHeight() {
-        return viewHeight;
+        getFOInputHandler().image(this);
     }
 
     public String getURL() {
         return url;
     }
 
-    public int getViewWidth() {
-        return viewWidth;
-    }
-
-    public boolean getClip() {
-        return clip;
-    }
-
-    public Rectangle2D getPlacement() {
-        return placement;
-    }
-
     /**
      * @see org.apache.fop.fo.FObj#addLayoutManager(List)
     */
     public void addLayoutManager(List list) {
-        setup();
         if (getURL() != null) {
             ExternalGraphicLayoutManager lm = new ExternalGraphicLayoutManager(this);
             list.add(lm);
--- src/java/org/apache/fop/layoutmgr/ExternalGraphicLayoutManager.java
+++ src/java/org/apache/fop/layoutmgr/ExternalGraphicLayoutManager.java
@@ -18,12 +18,19 @@
 
 package org.apache.fop.layoutmgr;
 
+// Java
+import java.awt.geom.Rectangle2D;
+
+// FOP
 import org.apache.fop.area.inline.Image;
 import org.apache.fop.area.inline.InlineArea;
 import org.apache.fop.area.inline.Viewport;
+import org.apache.fop.datatypes.Length;
 import org.apache.fop.fo.flow.ExternalGraphic;
 import org.apache.fop.fo.properties.CommonBorderAndPadding;
 import org.apache.fop.fo.properties.CommonBackground;
+import org.apache.fop.image.FopImage;
+import org.apache.fop.image.ImageFactory;
 
 /**
  * LayoutManager for the fo:external-graphic formatting object
@@ -31,6 +38,19 @@ import org.apache.fop.fo.properties.CommonBackground;
 public class ExternalGraphicLayoutManager extends LeafNodeLayoutManager {
 
     ExternalGraphic graphic = null;
+
+    private String url;
+    private int breakAfter;
+    private int breakBefore;
+    private int align;
+    private int startIndent;
+    private int endIndent;
+    private int spaceBefore;
+    private int spaceAfter;
+    private int viewWidth = -1;
+    private int viewHeight = -1;
+    private boolean clip = false;
+    private Rectangle2D placement = null;
 
     /**
      * Constructor
@@ -41,10 +61,151 @@ public class ExternalGraphicLayoutManager extends LeafNodeLayoutManager {
         super(node);
 
         graphic = node;
+        setup();
         InlineArea area = getExternalGraphicInlineArea();
         setCurrentArea(area);
         setAlignment(graphic.getProperty(PR_VERTICAL_ALIGN).getEnum());
-        setLead(graphic.getViewHeight());
+        setLead(viewHeight);
+    }
+
+    /**
+     * Setup this image.
+     * This gets the sizes for the image and the dimensions and clipping.
+     * @todo see if can simplify property handling logic
+     */
+    private void setup() {
+        url = ImageFactory.getURL(graphic.getURL());
+
+        // assume lr-tb for now and just use the .optimum value of the range
+        Length ipd = graphic.getPropertyList().get(PR_INLINE_PROGRESSION_DIMENSION).
+                                    getLengthRange().getOptimum().getLength();
+        if (!ipd.isAuto()) {
+            viewWidth = ipd.getValue();
+        } else {
+            ipd = graphic.getPropertyList().get(PR_WIDTH).getLength();
+            if (!ipd.isAuto()) {
+                viewWidth = ipd.getValue();
+            }
+        }
+        Length bpd = graphic.getPropertyList().get(PR_BLOCK_PROGRESSION_DIMENSION | CP_OPTIMUM).getLength();
+        if (!bpd.isAuto()) {
+            viewHeight = bpd.getValue();
+        } else {
+            bpd = graphic.getPropertyList().get(PR_HEIGHT).getLength();
+            if (!bpd.isAuto()) {
+                viewHeight = bpd.getValue();
+            }
+        }
+
+        // if we need to load this image to get its size
+        FopImage fopimage = null;
+
+        int cwidth = -1;
+        int cheight = -1;
+        Length ch = graphic.getPropertyList().get(PR_CONTENT_HEIGHT).getLength();
+        if (!ch.isAuto()) {
+            /*if (ch.scaleToFit()) {
+                if (viewHeight != -1) {
+                    cheight = viewHeight;
+                }
+            } else {*/
+            cheight = ch.getValue();
+        }
+        Length cw = graphic.getPropertyList().get(PR_CONTENT_WIDTH).getLength();
+        if (!cw.isAuto()) {
+            /*if (cw.scaleToFit()) {
+                if (viewWidth != -1) {
+                    cwidth = viewWidth;
+                }
+            } else {*/
+            cwidth = cw.getValue();
+        }
+
+        int scaling = graphic.getPropertyList().get(PR_SCALING).getEnum();
+        if ((scaling == Scaling.UNIFORM) || (cwidth == -1) || cheight == -1) {
+            ImageFactory fact = ImageFactory.getInstance();
+            fopimage = fact.getImage(url, graphic.getUserAgent());
+            if (fopimage == null) {
+                // error
+                url = null;
+                return;
+            }
+            // load dimensions
+            if (!fopimage.load(FopImage.DIMENSIONS)) {
+                // error
+                url = null;
+                return;
+            }
+            if (cwidth == -1 && cheight == -1) {
+                cwidth = (int)(fopimage.getWidth() * 1000);
+                cheight = (int)(fopimage.getHeight() * 1000);
+            } else if (cwidth == -1) {
+                cwidth = (int)(fopimage.getWidth() * cheight) / fopimage.getHeight();
+            } else if (cheight == -1) {
+                cheight = (int)(fopimage.getHeight() * cwidth) / fopimage.getWidth();
+            } else {
+                // adjust the larger
+                double rat1 = cwidth / (fopimage.getWidth() * 1000f);
+                double rat2 = cheight / (fopimage.getHeight() * 1000f);
+                if (rat1 < rat2) {
+                    // reduce cheight
+                    cheight = (int)(rat1 * fopimage.getHeight() * 1000);
+                } else {
+                    cwidth = (int)(rat2 * fopimage.getWidth() * 1000);
+                }
+            }
+        }
+
+        if (viewWidth == -1) {
+            viewWidth = cwidth;
+        }
+        if (viewHeight == -1) {
+            viewHeight = cheight;
+        }
+
+        if (cwidth > viewWidth || cheight > viewHeight) {
+            int overflow = graphic.getPropertyList().get(PR_OVERFLOW).getEnum();
+            if (overflow == Overflow.HIDDEN) {
+                clip = true;
+            } else if (overflow == Overflow.ERROR_IF_OVERFLOW) {
+                graphic.getLogger().error("Image: " + url
+                                  + " overflows the viewport, clipping to viewport");
+                clip = true;
+            }
+        }
+
+        int xoffset = 0;
+        int yoffset = 0;
+        int da = graphic.getPropertyList().get(PR_DISPLAY_ALIGN).getEnum();
+        switch(da) {
+            case DisplayAlign.BEFORE:
+            break;
+            case DisplayAlign.AFTER:
+                yoffset = viewHeight - cheight;
+            break;
+            case DisplayAlign.CENTER:
+                yoffset = (viewHeight - cheight) / 2;
+            break;
+            case DisplayAlign.AUTO:
+            default:
+            break;
+        }
+
+        int ta = graphic.getPropertyList().get(PR_TEXT_ALIGN).getEnum();
+        switch(ta) {
+            case TextAlign.CENTER:
+                xoffset = (viewWidth - cwidth) / 2;
+            break;
+            case TextAlign.END:
+                xoffset = viewWidth - cwidth;
+            break;
+            case TextAlign.START:
+            break;
+            case TextAlign.JUSTIFY:
+            default:
+            break;
+        }
+        placement = new Rectangle2D.Float(xoffset, yoffset, cwidth, cheight);
     }
 
      /**
@@ -56,10 +217,10 @@ public class ExternalGraphicLayoutManager extends LeafNodeLayoutManager {
      public InlineArea getExternalGraphicInlineArea() {
          Image imArea = new Image(graphic.getURL());
          Viewport vp = new Viewport(imArea);
-         vp.setWidth(graphic.getViewWidth());
-         vp.setHeight(graphic.getViewHeight());
-         vp.setClip(graphic.getClip());
-         vp.setContentPosition(graphic.getPlacement());
+         vp.setWidth(viewWidth);
+         vp.setHeight(viewHeight);
+         vp.setClip(clip);
+         vp.setContentPosition(placement);
          vp.setOffset(0);
 
          // Common Border, Padding, and Background Properties
