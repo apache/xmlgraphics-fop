02df8d85a616d663877d86f3858fb16b4c48e09f
Jeremias Maerki <jeremias@apache.org>
2005-01-05 21:08:27 +0000

    Refactored Area Tree XML Renderer to write XML to a TransformerHandler instead of to a Writer.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@198224 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/xml/XMLXMLHandler.java
+++ src/java/org/apache/fop/render/xml/XMLXMLHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,117 +21,106 @@ package org.apache.fop.render.xml;
 import org.apache.fop.render.XMLHandler;
 import org.apache.fop.render.RendererContext;
 
-import org.apache.batik.dom.util.DOMUtilities;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Attr;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
 
-import java.io.Writer;
-import java.io.IOException;
+import javax.xml.transform.sax.TransformerHandler;
 
 /**
  * XML handler for the XML renderer.
  */
 public class XMLXMLHandler implements XMLHandler {
     
-    /** Key for getting the Writer from the RendererContext */
-    public static final String WRITER = "writer";
+    /** Key for getting the TransformerHandler from the RendererContext */
+    public static final String HANDLER = "handler";
 
+    private AttributesImpl atts = new AttributesImpl();
+    
     /**
      * @see org.apache.fop.render.XMLHandler#handleXML(RendererContext, Document, String)
      */
     public void handleXML(RendererContext context, Document doc,
                           String ns) throws Exception {
-        Writer writer = (Writer) context.getProperty(WRITER);
+        TransformerHandler handler = (TransformerHandler) context.getProperty(HANDLER);
 
-        String svg = "http://www.w3.org/2000/svg";
-        // actually both do the same thing but one requires
-        // batik
-        if (svg.equals(ns)) {
-            DOMUtilities.writeDocument(doc, writer);
-        } else {
-            writeDocument(doc, writer);
-        }
-        writer.write("\n");
+        //String svg = "http://www.w3.org/2000/svg";
+        writeDocument(doc, handler);
     }
 
     /**
-     * Writes the given document using the given writer.
+     * Writes the given document using the given TransformerHandler.
      * @param doc DOM document
-     * @param writer Writer to write to
-     * @throws IOException In case of an I/O problem
+     * @param handler TransformerHandler to write to
+     * @throws SAXException In case of a problem while writing XML
      */
-    public static void writeDocument(Document doc,
-                                     Writer writer) throws IOException {
+    public void writeDocument(Document doc,
+                                     TransformerHandler handler) throws SAXException {
         for (Node n = doc.getFirstChild(); n != null;
                 n = n.getNextSibling()) {
-            writeNode(n, writer);
+            writeNode(n, handler);
         }
     }
 
     /**
      * Writes a node using the given writer.
      * @param node node to serialize
-     * @param writer Writer to write to
-     * @throws IOException In case of an I/O problem
+     * @param handler TransformerHandler to write to
+     * @throws SAXException In case of a problem while writing XML
      */
-    public static void writeNode(Node node, Writer writer) throws IOException {
+    public void writeNode(Node node, TransformerHandler handler) throws SAXException {
+        char[] ca;
         switch (node.getNodeType()) {
             case Node.ELEMENT_NODE:
-                writer.write("<");
-                writer.write(node.getNodeName());
+                atts.clear();
 
                 if (node.hasAttributes()) {
                     NamedNodeMap attr = node.getAttributes();
                     int len = attr.getLength();
                     for (int i = 0; i < len; i++) {
                         Attr a = (Attr) attr.item(i);
-                        writer.write(" ");
-                        writer.write(a.getNodeName());
-                        writer.write("=\"");
-                        writer.write(contentToString(a.getNodeValue()));
-                        writer.write("\"");
+                        atts.addAttribute("", a.getNodeName(), a.getNodeName(), 
+                                "CDATA", a.getNodeValue());
                     }
                 }
+                handler.startElement(node.getNamespaceURI(), 
+                        node.getLocalName(), node.getLocalName(), atts);
 
                 Node c = node.getFirstChild();
                 if (c != null) {
-                    writer.write(">");
                     for (; c != null; c = c.getNextSibling()) {
-                        writeNode(c, writer);
+                        writeNode(c, handler);
                     }
-                    writer.write("</");
-                    writer.write(node.getNodeName());
-                    writer.write(">");
-                } else {
-                    writer.write("/>");
                 }
+                handler.endElement(node.getNamespaceURI(), node.getNodeName(), node.getNodeName());
                 break;
             case Node.TEXT_NODE:
-                writer.write(contentToString(node.getNodeValue()));
+                ca = node.getNodeValue().toCharArray();
+                handler.characters(ca, 0, ca.length);
                 break;
             case Node.CDATA_SECTION_NODE:
-                writer.write("<![CDATA[");
-                writer.write(node.getNodeValue());
-                writer.write("]]>");
+                ca = node.getNodeValue().toCharArray();
+                handler.startCDATA();
+                handler.characters(ca, 0, ca.length);
+                handler.endCDATA();
                 break;
             case Node.ENTITY_REFERENCE_NODE:
+                System.out.println("Ignoring ENTITY_REFERENCE_NODE. NYI");
+                /*
                 writer.write("&");
-                writer.write(node.getNodeName());
+                writer.write();
                 writer.write(";");
+                */
                 break;
             case Node.PROCESSING_INSTRUCTION_NODE:
-                writer.write("<?");
-                writer.write(node.getNodeName());
-                writer.write(node.getNodeValue());
-                writer.write("?>");
+                handler.processingInstruction(node.getNodeName(), node.getNodeValue());
                 break;
             case Node.COMMENT_NODE:
-                writer.write("<!--");
-                writer.write(node.getNodeValue());
-                writer.write("-->");
+                ca = node.getNodeValue().toCharArray();
+                handler.comment(ca, 0, ca.length);
                 break;
             case Node.DOCUMENT_TYPE_NODE:
                 break;
@@ -147,7 +136,7 @@ public class XMLXMLHandler implements XMLHandler {
      * @param s content value
      * @return encoded value
      */
-    public static String contentToString(String s) {
+    private static String contentToString(String s) {
         StringBuffer result = new StringBuffer();
 
         for (int i = 0; i < s.length(); i++) {
