ae6f9e9fd5fa83d4e4b54b8486f2a6496d1af824
Jeremias Maerki <jeremias@apache.org>
2005-01-31 13:10:39 +0000

    Added the page number to the pageViewport
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@198344 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/xml/XMLRenderer.java
+++ src/java/org/apache/fop/render/xml/XMLRenderer.java
@@ -338,6 +338,7 @@ public class XMLRenderer extends AbstractRenderer {
     public void renderPage(PageViewport page) throws IOException, FOPException {
         atts.clear();
         addAttribute("bounds", page.getViewArea());
+        addAttribute("nr", page.getPageNumber());
         startElement("pageViewport", atts);
         startElement("page");
         super.renderPage(page);
