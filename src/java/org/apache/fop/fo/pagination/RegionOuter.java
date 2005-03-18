b44957f8da4c58980c9067f561639f2145298e6b
Jeremias Maerki <jeremias@apache.org>
2005-03-18 13:19:50 +0000

    Common base class for all outer regions (for easier access to the extent property).
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/Temp_KnuthStylePageBreaking@198513 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/fop/fo/pagination/RegionOuter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.fop.fo.pagination;
+
+import java.awt.Rectangle;
+
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.datatypes.FODimension;
+import org.apache.fop.datatypes.Length;
+import org.apache.fop.fo.FONode;
+import org.apache.fop.fo.PropertyList;
+
+/**
+ * @author Jeremias Maerki
+ */
+public abstract class RegionOuter extends Region {
+
+    private Length extent;
+    
+    /**
+     * @see org.apache.fop.fo.FONode#FONode(FONode)
+     */
+    protected RegionOuter(FONode parent) {
+        super(parent);
+    }
+
+    /**
+     * @see org.apache.fop.fo.FObj#bind(PropertyList)
+     */
+    public void bind(PropertyList pList) throws FOPException {
+        super.bind(pList);
+        extent = pList.get(PR_EXTENT).getLength();
+    }
+    
+    /**
+     * @return the "extent" property.
+     */
+    public Length getExtent() {
+        return extent;
+    }
+
+    
+
+    
+}
