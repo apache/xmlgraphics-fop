c29f10715e66bda537b3d33764cd96fb5f587e6a
Jeremias Maerki <jeremias@apache.org>
2005-03-23 13:51:37 +0000

    Javadocs and toString() methods.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/Temp_KnuthStylePageBreaking@198538 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/KnuthGlue.java
+++ src/java/org/apache/fop/layoutmgr/KnuthGlue.java
@@ -73,26 +73,40 @@ public class KnuthGlue extends KnuthElement {
         adjustmentClass = iAdjClass;
     }
 
+    /** @see org.apache.fop.layoutmgr.KnuthElement#isGlue() */
     public boolean isGlue() {
         return true;
     }
 
-    /**
-     * Return the stretchability of this glue.
-     */
+    /** @return the stretchability of this glue. */
     public int getY() {
         return stretchability;
     }
 
-    /**
-     * Return the shrinkability of this glue.
-     */
+    /** @return the shrinkability of this glue. */
     public int getZ() {
         return shrinkability;
     }
     
+    /** @return the adjustment class (or role) of this glue. */
     public int getAdjustmentClass() {
         return adjustmentClass;
+    }
+    
+    /** @see java.lang.Object#toString() */
+    public String toString() {
+        StringBuffer sb = new StringBuffer(64);
+        if (isAuxiliary()) {
+            sb.append("aux. ");
+        }
+        sb.append("glue");
+        sb.append(" w=").append(getW());
+        sb.append(" stretch=").append(getY());
+        sb.append(" shrink=").append(getZ());
+        if (getAdjustmentClass() >= 0) {
+            sb.append(" adj-class=").append(getAdjustmentClass());
+        }
+        return sb.toString();
     }
     
 }
