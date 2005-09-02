d2d832d5b66467388df91199268e4d120c1916c0
Jeremias Maerki <jeremias@apache.org>
2005-09-02 07:49:30 +0000

    Fixed getDocument() method (is now getDOMDocument()). The plan extension still worked because convertToSvg() was called by getDimension(). Anyway, this is now fixed.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@266300 13f79535-47bb-0310-9956-ffa450edef68

--- examples/plan/src/org/apache/fop/plan/PlanElement.java
+++ examples/plan/src/org/apache/fop/plan/PlanElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,8 +50,7 @@ public class PlanElement extends PlanObj {
      */
     public void processNode(String elementName, Locator locator, 
                             Attributes attlist, PropertyList propertyList)
-        throws FOPException
-    {
+        throws FOPException {
         super.processNode(elementName, locator, attlist, propertyList);
         createBasicDocument();
     }
@@ -80,9 +79,9 @@ public class PlanElement extends PlanObj {
     }
 
     /**
-     * @see org.apache.fop.fo.XMLObj#getDocument()
+     * @see org.apache.fop.fo.XMLObj#getDOMDocument()
      */
-    public Document getDocument() {
+    public Document getDOMDocument() {
         convertToSVG();
         return doc;
     }
