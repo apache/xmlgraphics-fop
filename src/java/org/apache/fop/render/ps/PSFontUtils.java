6dd7ae721210db42a92c9bbb2a999c24138da988
Jeremias Maerki <jeremias@apache.org>
2005-09-05 19:20:32 +0000

    Fixes a compile error. I used a method from a SVN checkout of Commons IO. Sorry.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@278816 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/ps/PSFontUtils.java
+++ src/java/org/apache/fop/render/ps/PSFontUtils.java
@@ -29,8 +29,8 @@ import java.util.Map;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.commons.io.CopyUtils;
 import org.apache.commons.io.EndianUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.fop.fonts.CustomFont;
 import org.apache.fop.fonts.FontInfo;
 import org.apache.fop.fonts.FontType;
@@ -163,7 +163,7 @@ public class PSFontUtils {
 
                     SubInputStream sin = new SubInputStream(in, dataSegLen);
                     ASCIIHexOutputStream hexOut = new ASCIIHexOutputStream(gen.getOutputStream());
-                    IOUtils.copy(sin, hexOut);
+                    CopyUtils.copy(sin, hexOut);
                     gen.newLine();
                     break;
                 case 3: //EOF
