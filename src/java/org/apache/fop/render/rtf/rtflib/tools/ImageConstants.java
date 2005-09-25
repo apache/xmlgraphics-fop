ebe5f5e4d1f2802cd2742d11c64afd547b129b02
Peter Herweg <pherweg@apache.org>
2005-09-25 17:32:24 +0000

    added support for external-graphic (width and height attributes should be specified)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@291449 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/rtf/rtflib/tools/ImageConstants.java
+++ src/java/org/apache/fop/render/rtf/rtflib/tools/ImageConstants.java
@@ -26,7 +26,6 @@
 
 package org.apache.fop.render.rtf.rtflib.tools;
 
-import java.util.Hashtable;
 
 /** Here will be defined all supported image formats.
  *  This class belongs to the <fo:external-graphic> tag processing.
@@ -46,25 +45,13 @@ public class ImageConstants {
     public static final int I_PNG = 1;
     /** Integer equivalent for JPG */
     public static final int I_JPG = 2;
+    /** Integer equivalent for BMP */
+    public static final int I_BMP = 3;
 
-    /** Defines the RTF properties */
-    public static final String [] RTF_TAGS = new String []
-        {
-            "emfblip", "pngblip", "jpegblip"
-        };
-
-    /** constant for image conversion basis (??) */
-    public static final int I_TO_CONVERT_BASIS = 50;
     /** Integer equivalent for GIF */
     public static final int I_GIF = 50;
     /** Integer equivalent for JPEG C (??) */
     public static final int I_JPG_C = 51;
-
-    /** Defines the types for converting rtf supported image types */
-    public static final int [] CONVERT_TO = new int []
-        {
-            I_JPG, I_JPG
-        };
 
     /** EMF file extension */
     public static final String EMF_EXT = "emf";
@@ -76,16 +63,6 @@ public class ImageConstants {
     public static final String JPEG_EXT = "jpeg";
     /** GIF file extension */
     public static final String GIF_EXT = "gif";
-
-    /** Defines the file extensions and the RTF property belongs to */
-    public static final Hashtable SUPPORTED_IMAGE_TYPES = new Hashtable ();
-    static {
-        SUPPORTED_IMAGE_TYPES.put (EMF_EXT, new Integer (I_EMF));
-        SUPPORTED_IMAGE_TYPES.put (PNG_EXT, new Integer (I_PNG));
-        SUPPORTED_IMAGE_TYPES.put (JPG_EXT, new Integer (I_JPG_C));
-        SUPPORTED_IMAGE_TYPES.put (JPEG_EXT, new Integer (I_JPG_C));
-        SUPPORTED_IMAGE_TYPES.put (GIF_EXT, new Integer (I_GIF));
-    }
 
     //////////////////////////////////////////////////
     // @@ Construction
