72491898720daa4f174c81de9418a7f1de366a19
Jeremias Maerki <jeremias@apache.org>
2005-10-19 16:30:58 +0000

    Fix to make the Graphics.create() method (calls a copy constructor) work. Extracted stuff into a PDFContext class which gets passed to each copy of PDFDocumentGraphics2D, so the state is uniform over all copies.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@326600 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/svg/PDFTranscoder.java
+++ src/java/org/apache/fop/svg/PDFTranscoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,7 +63,7 @@ import org.w3c.dom.svg.SVGLength;
  * millimeter conversion factor.
  *
  * @author <a href="mailto:keiron@aftexsw.com">Keiron Liddle</a>
- * @version $Id: PDFTranscoder.java,v 1.24 2003/03/07 09:51:26 jeremias Exp $
+ * @version $Id$
  */
 public class PDFTranscoder extends AbstractFOPTranscoder
         implements Configurable {
@@ -112,7 +112,6 @@ public class PDFTranscoder extends AbstractFOPTranscoder
         throws TranscoderException {
 
         graphics = new PDFDocumentGraphics2D();
-        graphics.setLogger(getLogger());
         
         try {
             if (this.cfg != null) {
