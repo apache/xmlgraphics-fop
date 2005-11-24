a175f4d91ed09716886687c50f5500bcf7d66a90
Jeremias Maerki <jeremias@apache.org>
2005-11-24 15:17:36 +0000

    Bugzilla #37521: Fixed typo. Submitted by: Matthew Daniel <bugzilla.at.mdaniel.scdi.com>
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@348751 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/properties/TableColLength.java
+++ src/java/org/apache/fop/fo/properties/TableColLength.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ public class TableColLength extends LengthProperty {
      */
     public double getNumericValue() {
         throw new UnsupportedOperationException(
-                "Must call getNumericValue with PercentageBaseContext");
+                "Must call getNumericValue with PercentBaseContext");
     }
 
     /**
@@ -92,7 +92,7 @@ public class TableColLength extends LengthProperty {
      */
     public int getValue() {
         throw new UnsupportedOperationException(
-                "Must call getValue with PercentageBaseContext");
+                "Must call getValue with PercentBaseContext");
     }
 
     /**
