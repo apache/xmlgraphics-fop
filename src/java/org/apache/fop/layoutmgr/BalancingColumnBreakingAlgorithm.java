45fee7442a433c41bd6017d60f9e0324f1fadbec
Jeremias Maerki <jeremias@apache.org>
2005-12-22 07:58:31 +0000

    Introduced a flag to tell the page breaking to operate in auto-height mode, i.e. no overflows can happen. This disables the overflow warnings when footnote-separators are defined. They don't have an explicit height.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@358522 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/BalancingColumnBreakingAlgorithm.java
+++ src/java/org/apache/fop/layoutmgr/BalancingColumnBreakingAlgorithm.java
@@ -41,7 +41,7 @@ public class BalancingColumnBreakingAlgorithm extends PageBreakingAlgorithm {
             boolean partOverflowRecovery,
             int columnCount) {
         super(topLevelLM, pageProvider, alignment, alignmentLast, 
-                footnoteSeparatorLength, partOverflowRecovery);
+                footnoteSeparatorLength, partOverflowRecovery, false);
         this.columnCount = columnCount;
         this.considerTooShort = true; //This is important!
     }
