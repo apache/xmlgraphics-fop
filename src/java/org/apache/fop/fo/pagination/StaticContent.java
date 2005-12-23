a54594db578b3717ff99209393f0aad9a856dfd2
Jeremias Maerki <jeremias@apache.org>
2005-12-23 23:33:38 +0000

    Merge from Branch fop-0_91: Relaxed validation for empty static-content for compatibility with 0.20.5.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@358884 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/pagination/StaticContent.java
+++ src/java/org/apache/fop/fo/pagination/StaticContent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +54,7 @@ public class StaticContent extends Flow {
      * @see org.apache.fop.fo.FONode#endOfNode
      */
     protected void endOfNode() throws FOPException {
-        if (childNodes == null) {
+        if (childNodes == null && getUserAgent().validateStrictly()) {
             missingChildElementError("(%block;)+");
         }
         getFOEventHandler().endFlow(this);
