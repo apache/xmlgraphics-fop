4a37dca7de08671db855164abede6c2fdce54cdc
Jeremias Maerki <jeremias@apache.org>
2006-02-02 09:01:29 +0000

    Don't swallow characters if the handler does not implement LexicalHandler.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@374312 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/xml/XMLXMLHandler.java
+++ src/java/org/apache/fop/render/xml/XMLXMLHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2005 The Apache Software Foundation.
+ * Copyright 1999-2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,6 +111,8 @@ public class XMLXMLHandler implements XMLHandler {
                     lh.startCDATA();
                     handler.characters(ca, 0, ca.length);
                     lh.endCDATA();
+                } else {
+                    handler.characters(ca, 0, ca.length);
                 }
                 break;
             case Node.ENTITY_REFERENCE_NODE:
