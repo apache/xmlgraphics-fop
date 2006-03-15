0f8ce41ce30ecf63faaa9ccfdfbcb3383c6b42af
Jeremias Maerki <jeremias@apache.org>
2006-03-15 09:28:49 +0000

    Bugfix: Fixed an NPE that was thrown when a table was used inside a footnote-body.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@386015 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/PageSequenceLayoutManager.java
+++ src/java/org/apache/fop/layoutmgr/PageSequenceLayoutManager.java
@@ -242,6 +242,7 @@ public class PageSequenceLayoutManager extends AbstractLayoutManager {
                         // element represents a line with footnote citations
                         bFootnotesPresent = true;
                         LayoutContext footnoteContext = new LayoutContext(context);
+                        footnoteContext.setStackLimit(context.getStackLimit());
                         footnoteContext.setRefIPD(getCurrentPV()
                                 .getRegionReference(Constants.FO_REGION_BODY).getIPD());
                         LinkedList footnoteBodyLMs = ((KnuthBlockBox) element).getFootnoteBodyLMs();
