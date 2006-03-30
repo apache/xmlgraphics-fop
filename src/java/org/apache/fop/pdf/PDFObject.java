fc889fb52f76f227a7ff8814dbe608a09394a971
Jeremias Maerki <jeremias@apache.org>
2006-03-30 09:57:32 +0000

    Changed PDFText.escapeText() so it doesn't output 7bit ASCII texts in hex mode. This makes it easier to read the PDF in a text editor in most cases. Fixed the property names for xmp:CreatorTool and xmp:CreateDate (case mistake on my side). Changed XMP-generation to output the creation date including the time zone to keep Acrobat's preflight check for PDF/A-1b happy. Found that only after more than 3 hours of trial&error.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@390051 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/pdf/PDFObject.java
+++ src/java/org/apache/fop/pdf/PDFObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2004 The Apache Software Foundation.
+ * Copyright 1999-2004,2006 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -198,7 +198,7 @@ public abstract class PDFObject {
             return PDFText.escapeByteArray(
                 getDocument().getEncryption().encrypt(buf, this));
         } else {
-            return encode(PDFText.escapeText(text, true));
+            return encode(PDFText.escapeText(text, false));
         }
     }
     
