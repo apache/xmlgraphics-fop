0040a29ecce57798e6947fdb33750a4dba1afea0
Manuel Mall <manuel@apache.org>
2006-04-27 15:08:17 +0000

    AFP Renderer as per IP clearance (http://mail-archives.apache.org/mod_mbox/xmlgraphics-general/200604.mbox/%3c20060426085421.B456.DEV@jeremias-maerki.ch%3e) plus some further corrections and changes made by manuel@apache.org since
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@397562 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/sandbox/org/apache/fop/render/afp/exceptions/RendererRuntimeException.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
+package org.apache.fop.render.afp.exceptions;
+
+/**
+ * A runtime exception for handling fatal errors in rendering.
+ * <p/>
+ */
+public class RendererRuntimeException extends NestedRuntimeException {
+
+	/**
+	 * Constructs a RendererRuntimeException with the specified message.
+	 * @param msg the exception mesaage
+	 */
+	public RendererRuntimeException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Constructs a RendererRuntimeException with the specified message
+	 * wrapping the underlying exception.
+	 * @param msg the exception mesaage
+	 * @param t the underlying exception
+	 */
+	public RendererRuntimeException(String msg, Throwable t) {
+		super(msg, t);
+	}
+
+}
