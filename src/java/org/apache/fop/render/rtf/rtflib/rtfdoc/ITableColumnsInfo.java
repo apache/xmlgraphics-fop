f970e15d282ae09cd0a00f04745d237e3fa71ac4
Jeremias Maerki <jeremias@apache.org>
2006-05-18 15:54:17 +0000

    Bugzilla #39533: Bugfix: NullPointerException in RTF output when a table does not contain table-columns. The code now informs the user that he should explicitely define the table-columns.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@407576 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/render/rtf/rtflib/rtfdoc/ITableColumnsInfo.java
+++ src/java/org/apache/fop/render/rtf/rtflib/rtfdoc/ITableColumnsInfo.java
@@ -31,7 +31,7 @@ package org.apache.fop.render.rtf.rtflib.rtfdoc;
 
 public interface ITableColumnsInfo {
     /** value for invalid column width */
-    float INVALID_COLUM_WIDTH = 200f;
+    float INVALID_COLUMN_WIDTH = 200f;
 
     /** reset the column iteration index, meant to be called when creating a new row */
     void selectFirstColumn();
