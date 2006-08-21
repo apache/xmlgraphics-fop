e35a86b4f764447f17eb07ea410ce5e3e08800e4
Andreas L. Delmelle <adelmelle@apache.org>
2006-08-21 22:19:35 +0000

    Reset visibility to public
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@433398 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/properties/LengthRangeProperty.java
+++ src/java/org/apache/fop/fo/properties/LengthRangeProperty.java
@@ -283,7 +283,7 @@ public class LengthRangeProperty extends Property implements CompoundDatatype {
     /**
      * @return this.lengthRange cast as an Object
      */
-    protected Object getObject() {
+    public Object getObject() {
         return this;
     }
 
