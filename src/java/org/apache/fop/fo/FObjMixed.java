12120819c90905f3111981b7f8ef324ecdc32436
Andreas L. Delmelle <adelmelle@apache.org>
2006-09-02 00:52:38 +0000

    Bugzilla #39414: Uninterrupted text blocks larger than 32K characters would cause overflows during layout, so make sure the FOText char arrays are never larger, by splitting them up into multiple instances.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@439511 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/FObjMixed.java
+++ src/java/org/apache/fop/fo/FObjMixed.java
@@ -85,40 +85,63 @@ public abstract class FObjMixed extends FObj {
     }
     
     /**
-     * Adds accumulated text as one FOText instance.
-     * Makes sure that nested calls to itself do nothing.
+     * Adds accumulated text as one FOText instance, unless
+     * the one instance's char array contains more than 
+     * Short.MAX_VALUE characters. In the latter case the 
+     * instance is split up into more manageable chunks.
+     * 
      * @throws FOPException if there is a problem during processing
      */
     protected void flushText() throws FOPException {
-       if (ft != null) {
+        if (ft != null) {
             FOText lft = ft;
+            /* make sure nested calls to itself have no effect */
             ft = null;
-            if (getNameId() == FO_BLOCK) {
-                lft.createBlockPointers((org.apache.fop.fo.flow.Block) this);
-                this.lastFOTextProcessed = lft;
-            } else if (getNameId() != FO_MARKER
-                    && getNameId() != FO_TITLE
-                    && getNameId() != FO_BOOKMARK_TITLE) {
-                FONode fo = parent;
-                int foNameId = fo.getNameId();
-                while (foNameId != FO_BLOCK
-                        && foNameId != FO_MARKER
-                        && foNameId != FO_TITLE
-                        && foNameId != FO_BOOKMARK_TITLE
-                        && foNameId != FO_PAGE_SEQUENCE) {
-                    fo = fo.getParent();
-                    foNameId = fo.getNameId();
+            FOText tmpText;
+            int indexStart = 0;
+            int indexEnd = (lft.ca.length > Short.MAX_VALUE 
+                            ? Short.MAX_VALUE : lft.ca.length) - 1;
+            int charCount = 0;
+            short tmpSize;
+            while (charCount < lft.ca.length) {
+                tmpSize = (short) (indexEnd - indexStart + 1);
+                charCount += tmpSize;
+                tmpText = (FOText) lft.clone(this, false);
+                tmpText.ca = new char[tmpSize];
+                tmpText.startIndex = 0;
+                tmpText.endIndex = tmpSize;
+                System.arraycopy(lft.ca, indexStart, 
+                                tmpText.ca, 0, indexEnd - indexStart + 1);
+                if (getNameId() == FO_BLOCK) {
+                    tmpText.createBlockPointers((org.apache.fop.fo.flow.Block) this);
+                    this.lastFOTextProcessed = tmpText;
+                } else if (getNameId() != FO_MARKER
+                        && getNameId() != FO_TITLE
+                        && getNameId() != FO_BOOKMARK_TITLE) {
+                    FONode fo = parent;
+                    int foNameId = fo.getNameId();
+                    while (foNameId != FO_BLOCK
+                            && foNameId != FO_MARKER
+                            && foNameId != FO_TITLE
+                            && foNameId != FO_BOOKMARK_TITLE
+                            && foNameId != FO_PAGE_SEQUENCE) {
+                        fo = fo.getParent();
+                        foNameId = fo.getNameId();
+                    }
+                    if (foNameId == FO_BLOCK) {
+                        tmpText.createBlockPointers((org.apache.fop.fo.flow.Block) fo);
+                        ((FObjMixed) fo).lastFOTextProcessed = tmpText;
+                    } else if (foNameId == FO_PAGE_SEQUENCE) {
+                        log.error("Could not create block pointers."
+                                + " FOText w/o Block ancestor.");
+                    }
                 }
-                if (foNameId == FO_BLOCK) {
-                    lft.createBlockPointers((org.apache.fop.fo.flow.Block) fo);
-                    ((FObjMixed) fo).lastFOTextProcessed = lft;
-                } else if (foNameId == FO_PAGE_SEQUENCE) {
-                    log.error("Could not create block pointers."
-                            + " FOText w/o Block ancestor.");
-                }
+                tmpText.endOfNode();
+                addChildNode(tmpText);
+                indexStart = indexEnd + 1;
+                indexEnd = (((lft.ca.length - charCount) < Short.MAX_VALUE)
+                    ? lft.ca.length : charCount + Short.MAX_VALUE) - 1;
             }
-            lft.endOfNode();
-            addChildNode(lft);
         }
     }
 
