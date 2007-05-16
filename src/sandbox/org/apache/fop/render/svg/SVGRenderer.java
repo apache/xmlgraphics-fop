9d9251d6940f69a0cc0a78044d4b6650d1896c99
Jeremias Maerki <jeremias@apache.org>
2007-05-16 09:57:47 +0000

    Small Javadoc issue fixed.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@538509 13f79535-47bb-0310-9956-ffa450edef68

--- src/sandbox/org/apache/fop/render/svg/SVGRenderer.java
+++ src/sandbox/org/apache/fop/render/svg/SVGRenderer.java
@@ -165,7 +165,7 @@ public class SVGRenderer extends Java2DRenderer {
 
     /**
      * Returns the OutputStream corresponding to this page
-     * @param 0-based pageNumber
+     * @param pageNumber 0-based page number
      * @return the corresponding OutputStream
      * @throws IOException In case of an I/O error
      */
