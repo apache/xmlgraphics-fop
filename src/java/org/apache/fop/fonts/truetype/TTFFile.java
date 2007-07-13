4e53cd8c59b4cc07ea7df4c84c8c71b829f9eb3e
Andreas L. Delmelle <adelmelle@apache.org>
2007-07-13 19:21:03 +0000

    Addition of a general-purpose int-to-int map to replace Integer-to-Integer HashMaps + first usage in TTFFile
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@556112 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fonts/truetype/TTFFile.java
+++ src/java/org/apache/fop/fonts/truetype/TTFFile.java
@@ -28,6 +28,7 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.fop.fonts.Glyphs;
+import org.apache.fop.util.IntMap;
 
 /**
  * Reads a TrueType file or a TrueType Collection.
@@ -110,8 +111,8 @@ public class TTFFile {
     
     // internal mapping of glyph indexes to unicode indexes
     // used for quick mappings in this class
-    private Map glyphToUnicodeMap = new HashMap();
-    private Map unicodeToGlyphMap = new HashMap();
+    private IntMap glyphToUnicodeMap = new IntMap();
+    private IntMap unicodeToGlyphMap = new IntMap();
 
     private TTFDirTabEntry currentDirTab;
 
@@ -133,8 +134,8 @@ public class TTFFile {
         UnicodeMapping(int glyphIndex, int unicodeIndex) {
             this.unicodeIndex = unicodeIndex;
             this.glyphIndex = glyphIndex;
-            glyphToUnicodeMap.put(new Integer(glyphIndex), new Integer(unicodeIndex));
-            unicodeToGlyphMap.put(new Integer(unicodeIndex), new Integer(glyphIndex));
+            glyphToUnicodeMap.put(glyphIndex, unicodeIndex);
+            unicodeToGlyphMap.put(unicodeIndex, glyphIndex);
         }
 
         /**
@@ -1504,7 +1505,7 @@ public class TTFFile {
      * @throws IOException if glyphIndex not found
      */
     private Integer glyphToUnicode(int glyphIndex) throws IOException {
-        return (Integer) glyphToUnicodeMap.get(new Integer(glyphIndex));
+        return new Integer(glyphToUnicodeMap.get(glyphIndex));
     }
     
     /**
@@ -1516,7 +1517,7 @@ public class TTFFile {
      */
     private Integer unicodeToGlyph(int unicodeIndex) throws IOException {
         final Integer result = 
-            (Integer) unicodeToGlyphMap.get(new Integer(unicodeIndex));
+            new Integer(unicodeToGlyphMap.get(unicodeIndex));
         if (result == null) {
             throw new IOException(
                     "Glyph index not found for unicode value " + unicodeIndex);
