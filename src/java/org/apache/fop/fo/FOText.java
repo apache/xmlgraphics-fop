7937897c8959d648d53651a9d151456c8920b629
Andreas L. Delmelle <adelmelle@apache.org>
2007-09-16 12:52:28 +0000

    Bugzilla 42705: Fixed swallowing PCDATA during white-space removal in retrieved markers.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@576095 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/FOText.java
+++ src/java/org/apache/fop/fo/FOText.java
@@ -158,9 +158,9 @@ public class FOText extends FONode {
                 System.arraycopy(ca, 0, ft.ca, 0, ca.length);
             }
         }
-        prevFOTextThisBlock = null;
-        nextFOTextThisBlock = null;
-        ancestorBlock = null;
+        ft.prevFOTextThisBlock = null;
+        ft.nextFOTextThisBlock = null;
+        ft.ancestorBlock = null;
         return ft;
     }
 
