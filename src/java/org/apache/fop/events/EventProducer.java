65df8f670236256686f63fa1b3e79ae253c92e3c
Vincent Hennebert <vhennebert@apache.org>
2008-04-08 10:58:30 +0000

    Another small typo fix
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/Temp_ProcessingFeedback@645848 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/events/EventProducer.java
+++ src/java/org/apache/fop/events/EventProducer.java
@@ -20,8 +20,8 @@
 package org.apache.fop.events;
 
 /**
- * This is a marker interface which all event producer interfaces need to extend. These interface
- * the agree to a convention:
+ * This is a marker interface which all event producer interfaces need to extend. These interfaces
+ * must agree to the following convention:
  * <ul>
  *   <li>The first parameter of each method must be: <code>Object source</code>
  * </ul>
