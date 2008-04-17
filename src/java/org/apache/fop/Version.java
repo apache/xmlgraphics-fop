500c70df1df64e3c91d0601f5ace901269c75df8
Jeremias Maerki <jeremias@apache.org>
2008-04-17 07:01:19 +0000

    HeadURL was missing in the properties.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@648985 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/Version.java
+++ src/java/org/apache/fop/Version.java
@@ -41,7 +41,7 @@ public final class Version {
         if (version == null) {
             //Fallback if FOP is used in a development environment
             String headURL 
-                = "$HeadURL: http://svn.apache.org/repos/asf/xmlgraphics/fop/trunk/src/java/org/apache/fop/Version.java $";
+                = "$HeadURL$";
             version = headURL;
             final String pathPrefix = "/xmlgraphics/fop/";
             int pos = version.indexOf(pathPrefix); 
