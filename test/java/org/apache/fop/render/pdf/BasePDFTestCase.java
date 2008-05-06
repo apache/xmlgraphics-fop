33034fd98bb702ec9416d6385e5d793d69fbfcb4
Adrian Cumiskey <acumiskey@apache.org>
2008-05-06 16:23:06 +0000

    Very small cleanup to unit test.  This method does not need to be protected.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@653828 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/fop/render/pdf/BasePDFTestCase.java
+++ test/java/org/apache/fop/render/pdf/BasePDFTestCase.java
@@ -101,7 +101,7 @@ public class BasePDFTestCase extends AbstractFOPTestCase {
         }
     }
 
-    protected static Exception extractOriginalException(Exception e) {
+    private static Exception extractOriginalException(Exception e) {
         if (e.getCause() != null) {
             return extractOriginalException((Exception)e.getCause());
         } else if (e instanceof SAXException) {
