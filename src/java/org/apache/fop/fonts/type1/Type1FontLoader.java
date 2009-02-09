77e32216371ab86afb0b303399204a8d8337d890
Jeremias Maerki <jeremias@apache.org>
2009-02-09 16:58:29 +0000

    Added code to detect when a Type 1 font is not configured to use its native encoding. In such a case it needs to be re-encoded in PostScript. That used to always happen in earlier FOP versions but since support was added for all glyphs in a font, this has become a problem. So this change restores the behaviour of before when XML font metrics files or just the PFM are used.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@742620 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fonts/type1/Type1FontLoader.java
+++ src/java/org/apache/fop/fonts/type1/Type1FontLoader.java
@@ -139,6 +139,7 @@ public class Type1FontLoader extends FontLoader {
         //Encoding
         if (afm != null) {
             String encoding = afm.getEncodingScheme();
+            singleFont.setUseNativeEncoding(true);
             if ("AdobeStandardEncoding".equals(encoding)) {
                 singleFont.setEncoding(CodePointMapping.STANDARD_ENCODING);
             } else {
