a0e20d32becbe6ed149c0edad492e3e76311f7af
Adrian Cumiskey <acumiskey@apache.org>
2009-03-20 15:18:54 +0000

    A little font configuration cleaning.
    * Extracted business logic from PrintRendererConfigurator and created a new FontDetector and FontAdder in org.apache.fop.fonts package.
    * Created a new FontInfoConfigurator that factors out a lot of the static stuff in PrintRendererConfigurator.
    * PDFDocumentGraphics2DConfigurator now uses a new FontInfoConfigurator() instead of static PrintRendererConfigurator.buildFontListFromConfiguration() call.
    * static method updateReferencedFonts() in PrintRendererConfigurator moved to an instance method in FontManager.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@756522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/fop/render/AbstractConfigurator.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id: $ */
+
+package org.apache.fop.render;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.fop.apps.FOUserAgent;
+
+/**
+ * An abstract configurator
+ */
+public abstract class AbstractConfigurator {
+    /** logger instance */
+    protected static Log log = LogFactory.getLog(AbstractConfigurator.class);
+
+    private static final String MIME = "mime";
+    
+    /** fop factory configuration */
+    protected FOUserAgent userAgent = null;
+
+    /**
+     * Default constructor
+     * @param userAgent user agent
+     */
+    public AbstractConfigurator(FOUserAgent userAgent) {
+        super();
+        this.userAgent = userAgent;
+    }
+
+    /**
+     * Returns the configuration subtree for a specific renderer.
+     * @param mimeType the MIME type of the renderer
+     * @return the requested configuration subtree, null if there's no configuration
+     */
+    protected Configuration getConfig(String mimeType) {
+        Configuration cfg = userAgent.getFactory().getUserConfig();
+        if (cfg == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("userconfig is null");
+            }
+            return null;
+        }
+
+        Configuration userConfig = null;
+
+        String type = getType();
+        Configuration[] cfgs
+            = cfg.getChild(type + "s").getChildren(type);
+        for (int i = 0; i < cfgs.length; ++i) {
+            Configuration child = cfgs[i];
+            try {
+                if (child.getAttribute(MIME).equals(mimeType)) {
+                    userConfig = child;
+                    break;
+                }
+            } catch (ConfigurationException e) {
+                // silently pass over configurations without mime type
+            }
+        }
+        log.debug((userConfig == null ? "No u" : "U")
+                  + "ser configuration found for MIME type " + mimeType);
+        return userConfig;
+    }
+
+    /**
+     * Returns the configurator type
+     * @return the configurator type
+     */
+    public abstract String getType();
+}
