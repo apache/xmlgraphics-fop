8f4b1002fd8fc438436230cab60ade8112e4308b
Vincent Hennebert <vhennebert@apache.org>
2009-07-22 17:43:38 +0000

    Merged clean-up-only changes made in the ChangingIPDHack branch. Those correspond to the following revision numbers: 785291 785632 787733 787769 788371 793645
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@796809 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/PageBreaker.java
+++ src/java/org/apache/fop/layoutmgr/PageBreaker.java
@@ -77,6 +77,14 @@ public class PageBreaker extends AbstractBreaker {
         return pslm.getPageProvider();
     }
 
+    /**
+     * Starts the page breaking process.
+     * @param flowBPD the constant available block-progression-dimension (used for every part)
+     */
+    void doLayout(int flowBPD) {
+        doLayout(flowBPD, false);
+    }
+
     /** {@inheritDoc} */
     protected PageBreakingLayoutListener createLayoutListener() {
         return new PageBreakingLayoutListener() {
