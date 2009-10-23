10e760606217b26ba77c7712204ee1aafa396fa4
Jeremias Maerki <jeremias@apache.org>
2009-10-23 12:22:58 +0000

    Added missing license headers.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@829021 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/afp/modca/triplets/AttributeValueTriplet.java
+++ src/java/org/apache/fop/afp/modca/triplets/AttributeValueTriplet.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
 package org.apache.fop.afp.modca.triplets;
 
 import java.io.IOException;
@@ -15,7 +34,7 @@ public class AttributeValueTriplet extends AbstractTriplet {
 
     /**
      * Main constructor
-     * 
+     *
      * @param attVal an attribute value
      */
     public AttributeValueTriplet(String attVal) {
@@ -45,7 +64,7 @@ public class AttributeValueTriplet extends AbstractTriplet {
     public int getDataLength() {
         return 4 + attVal.length();
     }
-    
+
     /** {@inheritDoc} */
     public String toString() {
         return attVal;
