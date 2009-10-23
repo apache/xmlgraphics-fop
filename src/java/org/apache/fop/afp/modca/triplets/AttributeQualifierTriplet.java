9c7a867efc15bd1bf181bd747ae4567598ba14d0
Jeremias Maerki <jeremias@apache.org>
2009-10-23 12:22:58 +0000

    Added missing license headers.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@829021 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/afp/modca/triplets/AttributeQualifierTriplet.java
+++ src/java/org/apache/fop/afp/modca/triplets/AttributeQualifierTriplet.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
 package org.apache.fop.afp.modca.triplets;
 
 import java.io.IOException;
@@ -16,7 +35,7 @@ public class AttributeQualifierTriplet extends AbstractTriplet {
 
     /**
      * Main constructor
-     * 
+     *
      * @param seqNumber the attribute qualifier sequence number
      * @param levNumber the attribute qualifier level number
      */
@@ -40,7 +59,7 @@ public class AttributeQualifierTriplet extends AbstractTriplet {
     public int getDataLength() {
         return 10;
     }
-    
+
     /** {@inheritDoc} */
     public String toString() {
         return "seqNumber=" + seqNumber + ", levNumber=" + levNumber;
