e6245e4939c018c81f41ec548dc61a8669c749d0
Jeremias Maerki <jeremias@apache.org>
2010-11-15 13:51:20 +0000

    Removed tab characters.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@1035275 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/fop/fonts/EncodingModeTest.java
+++ test/java/org/apache/fop/fonts/EncodingModeTest.java
@@ -3,15 +3,15 @@ package org.apache.fop.fonts;
 import junit.framework.TestCase;
 
 public class EncodingModeTest extends TestCase {
-	public void testGetName() {
-		assertEquals("auto", EncodingMode.AUTO.getName());
-		assertEquals("single-byte", EncodingMode.SINGLE_BYTE.getName());
-		assertEquals("cid", EncodingMode.CID.getName());
-	}
-	
-	public void testGetValue() {
-		assertEquals(EncodingMode.AUTO, EncodingMode.getEncodingMode("auto"));
-		assertEquals(EncodingMode.SINGLE_BYTE, EncodingMode.getEncodingMode("single-byte"));
-		assertEquals(EncodingMode.CID, EncodingMode.getEncodingMode("cid"));
-	}
+    public void testGetName() {
+        assertEquals("auto", EncodingMode.AUTO.getName());
+        assertEquals("single-byte", EncodingMode.SINGLE_BYTE.getName());
+        assertEquals("cid", EncodingMode.CID.getName());
+    }
+
+    public void testGetValue() {
+        assertEquals(EncodingMode.AUTO, EncodingMode.getEncodingMode("auto"));
+        assertEquals(EncodingMode.SINGLE_BYTE, EncodingMode.getEncodingMode("single-byte"));
+        assertEquals(EncodingMode.CID, EncodingMode.getEncodingMode("cid"));
+    }
 }
