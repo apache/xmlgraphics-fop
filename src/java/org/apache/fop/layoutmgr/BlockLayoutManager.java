7a7b7467aea03e3558bdc1234d49adaa9a81deee
Andreas L. Delmelle <adelmelle@apache.org>
2011-02-06 14:14:51 +0000

    Decommission BlockStackingLM.StackingIter
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@1067670 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/BlockLayoutManager.java
+++ src/java/org/apache/fop/layoutmgr/BlockLayoutManager.java
@@ -279,7 +279,7 @@ public class BlockLayoutManager extends BlockStackingLayoutManager
 
         // the Positions in positionList were inside the elements
         // created by the LineLM
-        childPosIter = new StackingIter(positionList.listIterator());
+        childPosIter = new PositionIterator(positionList.listIterator());
 
         while ((childLM = childPosIter.getNextChildLM()) != null) {
             // set last area flag
