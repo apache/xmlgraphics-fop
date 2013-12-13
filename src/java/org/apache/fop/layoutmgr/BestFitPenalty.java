80c12d5e35cec4929eb3564c8713082f9e663eb8
Vincent Hennebert <vhennebert@apache.org>
2013-12-13 23:04:04 +0000

    Attach the selected variant to KnuthPageNode in order to propagate the information down to area tree creation. Patch by Seifeddine Dridi, applied with some modifications.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/Temp_WhitespaceManagement@1550847 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/BestFitPenalty.java
+++ src/java/org/apache/fop/layoutmgr/BestFitPenalty.java
@@ -19,55 +19,59 @@
 
 package org.apache.fop.layoutmgr;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.fop.layoutmgr.BestFitLayoutUtils.BestFitPosition;
 
 /**
- * A penalty class used to specify a set of alternatives for the layout engine
+ * A type of penalty used to specify a set of alternatives for the layout engine
  * to choose from. The chosen alternative must have an occupied size
- * that is less than the available BPD of the current page
- * and it must also be the best match when it is evaluated by {@link FittingStrategy}.
+ * that is less than the available BPD of the current page.
  */
 public class BestFitPenalty extends KnuthPenalty {
 
-    private final List<ListElement> knuthList;
-    public boolean ignorePenalty;
+    public class Variant {
 
-    public BestFitPenalty(int width, List<ListElement> knuthList, Position pos) {
-        super(width, 0, false, pos, false);
-        this.knuthList = knuthList;
-    }
+        public final List<ListElement> knuthList;
+        public final int width;
 
-    public void activateContent() {
-        BestFitPosition pos = getBestFitPosition();
-        pos.setKnuthList(knuthList);
-    }
-
-    public int getWidth() {
-        if (ignorePenalty) {
-            return 0;
+        public Variant(List<ListElement> knuthList, int width) {
+            this.knuthList = knuthList;
+            this.width = width;
         }
-        return super.getWidth();
+        public KnuthElement toPenalty() {
+            return new KnuthPenalty(width, 0, false, null, false);
+        }
     }
 
-    public BestFitPosition getBestFitPosition() {
-        Position pos = super.getPosition();
-        while (pos != null) {
-            if (pos instanceof BestFitPosition) {
-                return (BestFitPosition) pos;
-            }
-            pos = pos.getPosition();
-        }
-        return null;
+    private final BestFitPosition bestFitPosition;
+
+    private final List<Variant> variantList;
+
+    public BestFitPenalty(BestFitPosition pos) {
+        super(0, 0, false, pos, false);
+        this.bestFitPosition = pos;
+        variantList = new ArrayList<Variant>();
+    }
+
+    public void addVariant(List<ListElement> knuthList, int width) {
+        variantList.add(new Variant(knuthList, width));
+    }
+
+    public void activatePenalty(Variant bestVariant) {
+        bestFitPosition.setKnuthList(bestVariant.knuthList);
+    }
+
+    public List<Variant> getVariantList() {
+        return variantList;
     }
 
     @Override
     public String toString() {
         String str = super.toString();
         StringBuffer buffer = new StringBuffer(64);
-//        buffer.append(" number of alternatives = " + alternatives.size());
-//        buffer.append(" fitting-strategy = " + strategy.getStrategyName());
+        buffer.append(" number of variants = " + variantList.size());
         return str + buffer.toString();
     }
 
