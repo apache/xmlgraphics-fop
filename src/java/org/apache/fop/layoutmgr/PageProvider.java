358ad666359815ae26db4353c42f7bed93c23e73
Matthias Reischenbacher <matthias@apache.org>
2015-06-16 10:58:27 +0000

    FOP-2487: fix checkstyle issue
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@1685757 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/layoutmgr/PageProvider.java
+++ src/java/org/apache/fop/layoutmgr/PageProvider.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.apache.fop.area.AreaTreeHandler;
 import org.apache.fop.area.PageViewport;
 import org.apache.fop.fo.Constants;
