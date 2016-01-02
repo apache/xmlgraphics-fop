6d6c7f0a84279f8fdb7531941c1cbfad7ef8dfd8
Andreas L. Delmelle <adelmelle@apache.org>
2016-01-02 23:32:37 +0000

    Checkstyle fix - Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/trunk@1722681 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fop/fo/FObj.java
+++ src/java/org/apache/fop/fo/FObj.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
