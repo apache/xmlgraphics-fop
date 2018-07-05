56fd6c20022cd376f7c6ee9a5374f8d1e8279c7e
Simon Steiner <ssteiner@apache.org>
2018-07-05 12:35:12 +0000

    FOP-1760: Findbugs
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/fop/branches/Temp_ChangeBars2@1835133 13f79535-47bb-0310-9956-ffa450edef68

--- fop-core/src/main/java/org/apache/fop/area/inline/InlineBlock.java
+++ fop-core/src/main/java/org/apache/fop/area/inline/InlineBlock.java
@@ -27,6 +27,7 @@ import org.apache.fop.area.Block;
  */
 public class InlineBlock extends InlineParent {
 
+    private static final long serialVersionUID = -3725062353292109517L;
     private final Block block;
 
     public InlineBlock(Block block) {
